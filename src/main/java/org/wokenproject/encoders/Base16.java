fc4c549321dcc0e130d21a10aa2237e79c8a9b51
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 15:30:53 +0100

    Update Base16.java

--- src/main/java/org/wokenproject/encoders/Base16.java
+++ src/main/java/org/wokenproject/encoders/Base16.java
@@ -6,4 +6,17 @@ public class Base16 {
     private final static char[] HEX = new char[]{
             '0', '1', '2', '3', '4', '5', '6', '7',
             '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    /**
+     * Convert bytes to a base16 string.
+     */
+    public static String encode(byte[] byteArray)
+    {
+        StringBuffer hexBuffer = new StringBuffer(byteArray.length * 2);
+        for (int i = 0; i < byteArray.length; i++)
+            for (int j = 1; j >= 0; j--)
+                hexBuffer.append(HEX[(byteArray[i] >> (j * 4)) & 0xF]);
+            
+        return hexBuffer.toString();
+    }
 }
