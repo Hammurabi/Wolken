91e8e24c059b7e1e83f064f4cbbbc4b638f4bc42
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 15:33:28 +0100

    Update Base58.java

--- src/main/java/org/wokenproject/encoders/Base58.java
+++ src/main/java/org/wokenproject/encoders/Base58.java
@@ -3,4 +3,5 @@ package org.wokenproject.encoders;
 public class Base58 {
     private static final char[] ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
             .toCharArray();
+    private static final int BASE_58 = ALPHABET.length;
 }
