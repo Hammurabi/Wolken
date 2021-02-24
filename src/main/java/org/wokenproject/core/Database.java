8a044def555df021b9ea583d39e298ae40466859
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 17:04:57 +0100

    Update Database.java

--- src/main/java/org/wokenproject/core/Database.java
+++ src/main/java/org/wokenproject/core/Database.java
@@ -4,6 +4,7 @@ import org.iq80.leveldb.DB;
 import org.wokenproject.utils.FileService;
 import org.iq80.leveldb.Options;
 import org.iq80.leveldb.impl.Iq80DBFactory;
+import org.wokenproject.utils.Utils;
 
 import java.io.IOException;
 import java.util.concurrent.locks.ReentrantLock;
@@ -13,10 +14,12 @@ public class Database {
     private ReentrantLock   mutex;
 
     private final static byte[]
-    ChainTipHash            = {'t', 'h'},
-    ChainWork               = {'c', 'w'},
-    BlockLookupFromHeight   = {'b', 'h'},
-    BlockLookup             = {'b'}
+    UnspentTransactionOutput= Utils.takeApartShort((short) 1),
+    TransactionFromHash     = Utils.takeApartShort((short) 2),
+    ChainTipHash            = Utils.takeApartShort((short) 3),
+    ChainWork               = Utils.takeApartShort((short) 4),
+    BlockLookupFromHeight   = Utils.takeApartShort((short) 5),
+    BlockLookup             = Utils.takeApartShort((short) 6)
     ;
 
     public Database(FileService location) throws IOException {
