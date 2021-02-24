fd0958d80e37c5731fe6a274665ccd94eb3b59a0
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 16:11:51 +0100

    Update Context.java

--- src/main/java/org/wokenproject/core/Context.java
+++ src/main/java/org/wokenproject/core/Context.java
@@ -1,4 +1,23 @@
 package org.wokenproject.core;
 
+import org.wokenproject.exceptions.WolkenException;
+
 public class Context {
+    private static Context instance;
+
+    private NetworkParameters networkParameters;
+
+    public Context() throws WolkenException {
+        this.networkParameters = new NetworkParameters(false);
+    }
+
+    public NetworkParameters getNetworkParameters()
+    {
+        return networkParameters;
+    }
+
+    public static Context getInstance()
+    {
+        return instance;
+    }
 }
