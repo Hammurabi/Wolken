a63c9a7b76c47b95d420b9c9b855128caa541b5b
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 16:42:46 +0100

    Update Database.java

--- src/main/java/org/wokenproject/core/Database.java
+++ src/main/java/org/wokenproject/core/Database.java
@@ -4,12 +4,18 @@ import org.iq80.leveldb.DB;
 import org.wokenproject.utils.FileService;
 import org.iq80.leveldb.Options;
 import org.iq80.leveldb.impl.Iq80DBFactory;
+import sun.awt.Mutex;
+
+import java.io.IOException;
 
 public class Database {
-    private DB db;
+    private DB      db;
+    private Mutex   mutex;
 
-    public Database(FileService location)
-    {
-        db = Iq80DBFactory.factory.open(location.file(), new Options());
+    public Database(FileService location) throws IOException {
+        db      = Iq80DBFactory.factory.open(location.file(), new Options());
+        mutex   = new Mutex();
     }
+
+    public LookupResult<Output>
 }
