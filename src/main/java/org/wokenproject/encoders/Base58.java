81926cb00706740a551ed8f48205e16edafcf51c
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 15:33:19 +0100

    Update Base58.java

--- src/main/java/org/wokenproject/encoders/Base58.java
+++ src/main/java/org/wokenproject/encoders/Base58.java
@@ -1,4 +1,6 @@
 package org.wokenproject.encoders;
 
 public class Base58 {
+    private static final char[] ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+            .toCharArray();
 }
