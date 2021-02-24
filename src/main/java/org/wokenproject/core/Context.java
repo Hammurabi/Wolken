0aacdedda9ad609a052f1db928ccbf0ee48eaacc
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 17:53:55 +0100

    Update Context.java

--- src/main/java/org/wokenproject/core/Context.java
+++ src/main/java/org/wokenproject/core/Context.java
@@ -1,16 +1,21 @@
 package org.wokenproject.core;
 
 import org.wokenproject.exceptions.WolkenException;
+import org.wokenproject.utils.FileService;
+
+import java.io.IOException;
 
 public class Context {
     private static Context instance;
 
     private Database            database;
     private NetworkParameters   networkParameters;
+    private FileService         fileService;
 
-    public Context() throws WolkenException {
-        this.database           = new Database();
+    public Context(FileService service) throws WolkenException, IOException {
+        this.database           = new Database(service.newFile("db"));
         this.networkParameters  = new NetworkParameters(false);
+        this.fileService        = service;
     }
 
     public Database getDatabase()
