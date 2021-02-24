b3aae357b08230808b627cb2894d77a90e486fc6
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 15:30:32 +0100

    Create Base16.java

---
+++ src/main/java/org/wokenproject/encoders/Base16.java
@@ -0,0 +1,9 @@
+package org.wokenproject.encoders;
+
+import java.util.Arrays;
+
+public class Base16 {
+    private final static char[] HEX = new char[]{
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+}
