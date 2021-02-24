f04ce6b029bc60b462898d3e2abffeca8b2d2200
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 15:33:51 +0100

    Update Base58.java

--- src/main/java/org/wokenproject/encoders/Base58.java
+++ src/main/java/org/wokenproject/encoders/Base58.java
@@ -7,4 +7,13 @@ public class Base58 {
     private static final int BASE_256 = 256;
 
     private static final int[] INDEXES = new int[128];
+
+    static {
+        for (int i = 0; i < INDEXES.length; i++) {
+            INDEXES[i] = -1;
+        }
+        for (int i = 0; i < ALPHABET.length; i++) {
+            INDEXES[ALPHABET[i]] = i;
+        }
+    }
 }
