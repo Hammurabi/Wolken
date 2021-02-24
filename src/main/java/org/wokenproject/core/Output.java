62a969b029dc7a075728b7ab8a158c6e8e55015d
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 17:48:25 +0100

    Update Output.java

--- src/main/java/org/wokenproject/core/Output.java
+++ src/main/java/org/wokenproject/core/Output.java
@@ -1,6 +1,44 @@
 package org.wokenproject.core;
 
+import org.wokenproject.exceptions.WolkenException;
+
+import java.nio.ByteBuffer;
+
 public class Output {
-    public Output(byte[] data) {
+    private long value;
+    private byte data[];
+
+    public Output(long value, byte data[])
+    {
+        this.value  = value;
+        this.data   = data;
+    }
+
+    public Output(byte[] data) throws WolkenException {
+        if (data == null)
+        {
+            throw new WolkenException("null byte array provided.");
+        }
+
+        if (data.length < 12)
+        {
+            throw new WolkenException("invalid byte array provided.");
+        }
+
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        this.value = buffer.getLong();
+        int length = buffer.getInt();
+        this.data = new byte[length];
+        buffer.get(this.data);
+    }
+
+    public long getValue()
+    {
+        return value;
+    }
+
+    public byte[] getData()
+    {
+        return data;
     }
 }
