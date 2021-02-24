45cb06bc821f1a7c03eaa98d85206725e0c7a19b
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-24 15:31:18 +0100

    Update Base16.java

--- src/main/java/org/wokenproject/encoders/Base16.java
+++ src/main/java/org/wokenproject/encoders/Base16.java
@@ -16,7 +16,33 @@ public class Base16 {
         for (int i = 0; i < byteArray.length; i++)
             for (int j = 1; j >= 0; j--)
                 hexBuffer.append(HEX[(byteArray[i] >> (j * 4)) & 0xF]);
-            
+
         return hexBuffer.toString();
+    }
+
+    /**
+     * Convert a base16 string into a byte array.
+     */
+    public static byte[] decode(String s)
+    {
+        s = s.toUpperCase();
+        int len = s.length();
+        byte[] r = new byte[len / 2];
+        for (int i = 0; i < r.length; i++)
+        {
+            int digit1 = s.charAt(i * 2), digit2 = s.charAt(i * 2 + 1);
+            if (digit1 >= '0' && digit1 <= '9')
+                digit1 -= '0';
+            else if (digit1 >= 'A' && digit1 <= 'F')
+                digit1 -= 'A' - 10;
+            if (digit2 >= '0' && digit2 <= '9')
+                digit2 -= '0';
+            else if (digit2 >= 'A' && digit2 <= 'F')
+                digit2 -= 'A' - 10;
+
+            r[i] = (byte) ((digit1 << 4) + digit2);
+        }
+
+        return r;
     }
 }
