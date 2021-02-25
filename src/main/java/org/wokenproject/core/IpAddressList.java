e572c30746ee36a11b04c5a04b64db361d9a6721
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 16:59:08 +0100

    Update IpAddressList.java

--- src/main/java/org/wokenproject/core/IpAddressList.java
+++ src/main/java/org/wokenproject/core/IpAddressList.java
@@ -1,4 +1,37 @@
 package org.wokenproject.core;
 
+import org.wokenproject.network.NetAddress;
+import org.wokenproject.utils.FileService;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashSet;
+import java.util.Set;
+
 public class IpAddressList {
+    private Set<NetAddress> addresses;
+
+    public IpAddressList(FileService service)
+    {
+        if (service.exists())
+        {
+            try {
+                ObjectInputStream stream = new ObjectInputStream(new FileInputStream(service.file()));
+                this.addresses = (Set<NetAddress>) stream.readObject();
+                stream.close();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            this.addresses  = new HashSet<>();
+        }
+    }
+
+    public Set<NetAddress> getAddresses()
+    {
+        return addresses;
+    }
 }
