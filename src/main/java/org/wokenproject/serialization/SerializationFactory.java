bcf5f51300c9c5f1235678a5fa559a7f4d24d675
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 22:07:08 +0100

    Update SerializationFactory.java

--- src/main/java/org/wokenproject/serialization/SerializationFactory.java
+++ src/main/java/org/wokenproject/serialization/SerializationFactory.java
@@ -2,14 +2,30 @@ package org.wokenproject.serialization;
 
 import org.wokenproject.serialization.SerializableI;
 
+import java.io.BufferedInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SerializationFactory {
+    private Map<Class, Integer>         classMagicReferences;
     private Map<Integer, SerializableI> magicReferences;
 
     public SerializationFactory()
     {
+        classMagicReferences = new HashMap<>();
         magicReferences = new HashMap<>();
+    }
+
+    /*
+        register an instance of serializable object.
+     */
+    public void registerClass(Class<?> classType, SerializableI serializableInstance)
+    {
+
+    }
+
+    public <Type extends SerializableI> Type fromStream(BufferedInputStream stream)
+    {
+        return null;
     }
 }
