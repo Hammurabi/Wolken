3163c4524d6f8d23b3fdf8e578166326b6242e4d
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 16:52:38 +0100

    Update Server.java

--- src/main/java/org/wokenproject/network/Server.java
+++ src/main/java/org/wokenproject/network/Server.java
@@ -3,6 +3,7 @@ package org.wokenproject.network;
 import org.wokenproject.core.Context;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Set;
@@ -16,8 +17,26 @@ public class Server {
         Context.getInstance().getThreadPool().execute(this::listenForIncomingConnections);
     }
 
-    public boolean connectToNodes()
+    public boolean connectToNodes(Set<NetAddress> addresses)
     {
+        int connections = 0;
+
+        for (NetAddress address : addresses)
+        {
+            try {
+                Socket socket = new Socket(address.getAddress(), address.getPort());
+                connectedNodes.add(new Node(socket));
+
+                if (++ connections == Context.getInstance().getNetworkParameters().getMaxAllowedOutboundConnections())
+                {
+                    return true;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
     }
 
     private void listenForIncomingConnections()
