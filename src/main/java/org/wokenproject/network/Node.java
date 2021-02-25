1bcceffa3f01f306124f30ade7fee28df9c940c7
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 15:56:22 +0100

    Update Node.java

--- src/main/java/org/wokenproject/network/Node.java
+++ src/main/java/org/wokenproject/network/Node.java
@@ -2,7 +2,9 @@ package org.wokenproject.network;
 
 import org.wokenproject.core.Context;
 import org.wokenproject.exceptions.WolkenException;
+import org.wokenproject.utils.Utils;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
@@ -17,21 +19,19 @@ public class Node {
     private MessageCache    messageCache;
     private int             errors;
 
-    private byte            messageHeader[];
-    private byte            buffer[];
+    private BufferedInputStream inputStream;
 
     public Node(String ip, int port) throws IOException {
         this(new Socket(ip, port));
     }
 
-    public Node(Socket socket) {
+    public Node(Socket socket) throws IOException {
         this.socket         = socket;
         this.mutex          = new ReentrantLock();
         this.messages       = new ConcurrentLinkedQueue<>();
         this.messageCache   = new MessageCache();
         this.errors         = 0;
-        this.messageHeader  = new byte[16];
-        this.buffer         = new byte[Context.getInstance().getNetworkParameters().getBufferSize()];
+        this.inputStream    = new BufferedInputStream(socket.getInputStream(), Context.getInstance().getNetworkParameters().getBufferSize());
     }
 
     public void sendMessage(Message message) {
@@ -46,10 +46,28 @@ public class Node {
 
         // a loop that hangs the entire thread might be dangerous.
         //         while ((read = stream.read(messageHeader, read, messageHeader.length - read)) != messageHeader.length);
+        byte messageHeader[] = new byte[20];
+
         int read = stream.read(messageHeader);
         if (read != messageHeader.length)
         {
             return null;
         }
+
+        int version = Utils.makeInt(messageHeader);
+        int flags   = Utils.makeInt(messageHeader);
+        int type    = Utils.makeInt(messageHeader);
+        int count   = Utils.makeInt(messageHeader);
+        int length  = Utils.makeInt(messageHeader);
+
+        byte content[] = new byte[length];
+
+        read = stream.read(content); // while (read < content.length) { read += stream.read(content, read, content.length - read); }
+        if (read != content.length)
+        {
+            return null;
+        }
+
+        return new Message(version, flags, type, count, content);
     }
 }
