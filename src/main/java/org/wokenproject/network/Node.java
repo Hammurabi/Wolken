a4dbd7b48d1e0027beb85837dc877e0617aa4630
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 14:43:39 +0100

    Update Node.java

--- src/main/java/org/wokenproject/network/Node.java
+++ src/main/java/org/wokenproject/network/Node.java
@@ -1,4 +1,20 @@
 package org.wokenproject.network;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Node {
+    private Socket          socket;
+    private ReentrantLock   mutex;
+
+    public Node(String ip, int port) throws IOException {
+        this(new Socket(ip, port));
+    }
+
+    public Node(Socket socket)
+    {
+        this.socket = socket;
+        this.mutex  = new ReentrantLock();
+    }
 }
