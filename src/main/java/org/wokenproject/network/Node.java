95e25cdb7462cd5f47773d52c3a4c481afcd3bd2
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 17:18:53 +0100

    Update Node.java

--- src/main/java/org/wokenproject/network/Node.java
+++ src/main/java/org/wokenproject/network/Node.java
@@ -47,34 +47,37 @@ public class Node {
         }
     }
 
-    public Message listen() throws IOException {
-        // a loop that hangs the entire thread might be dangerous.
-        //         while ((read = stream.read(messageHeader, read, messageHeader.length - read)) != messageHeader.length);
-        byte messageHeader[] = new byte[20];
+    public Message listen() {
+        try {
+            // a loop that hangs the entire thread might be dangerous.
+            //         while ((read = stream.read(messageHeader, read, messageHeader.length - read)) != messageHeader.length);
+            byte messageHeader[] = new byte[20];
 
-        int read = inputStream.read(messageHeader);
-        if (read != messageHeader.length)
-        {
+            int read = inputStream.read(messageHeader);
+            if (read != messageHeader.length) {
+                errors++;
+                return null;
+            }
+
+            int version = Utils.makeInt(messageHeader);
+            int flags   = Utils.makeInt(messageHeader, 4);
+            int type    = Utils.makeInt(messageHeader, 8);
+            int count   = Utils.makeInt(messageHeader, 12);
+            int length  = Utils.makeInt(messageHeader, 16);
+
+            byte content[] = new byte[length];
+
+            read = inputStream.read(content); // while (read < content.length) { read += stream.read(content, read, content.length - read); }
+            if (read != content.length) {
+                errors++;
+                return null;
+            }
+
+            return new Message(version, flags, type, count, content);
+        } catch (IOException e) {
             errors ++;
             return null;
         }
-
-        int version = Utils.makeInt(messageHeader);
-        int flags   = Utils.makeInt(messageHeader, 4);
-        int type    = Utils.makeInt(messageHeader, 8);
-        int count   = Utils.makeInt(messageHeader, 12);
-        int length  = Utils.makeInt(messageHeader, 16);
-
-        byte content[] = new byte[length];
-
-        read = inputStream.read(content); // while (read < content.length) { read += stream.read(content, read, content.length - read); }
-        if (read != content.length)
-        {
-            errors ++;
-            return null;
-        }
-
-        return new Message(version, flags, type, count, content);
     }
 
     public void send()
