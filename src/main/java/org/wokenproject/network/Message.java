32c9a2ebb1a5baea5cfd11716242fc90fef52a39
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 15:01:45 +0100

    Update Message.java

--- src/main/java/org/wokenproject/network/Message.java
+++ src/main/java/org/wokenproject/network/Message.java
@@ -1,16 +1,30 @@
 package org.wokenproject.network;
 
-public class Message {
-    private int version;
-    private int flags;
-    private int contentType;
-    private int instanceCount;
+import org.wokenproject.utils.HashUtil;
+import org.wokenproject.utils.Utils;
 
-    public Message(int version, int flags, int type, int count)
+public class Message {
+    private int     version;
+    private int     flags;
+    private int     contentType;
+    private int     instanceCount;
+    private byte    content[];
+
+    public Message(int version, int flags, int type, int count, byte content[])
     {
         this.version        = version;
         this.flags          = flags;
         this.contentType    = type;
         this.instanceCount  = count;
+        this.content        = content;
+    }
+
+    public byte[] contentHash()
+    {
+        return HashUtil.hash160(Utils.concatenate(
+                Utils.takeApart(contentType),
+                Utils.takeApart(instanceCount),
+                content
+        ));
     }
 }
