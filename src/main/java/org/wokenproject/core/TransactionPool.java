d356019a10cba71e692ece60d21f27353ecf396c
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-26 00:31:40 +0100

    Update TransactionPool.java

--- src/main/java/org/wokenproject/core/TransactionPool.java
+++ src/main/java/org/wokenproject/core/TransactionPool.java
@@ -1,4 +1,38 @@
 package org.wokenproject.core;
 
+import org.wokenproject.utils.Utils;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
 public class TransactionPool {
+    private Queue<TransactionI> transactions;
+    private Set<byte[]>         txids;
+
+    public TransactionPool()
+    {
+        transactions    = new PriorityQueue<>();
+        txids           = new HashSet<>();
+    }
+
+    public boolean contains(byte[] txid)
+    {
+        return txids.contains(txid);
+    }
+
+    public Set<byte[]> getNonDuplicateTransactions(Set<byte[]> list) {
+        Set<byte[]> result = new HashSet<>();
+
+        for (byte[] txid : list)
+        {
+            if (!contains(txid))
+            {
+                result.add(txid);
+            }
+        }
+
+        return result;
+    }
 }
