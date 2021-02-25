6f393349b422b06a9397e7b2b803612406b755ed
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 17:41:00 +0100

    Update MessageCache.java

--- src/main/java/org/wokenproject/network/MessageCache.java
+++ src/main/java/org/wokenproject/network/MessageCache.java
@@ -1,9 +1,15 @@
 package org.wokenproject.network;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MessageCache {
     private Map<byte[], Message> messageMap;
+
+    public MessageCache()
+    {
+        messageMap = new HashMap<>();
+    }
 
     public void setReceivedMessage(Message message)
     {
