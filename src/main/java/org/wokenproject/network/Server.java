a2e15d5d0e025441d20ce909abf27f738a9c4083
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 16:35:31 +0100

    Create Server.java

---
+++ src/main/java/org/wokenproject/network/Server.java
@@ -0,0 +1,38 @@
+package org.wokenproject.network;
+
+import org.wokenproject.core.Context;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Set;
+
+public class Server {
+    private ServerSocket    socket;
+    private Set<Node>       connectedNodes;
+
+    public Server()
+    {
+        Context.getInstance().getThreadPool()
+    }
+
+    private void listenForIncomingConnections()
+    {
+        Socket incoming = null;
+        while (true)
+        {
+            try {
+                incoming = socket.accept();
+                if (connectedNodes.size() < (Context.getInstance().getNetworkParameters().getMaxAllowedInboundConnections() + Context.getInstance().getNetworkParameters().getMaxAllowedOutboundConnections()))
+                {
+                    connectedNodes.add(new Node(incoming));
+                }
+                else
+                {
+                    incoming.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
+}
