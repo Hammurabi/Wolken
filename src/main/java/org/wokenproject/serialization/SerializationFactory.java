af83907088cb7f874fb46269e327860ee29d9b95
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 22:54:58 +0100

    Update SerializationFactory.java

--- src/main/java/org/wokenproject/serialization/SerializationFactory.java
+++ src/main/java/org/wokenproject/serialization/SerializationFactory.java
@@ -1,8 +1,10 @@
 package org.wokenproject.serialization;
 
 import org.wokenproject.serialization.SerializableI;
+import org.wokenproject.utils.Utils;
 
 import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,8 +28,14 @@ public class SerializationFactory {
         magicReferences.put(magic, serializableInstance);
     }
 
-    public <Type extends SerializableI> Type fromStream(BufferedInputStream stream)
-    {
-        return null;
+    public <Type extends SerializableI> Type fromStream(BufferedInputStream stream) throws IOException {
+        byte magicBytes[] = new byte[4];
+        stream.read(magicBytes);
+
+        SerializableI serializable  = magicReferences.get(Utils.makeInt(magicBytes));
+        Type result                 = serializable.newInstance();
+        result.read(stream);
+
+        return result;
     }
 }
