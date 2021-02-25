e9529475917fb596323d23b8ad94e869378507c4
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 23:31:29 +0100

    Delete NotificationMessage.java

--- src/main/java/org/wokenproject/network/messages/NotificationMessage.java
+++
@@ -1,9 +0,0 @@
-package org.wokenproject.network.messages;
-
-import org.wokenproject.network.Message;
-
-public class NotificationMessage extends Message {
-    public NotificationMessage(int version, int type, int count, byte[] content) {
-        super(version, Message.Flags.NOTIFY, type, count, content);
-    }
-}
