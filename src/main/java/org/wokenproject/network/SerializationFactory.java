cb8744cec44140c9fe721126b71a9e9d4563fbaf
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 21:36:22 +0100

    Update SerializationFactory.java

--- src/main/java/org/wokenproject/network/SerializationFactory.java
+++ src/main/java/org/wokenproject/network/SerializationFactory.java
@@ -1,4 +1,13 @@
 package org.wokenproject.network;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SerializationFactory {
+    private Map<Integer, SerializableI> magicReferences;
+
+    public SerializationFactory()
+    {
+        magicReferences = new HashMap<>();
+    }
 }
