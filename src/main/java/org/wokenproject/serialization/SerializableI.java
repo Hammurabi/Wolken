ff8af77ec017807bcaba44e8882ab07f4a35fb3b
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 22:47:38 +0100

    Update SerializableI.java

--- src/main/java/org/wokenproject/serialization/SerializableI.java
+++ src/main/java/org/wokenproject/serialization/SerializableI.java
@@ -1,4 +1,28 @@
 package org.wokenproject.serialization;
 
-public interface SerializableI {
+import org.wokenproject.core.Context;
+
+import java.io.*;
+
+public abstract class SerializableI {
+    public abstract void write(OutputStream stream);
+    public abstract void read(InputStream stream);
+
+    public byte[] asByteArray() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        write(outputStream);
+        outputStream.flush();
+        outputStream.close();
+
+        return outputStream.toByteArray();
+    }
+
+    public <Type extends SerializableI> Type makeCopy() throws IOException {
+        byte array[] = asByteArray();
+        BufferedInputStream inputStream = new BufferedInputStream(new ByteArrayInputStream(array));
+        Type t = Context.getInstance().getSerialFactory().fromStream(inputStream);
+        inputStream.close();
+
+        return t;
+    }
 }
