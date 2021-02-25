aed572ab52086108b6d784bdbe4589a206278ddc
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 14:53:37 +0100

    Update Message.java

--- src/main/java/org/wokenproject/network/Message.java
+++ src/main/java/org/wokenproject/network/Message.java
@@ -1,4 +1,16 @@
 package org.wokenproject.network;
 
 public class Message {
+    private int version;
+    private int flags;
+    private int contentType;
+    private int instanceCount;
+
+    public Message(int version, int flags, int type, int count)
+    {
+        this.version        = version;
+        this.flags          = flags;
+        this.contentType    = type;
+        this.instanceCount  = count;
+    }
 }
