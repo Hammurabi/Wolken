7efe24a7a890c207c751fa2e7619ce34707274c9
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-25 15:25:17 +0100

    Update MessageCache.java

--- src/main/java/org/wokenproject/network/MessageCache.java
+++ src/main/java/org/wokenproject/network/MessageCache.java
@@ -1,4 +1,17 @@
 package org.wokenproject.network;
 
 public class MessageCache {
+    public void setReceivedMessage(byte[] hash)
+    {
+    }
+
+    private int numTimesReceived(byte[] hash)
+    {
+        return 0;
+    }
+
+    public boolean shouldSend(Message message)
+    {
+        return false;
+    }
 }
