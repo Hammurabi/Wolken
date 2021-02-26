d4d1756f4d63a11322a9434db1c149c23d6c6d11
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-26 14:15:48 +0100

    Update VersionInformation.java

--- src/main/java/org/wokenproject/network/VersionInformation.java
+++ src/main/java/org/wokenproject/network/VersionInformation.java
@@ -2,13 +2,22 @@ package org.wokenproject.network;
 
 import org.wokenproject.exceptions.WolkenException;
 import org.wokenproject.serialization.SerializableI;
+import org.wokenproject.utils.Utils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class VersionInformation extends SerializableI {
-    public VersionInformation()
+    /**
+     * @param version       client version
+     * @param services      bitfield of services provided by this client
+     * @param timestamp     unix timestamp of message creation time
+     * @param sender        sender address
+     * @param receiver      received address
+     * @param blockHeight   current block height
+     */
+    public VersionInformation(int version, long services, long timestamp, NetAddress sender, NetAddress receiver, int blockHeight)
     {
     }
 
