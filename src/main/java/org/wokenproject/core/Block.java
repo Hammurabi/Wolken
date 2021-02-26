2923f07b6d56195708cec104ac49a174beadf944
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-26 14:12:34 +0100

    Update Block.java

--- src/main/java/org/wokenproject/core/Block.java
+++ src/main/java/org/wokenproject/core/Block.java
@@ -11,7 +11,7 @@ public class Block {
         return header.getVersion();
     }
 
-    public final long getHeight() {
+    public final int getHeight() {
         return header.getHeight();
     }
 
