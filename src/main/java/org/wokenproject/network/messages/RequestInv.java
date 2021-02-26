073c8efda03eabc16ff8c1fc844b04ab8a186c54
Hammurabi <unconfigured@null.spigotmc.org>
2021-02-26 16:40:42 +0100

    Create RequestInv.java

---
+++ src/main/java/org/wokenproject/network/messages/RequestInv.java
@@ -0,0 +1,40 @@
+package org.wokenproject.network.messages;
+
+import org.wokenproject.core.Context;
+import org.wokenproject.exceptions.WolkenException;
+import org.wokenproject.network.Message;
+import org.wokenproject.network.Node;
+import org.wokenproject.network.Server;
+import org.wokenproject.serialization.SerializableI;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RequestInv extends Message {
+    public RequestInv(int version) {
+        super(version, Flags.Request);
+    }
+
+    @Override
+    public void executePayload(Server server, Node node) {
+    }
+
+    @Override
+    public void writeContents(OutputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public void readContents(InputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return (Type) new RequestInv(0);
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return Context.getInstance().getSerialFactory().getSerialNumber(RequestInv.class);
+    }
+}
