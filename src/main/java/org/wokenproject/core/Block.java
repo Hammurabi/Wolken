0cd90203ed1ad77d90c5697f2974ee0fc0c1e1f2
Hammurabi <abdullahafadhil@gmail.com>
2021-02-26 18:16:08 +0100

    Update Block.java

--- src/main/java/org/wokenproject/core/Block.java
+++ src/main/java/org/wokenproject/core/Block.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 
-public class Block extends SerializableI {
+public class Block extends BlockHeader {
     public static int UniqueIdentifierLength = 32;
     private Set<TransactionI>   transactions;
 
@@ -22,48 +22,8 @@ public class Block extends SerializableI {
         return getBytes().length;
     }
 
-    public final int getVersion() {
-        return header.getVersion();
-    }
-
-    public final int getHeight() {
-        return header.getHeight();
-    }
-
-    public final long getTimestamp() {
-        return header.getTimeStamp();
-    }
-
-    public final byte[] getPreviousHash() {
-        return header.getParentHash();
-    }
-
-    public final byte[] getMerkleRoot() {
-        return header.getMerkleRoot();
-    }
-
-    public final byte[] getChainWork() {
-        return header.getChainWork();
-    }
-
-    public final byte[] getBits() {
-        return header.getBits();
-    }
-
-    public final int getNonce() {
-        return header.getNonce();
-    }
-
-    public final byte[] getBytes() {
-        return null;
-    }
-
-    public final byte[] getHashCode() {
-        return getBlockHeader().getHashCode();
-    }
-
     public final BlockHeader getBlockHeader() {
-        return header;
+        return new BlockHeader(getVersion(), getTimestamp(), getParentHash(), getMerkleRoot(), getBits(), getNonce());
     }
 
     @Override
