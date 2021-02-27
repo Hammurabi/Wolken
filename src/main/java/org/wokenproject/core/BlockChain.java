8dcb4146e273da86fcf3c6a25bbca87b15d14a00
Hammurabi <abdullahafadhil@gmail.com>
2021-02-27 15:02:50 +0100

    Create BlockChain.java

---
+++ src/main/java/org/wokenproject/core/BlockChain.java
@@ -0,0 +1,20 @@
+package org.wokenproject.core;
+
+import java.util.Queue;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BlockChain implements Runnable {
+    private Block           tip;
+    private byte[]          chainWork;
+    private Queue<Block>    orphanedBlocks;
+
+    private ReentrantLock   lock;
+
+    public final void consensus()
+    {
+    }
+
+    @Override
+    public void run() {
+    }
+}
