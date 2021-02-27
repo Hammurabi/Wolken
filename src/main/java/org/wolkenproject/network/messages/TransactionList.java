7a1854002f2e623b3be4ec03056ad4ee61fff9e1
Hammurabi <abdullahafadhil@gmail.com>
2021-02-27 15:54:26 +0100

    Create TransactionList.java

---
+++ src/main/java/org/wolkenproject/network/messages/TransactionList.java
@@ -0,0 +1,66 @@
+package org.wolkenproject.network.messages;
+
+import org.wolkenproject.core.Context;
+import org.wolkenproject.core.TransactionI;
+import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.network.Message;
+import org.wolkenproject.network.Node;
+import org.wolkenproject.network.Server;
+import org.wolkenproject.serialization.SerializableI;
+import org.wolkenproject.utils.Utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class TransactionList extends Message {
+    private Set<TransactionI> transactions;
+
+    public TransactionList(int version, Collection<TransactionI> transactions) {
+        super(version, Flags.Response);
+        this.transactions = new LinkedHashSet<>(transactions);
+    }
+
+    @Override
+    public void executePayload(Server server, Node node) {
+    }
+
+    @Override
+    public void writeContents(OutputStream stream) throws IOException, WolkenException {
+        Utils.writeInt(transactions.size(), stream);
+        for (TransactionI transaction : transactions)
+        {
+            transaction.write(stream);
+        }
+    }
+
+    @Override
+    public void readContents(InputStream stream) throws IOException, WolkenException {
+        byte buffer[] = new byte[4];
+        stream.read(buffer);
+        int length = Utils.makeInt(buffer);
+
+        for (int i = 0; i < length; i ++)
+        {
+            try {
+                TransactionI transaction = Context.getInstance().getSerialFactory().fromStream(Context.getInstance().getSerialFactory().getSerialNumber(TransactionI.class), stream);
+                transactions.add(transaction);
+            } catch (WolkenException e) {
+                throw new IOException(e);
+            }
+        }
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return (Type) new TransactionList(getVersion(), transactions);
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return Context.getInstance().getSerialFactory().getSerialNumber(TransactionList.class);
+    }
+}
