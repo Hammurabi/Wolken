53e0bfd5d6c42dccb78ef76217780dcfdca1b4ee
Hammurabi <abdullahafadhil@gmail.com>
2021-02-27 19:02:12 +0100

    Update TransactionI.java

--- src/main/java/org/wolkenproject/core/TransactionI.java
+++ src/main/java/org/wolkenproject/core/TransactionI.java
@@ -1,6 +1,7 @@
 package org.wolkenproject.core;
 
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
+import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
 
 public abstract class TransactionI extends SerializableI {
@@ -15,9 +16,10 @@ public abstract class TransactionI extends SerializableI {
     public abstract int getVersion();
     public abstract int getFlag();
     public abstract int getLockTime();
-    public abstract long getTotalValue();
-    public abstract long getRemainingValue();
-    public abstract long getFee();
+    public abstract long getTotalInputValue() throws WolkenException;
+    public abstract long getTotalOutputValue();
+    public abstract long getRemainingValue() throws WolkenException;
+    public abstract long getFee() throws WolkenException;
     public abstract Input[] getInputs();
     public abstract Output[] getOutputs();
     public abstract TransactionI signWithKey(BCECPrivateKey currentPrivateKey);
