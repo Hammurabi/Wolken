64deaf94ec6b29ccacfec78564039f05134ef83e
Hammurabi <abdullahafadhil@gmail.com>
2021-02-27 15:18:06 +0100

    Update BlockIndex.java

--- src/main/java/org/wokenproject/core/BlockIndex.java
+++ src/main/java/org/wokenproject/core/BlockIndex.java
@@ -1,13 +1,21 @@
 package org.wokenproject.core;
 
 import org.wokenproject.exceptions.WolkenException;
+import org.wokenproject.serialization.SerializableI;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.math.BigInteger;
 
-public class BlockIndex {
+public class BlockIndex extends SerializableI {
     private Block block;
     private BigInteger chainWork;
     private int height;
+
+    public BlockIndex() {
+        this(new Block(), BigInteger.ZERO, 0);
+    }
 
     public BlockIndex(Block block, BigInteger chainWork, int height) {
         this.block = block;
@@ -29,5 +37,23 @@ public class BlockIndex {
 
     public BlockIndex generateNextBlock() throws WolkenException {
         return new BlockIndex(new Block(), chainWork.add(block.getWork()), height + 1);
+    }
+
+    @Override
+    public void write(OutputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public void read(InputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return (Type) new BlockIndex();
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return Context.getInstance().getSerialFactory().getSerialNumber(BlockIndex.class);
     }
 }
