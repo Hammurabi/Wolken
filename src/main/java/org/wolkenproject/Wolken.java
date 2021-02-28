a78966ac59c9741e9f18f772386cda68fa873416
Hammurabi <abdullahafadhil@gmail.com>
2021-02-28 19:56:57 +0100

    Update Wolken.java

--- src/main/java/org/wolkenproject/Wolken.java
+++ src/main/java/org/wolkenproject/Wolken.java
@@ -1,15 +1,22 @@
 package org.wolkenproject;
 
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
+import org.wolkenproject.core.Context;
 import org.wolkenproject.encoders.Base16;
 import org.wolkenproject.encoders.CryptoLib;
+import org.wolkenproject.utils.FileService;
 import org.wolkenproject.utils.HashUtil;
 
 public class Wolken {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) throws ParseException {
         CryptoLib.initialize();
 
         Options options = new Options();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse( options, args);
+
+        FileService mainDirectory   = new FileService();
+        boolean     isTestNet       =;
+        Context context = new Context(mainDirectory, isTestNet);
     }
 }
