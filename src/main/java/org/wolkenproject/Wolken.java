ef004d25ef868f302c3da06674cf9b7ca1c9fb40
Hammurabi <abdullahafadhil@gmail.com>
2021-02-28 19:51:36 +0100

    Update Wolken.java

--- src/main/java/org/wolkenproject/Wolken.java
+++ src/main/java/org/wolkenproject/Wolken.java
@@ -1,7 +1,10 @@
 package org.wolkenproject;
 
+import org.wolkenproject.encoders.CryptoLib;
+
 public class Wolken {
     public static void main(String args[])
     {
+        CryptoLib.initialize();
     }
 }
