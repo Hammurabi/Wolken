339a6e8c6bd2ebd3366062008d166c41bea45ffe
Hammurabi <abdullahafadhil@gmail.com>
2021-02-28 19:51:56 +0100

    Update Wolken.java

--- src/main/java/org/wolkenproject/Wolken.java
+++ src/main/java/org/wolkenproject/Wolken.java
@@ -1,10 +1,15 @@
 package org.wolkenproject;
 
+import org.apache.commons.cli.Options;
+import org.wolkenproject.encoders.Base16;
 import org.wolkenproject.encoders.CryptoLib;
+import org.wolkenproject.utils.HashUtil;
 
 public class Wolken {
     public static void main(String args[])
     {
         CryptoLib.initialize();
+
+        Options options = new Options();
     }
 }
