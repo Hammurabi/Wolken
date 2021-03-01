9c3dd620321c224a81ae0ee4f0c7efd8e4170239
Hammurabi <abdullahafadhil@gmail.com>
2021-03-01 17:57:39 +0100

    Update Output.java

--- src/main/java/org/wolkenproject/core/Output.java
+++ src/main/java/org/wolkenproject/core/Output.java
@@ -59,6 +59,7 @@ public class Output extends SerializableI {
         byte buffer[]   = new byte[8];
         stream.read(buffer);
         this.value      = Utils.makeLong(buffer);
+        stream.read(buffer, 0, 2);
         this.data       = new byte[Utils.makeInt((byte) 0, (byte) 0, buffer[0], buffer[1])];
     }
 
