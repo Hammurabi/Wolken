df52f525839f39b8b3fafe09a6b36902f4495571
Hammurabi <abdullahafadhil@gmail.com>
2021-03-02 21:26:25 +0100

    Update Address.java

--- src/main/java/org/wolkenproject/core/Address.java
+++ src/main/java/org/wolkenproject/core/Address.java
@@ -1,4 +1,11 @@
 package org.wolkenproject.core;
 
 public class Address {
+    public static boolean isValidAddress(byte[] address) {
+        if (address.length != 25) {
+            return false;
+        }
+
+        return true;
+    }
 }
