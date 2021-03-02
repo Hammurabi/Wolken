e68ea72c30d7c99b8ef4db6a3ae61a8b46241186
Hammurabi <abdullahafadhil@gmail.com>
2021-03-02 21:37:25 +0100

    Update Address.java

--- src/main/java/org/wolkenproject/core/Address.java
+++ src/main/java/org/wolkenproject/core/Address.java
@@ -4,6 +4,12 @@ import org.wolkenproject.utils.HashUtil;
 import org.wolkenproject.utils.Utils;
 
 public class Address {
+    private byte[] address;
+
+    public Address(byte address[]) {
+        this.address = address;
+    }
+
     public byte[] generate(byte prefix, byte publicKeyBytes[]) {
         byte prefixed[] = Utils.concatenate(new byte[] { prefix }, HashUtil.hash160(publicKeyBytes) );
         return Utils.concatenate(prefixed, Utils.trim(HashUtil.sha256d(prefixed), 0, 4));
@@ -19,7 +25,7 @@ public class Address {
         if (prefixed[0] != Context.getInstance().getNetworkParameters().getGenericMainnetAddressPrefix() && prefixed[0] != Context.getInstance().getNetworkParameters().getGenericTestnetAddressPrefix()) {
             return false;
         }
-        
+
         return Utils.equals(Utils.trim(HashUtil.sha256d(prefixed), 0, 4), Utils.trim(address, 21, 4));
     }
 }
