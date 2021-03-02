651f9a869b8b1081719385181902a08e8d83104b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-02 22:14:25 +0100

    Create ResponseMessage.java

---
+++ src/main/java/org/wolkenproject/network/messages/ResponseMessage.java
@@ -0,0 +1,46 @@
+package org.wolkenproject.network.messages;
+
+import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.network.Message;
+import org.wolkenproject.network.Node;
+import org.wolkenproject.network.Server;
+import org.wolkenproject.serialization.SerializableI;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public abstract class ResponseMessage extends Message {
+    private byte[] requester;
+
+    public ResponseMessage(int version, byte uniqueMessageIdentifier[]) {
+        super(version, Flags.Response);
+        this.requester = uniqueMessageIdentifier;
+    }
+
+    @Override
+    public void executePayload(Server server, Node node) {
+    }
+
+    @Override
+    public void writeContents(OutputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public void readContents(InputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return null;
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return 0;
+    }
+
+    protected byte[] getRequester() {
+        return requester;
+    }
+}
