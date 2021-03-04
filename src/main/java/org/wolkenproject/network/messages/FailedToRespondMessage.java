4f46d1130df82fdec587be1c0447421ae92d3c9b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 22:44:37 +0100

    Update FailedToRespondMessage.java

--- src/main/java/org/wolkenproject/network/messages/FailedToRespondMessage.java
+++ src/main/java/org/wolkenproject/network/messages/FailedToRespondMessage.java
@@ -1,5 +1,6 @@
 package org.wolkenproject.network.messages;
 
+import org.wolkenproject.core.Context;
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.utils.Utils;
 
@@ -50,5 +51,10 @@ public class FailedToRespondMessage extends ResponseMessage {
     @Override
     public int getResponseType() {
         return 0;
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return Context.getInstance().getSerialFactory().getSerialNumber(FailedToRespondMessage.class);
     }
 }
