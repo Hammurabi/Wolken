9eb1ec45026c71096fcfde7125eaec04d772efda
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 02:43:35 +0100

    Update Server.java

--- src/main/java/org/wolkenproject/network/Server.java
+++ src/main/java/org/wolkenproject/network/Server.java
@@ -190,4 +190,7 @@ public class Server implements Runnable {
     public NetAddress getNetAddress() {
         return netAddress;
     }
+
+    public void broadcastRequest(Message request) {
+    }
 }
