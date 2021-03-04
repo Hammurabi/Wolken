fd373b366a14c08519718e7b49645483167c021e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 22:51:00 +0100

    Update Node.java

--- src/main/java/org/wolkenproject/network/Node.java
+++ src/main/java/org/wolkenproject/network/Node.java
@@ -75,7 +75,7 @@ public class Node implements Runnable {
             if (messageCache.shouldSend(message))
             {
                 messages.add(message);
-                expectedResponse.put(id, message.getResponseType());
+                expectedResponse.put(id, ((Response)).getResponseMetadata());
                 shouldWait = true;
             }
         } finally {
