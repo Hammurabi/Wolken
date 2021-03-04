0f99f527d4d9933fc9f39a2196517fcbe4312edf
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 22:41:44 +0100

    Update FailedToRespondMessage.java

--- src/main/java/org/wolkenproject/network/messages/FailedToRespondMessage.java
+++ src/main/java/org/wolkenproject/network/messages/FailedToRespondMessage.java
@@ -7,14 +7,25 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class FailedToRespondMessage extends ResponseMessage {
+    public static final class ReasonFlags {
+        public static final long
+        NoReasonSpecified = 0,
+        CouldNotFindRequestedData = 1 << 1;
+    }
+
+    private long reasonFlags;
+
+    public FailedToRespondMessage(int version, long reasonFlags, byte[] uniqueMessageIdentifier) {
+        super(version, uniqueMessageIdentifier);
+        this.reasonFlags = reasonFlags;
+    }
+
     @Override
     public void writeContents(OutputStream stream) throws IOException, WolkenException {
-        
     }
 
     @Override
     public void readContents(InputStream stream) throws IOException, WolkenException {
-
     }
 
     @Override
