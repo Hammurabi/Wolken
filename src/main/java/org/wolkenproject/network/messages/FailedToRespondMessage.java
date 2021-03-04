3cf8a323e31800225aee5b4a43aea8797bd63887
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 22:39:35 +0100

    Update FailedToRespondMessage.java

--- src/main/java/org/wolkenproject/network/messages/FailedToRespondMessage.java
+++ src/main/java/org/wolkenproject/network/messages/FailedToRespondMessage.java
@@ -1,4 +1,39 @@
 package org.wolkenproject.network.messages;
 
+import org.wolkenproject.exceptions.WolkenException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class FailedToRespondMessage extends ResponseMessage {
+    @Override
+    public void writeContents(OutputStream stream) throws IOException, WolkenException {
+        
+    }
+
+    @Override
+    public void readContents(InputStream stream) throws IOException, WolkenException {
+
+    }
+
+    @Override
+    public <Type> Type getPayload() {
+        return null;
+    }
+
+    @Override
+    public boolean containsResponse() {
+        return false;
+    }
+
+    @Override
+    public boolean containsFullResponse() {
+        return false;
+    }
+
+    @Override
+    public int getResponseType() {
+        return 0;
+    }
 }
