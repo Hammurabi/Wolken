d1e62d8bdd87669b9375b7349baa096ac7244988
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 01:49:31 +0100

    Update BlockChain.java

--- src/main/java/org/wolkenproject/core/BlockChain.java
+++ src/main/java/org/wolkenproject/core/BlockChain.java
@@ -31,6 +31,7 @@ public class BlockChain implements Runnable {
                         if (block.getHeight() == tip.getHeight()) {
                         } else if (block.getHeight() == (tip.getHeight() + 1)) {
                         } else if (block.getHeight() > tip.getHeight()) {
+                        } else if (block.getHeight() < tip.getHeight()) {
                         }
                     }
                 }
