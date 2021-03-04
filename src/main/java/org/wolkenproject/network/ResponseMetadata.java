76bb22ee09804cc14799e95adc28ebc39d2c458a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 22:54:11 +0100

    Update ResponseMetadata.java

--- src/main/java/org/wolkenproject/network/ResponseMetadata.java
+++ src/main/java/org/wolkenproject/network/ResponseMetadata.java
@@ -3,5 +3,5 @@ package org.wolkenproject.network;
 import org.wolkenproject.network.messages.ResponseMessage;
 
 public interface ResponseMetadata {
-    boolean isResponseValid(ResponseMessage responseMessage);
+    boolean isResponseValid(Message responseMessage);
 }
