59589cc96c413198c0bd2b57ba1ff0669e23c278
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 09:38:46 +0100

    Update BlockIndexComparator.java

--- src/main/java/org/wolkenproject/core/BlockIndexComparator.java
+++ src/main/java/org/wolkenproject/core/BlockIndexComparator.java
@@ -1,6 +1,42 @@
 package org.wolkenproject.core;
 
+import org.wolkenproject.exceptions.WolkenException;
+
 import java.util.Comparator;
 
 public class BlockIndexComparator implements Comparator<BlockIndex> {
+    @Override
+    public int compare(BlockIndex a, BlockIndex b) {
+        try {
+            int compare = a.getChainWork().compareTo(b.getChainWork());
+
+            if (compare > 0) {
+                return -1;
+            }
+
+            if (compare < 0) {
+                return 1;
+            }
+
+            if (a.getSequenceId() < b.getSequenceId()) {
+                return -1;
+            }
+
+            if (a.getSequenceId() > b.getSequenceId()) {
+                return 1;
+            }
+
+            if (a.getBlock().getTransactionCount() > b.getBlock().getTransactionCount()) {
+                return 1;
+            }
+
+            if (a.getBlock().getTransactionCount() < b.getBlock().getTransactionCount()) {
+                return -1;
+            }
+
+            return -1;
+        } catch (WolkenException e) {
+            return 0;
+        }
+    }
 }
