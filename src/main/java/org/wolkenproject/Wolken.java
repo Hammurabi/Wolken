b8c9048bef2813c380edb0039094197c0f1b2b8d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 06:09:13 +0100

    Update Wolken.java

--- src/main/java/org/wolkenproject/Wolken.java
+++ src/main/java/org/wolkenproject/Wolken.java
@@ -19,6 +19,7 @@ public class Wolken {
         options.addOption("dir", true, "set the main directory for wolken, otherwise uses the default application directory of the system.");
         options.addOption("enable_testnet", true, "set the testnet to enabled/disabled.");
         options.addOption("enable_mining", true, "set the node to a mining node.");
+        options.addOption("force_connect", true, "force a connection to an ip:port.");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
