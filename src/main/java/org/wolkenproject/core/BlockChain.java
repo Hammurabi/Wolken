7961063f3825d11f744e01ed4574249bbf604f73
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 03:37:15 +0100

    Update BlockChain.java

--- src/main/java/org/wolkenproject/core/BlockChain.java
+++ src/main/java/org/wolkenproject/core/BlockChain.java
@@ -124,6 +124,9 @@ public class BlockChain implements Runnable {
         }
     }
 
+    private void trimOrphans(int count) {
+    }
+
     public BlockIndex makeGenesisBlock() throws WolkenException {
         Block genesis = new Block(new byte[Block.UniqueIdentifierLength], 0);
         genesis.addTransaction(TransactionI.newCoinbase(0, "", Context.getInstance().getNetworkParameters().getMaxReward(), Context.getInstance().getPayList()));
