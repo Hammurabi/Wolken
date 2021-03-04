eb985782cd7307421c3315ec75a63f883c3e684e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 22:49:37 +0100

    Update Message.java

--- src/main/java/org/wolkenproject/network/Message.java
+++ src/main/java/org/wolkenproject/network/Message.java
@@ -105,7 +105,4 @@ public abstract class Message extends SerializableI {
     public abstract void readContents(InputStream stream) throws IOException, WolkenException;
 
     public abstract <Type> Type getPayload();
-    public abstract boolean containsResponse();
-    public abstract boolean containsFullResponse();
-    public abstract int getResponseType();
 }
