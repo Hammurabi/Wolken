3e7294148497a0150706737b60a966aa7c24a97a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-04 23:00:00 +0100

    Update ResponseMetadata.java

--- src/main/java/org/wolkenproject/network/ResponseMetadata.java
+++ src/main/java/org/wolkenproject/network/ResponseMetadata.java
@@ -4,6 +4,11 @@ import org.wolkenproject.network.messages.ResponseMessage;
 
 public interface ResponseMetadata {
     public static final class ValidationBits {
+        public static final int
+            FullResponse    = 0,
+            PartialResponse = 1,
+            InvalidResponse = 2;
+        ;
     }
 
     int isResponseValid(Message responseMessage);
