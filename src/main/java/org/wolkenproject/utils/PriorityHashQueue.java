c68edf299d01545017a33a4ad02a18d334961bba
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 02:11:26 +0100

    Update PriorityHashQueue.java

--- src/main/java/org/wolkenproject/utils/PriorityHashQueue.java
+++ src/main/java/org/wolkenproject/utils/PriorityHashQueue.java
@@ -7,12 +7,7 @@ public class PriorityHashQueue<T extends Comparable<T>> implements Queue<T> {
     private Queue<Entry<T>>         queue;
 
     private static class DefaultComparator implements Comparator<T extends Comparable<T>> {
-        @Override
-        public int compare(T t, T t1) {
-            return 0;
-        }
     }
-    
     private static class Entry<T extends Comparable<T>> implements Comparable<Entry<T>> {
         private T element;
 
