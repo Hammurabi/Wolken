57ebeac18943e61959fab90be4eafbb6a97ca9b6
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 02:12:08 +0100

    Update PriorityHashQueue.java

--- src/main/java/org/wolkenproject/utils/PriorityHashQueue.java
+++ src/main/java/org/wolkenproject/utils/PriorityHashQueue.java
@@ -6,7 +6,11 @@ public class PriorityHashQueue<T extends Comparable<T>> implements Queue<T> {
     private Map<byte[], Entry<T>>   entryMap;
     private Queue<Entry<T>>         queue;
 
-    private static class DefaultComparator implements Comparator<Entry<T extends Comparable<T>>> {
+    private static class DefaultComparator<T extends Comparable<T>> implements Comparator<Entry<T>> {
+        @Override
+        public int compare(Entry<T> tEntry, Entry<T> t1) {
+            return 0;
+        }
     }
 
     private static class Entry<T extends Comparable<T>> implements Comparable<Entry<T>> {
