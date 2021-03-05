cd13255287eeba918b057d813602795d9a24843f
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 04:06:40 +0100

    Create HeaderList.java

---
+++ src/main/java/org/wolkenproject/network/messages/HeaderList.java
@@ -0,0 +1,64 @@
+package org.wolkenproject.network.messages;
+
+import org.wolkenproject.core.BlockHeader;
+import org.wolkenproject.core.BlockIndex;
+import org.wolkenproject.core.Context;
+import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.serialization.SerializableI;
+import org.wolkenproject.utils.Utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+
+public class HeaderList extends ResponseMessage {
+    private Collection<BlockHeader> headers;
+
+    public HeaderList(int version, Collection<BlockIndex> blocks, byte[] uniqueMessageIdentifier) {
+        super(version, uniqueMessageIdentifier);
+        this.blocks   = new LinkedHashSet<>(blocks);
+    }
+
+    @Override
+    public void writeContents(OutputStream stream) throws IOException, WolkenException {
+        Utils.writeInt(blocks.size(), stream);
+        for (BlockIndex block : blocks)
+        {
+            block.write(stream);
+        }
+    }
+
+    @Override
+    public void readContents(InputStream stream) throws IOException, WolkenException {
+        byte buffer[] = new byte[4];
+        stream.read(buffer);
+        int length = Utils.makeInt(buffer);
+
+        for (int i = 0; i < length; i ++)
+        {
+            try {
+                BlockIndex block = Context.getInstance().getSerialFactory().fromStream(Context.getInstance().getSerialFactory().getSerialNumber(BlockIndex.class), stream);
+                blocks.add(block);
+            } catch (WolkenException e) {
+                throw new IOException(e);
+            }
+        }
+    }
+
+    @Override
+    public <Type> Type getPayload() {
+        return (Type) blocks;
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return (Type) new HeaderList(getVersion(), blocks, getUniqueMessageIdentifier());
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return Context.getInstance().getSerialFactory().getSerialNumber(HeaderList.class);
+    }
+}
