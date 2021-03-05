601cdb4c0b343cd5e69fcfa4126383e18e37a6a0
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 19:52:36 +0100

    Update Opcode.java

--- src/main/java/org/wolkenproject/core/script/Opcode.java
+++ src/main/java/org/wolkenproject/core/script/Opcode.java
@@ -3,4 +3,9 @@ package org.wolkenproject.core.script;
 import org.wolkenproject.serialization.SerializableI;
 
 public abstract class Opcode extends SerializableI {
+    private int identifier;
+
+    public void setIdentifier(int id) {
+        this.identifier = id;
+    }
 }
