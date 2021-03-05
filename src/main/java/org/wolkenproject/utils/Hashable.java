26c098f85354fda8657bff32df577bba7af0fd2d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 02:21:50 +0100

    Update Hashable.java

--- src/main/java/org/wolkenproject/utils/Hashable.java
+++ src/main/java/org/wolkenproject/utils/Hashable.java
@@ -1,6 +1,10 @@
 package org.wolkenproject.utils;
 
 public abstract class Hashable {
+    private byte[] hash160;
+    private byte[] hash256;
+    private byte[] hash256d;
+
     abstract byte[] getHash160();
     abstract byte[] getHash256();
     abstract byte[] getHash256d();
