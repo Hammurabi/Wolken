0440fa9becf1d2e97e6a9126602aec5cc4d9366e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 05:27:46 +0100

    Update AddressList.java

--- src/main/java/org/wolkenproject/network/AddressList.java
+++ src/main/java/org/wolkenproject/network/AddressList.java
@@ -3,10 +3,12 @@ package org.wolkenproject.network;
 import org.wolkenproject.core.Context;
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
+import org.wolkenproject.utils.Utils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class AddressList extends Message {
@@ -26,17 +28,25 @@ public class AddressList extends Message {
 
     @Override
     public void writeContents(OutputStream stream) throws IOException, WolkenException {
-
+        Utils.writeInt(addresses.size(), stream);
+        for (NetAddress address : addresses) {
+            address.write(stream);
+        }
     }
 
     @Override
     public void readContents(InputStream stream) throws IOException, WolkenException {
-
+        byte buffer[] = new byte[4];
+        stream.read(buffer);
+        int length = Utils.makeInt(buffer);
+        for (int i = 0; i < length; i ++) {
+            addresses.add(Context.getInstance().getSerialFactory().fromStream(NetAddress.class, stream));
+        }
     }
 
     @Override
     public <Type> Type getPayload() {
-        return null;
+        return (Type) addresses;
     }
 
     @Override
@@ -46,11 +56,11 @@ public class AddressList extends Message {
 
     @Override
     public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
-        return null;
+        return (Type) new AddressList(0, new LinkedHashSet<>());
     }
 
     @Override
     public int getSerialNumber() {
-        return 0;
+        return Context.getInstance().getSerialFactory().getSerialNumber(AddressList.class);
     }
 }
