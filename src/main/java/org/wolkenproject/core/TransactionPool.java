878a14f40a2daebdea1238e5e5ef6342205b6f56
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 02:31:15 +0100

    Update TransactionPool.java

--- src/main/java/org/wolkenproject/core/TransactionPool.java
+++ src/main/java/org/wolkenproject/core/TransactionPool.java
@@ -3,7 +3,7 @@ package org.wolkenproject.core;
 import java.util.*;
 
 public class TransactionPool {
-    private Map<byte[], TransactionI>   transactions;
+    private Queue<TransactionI>         transactions;
     private static final int            MaximumBlockQueueSize = 1_250_000_000;
 
     public TransactionPool() {
