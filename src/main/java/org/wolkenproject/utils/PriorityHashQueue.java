214b6ca734c6b7143dfb3a982ef21fdf2b81b49a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 02:07:07 +0100

    Update PriorityHashQueue.java

--- src/main/java/org/wolkenproject/utils/PriorityHashQueue.java
+++ src/main/java/org/wolkenproject/utils/PriorityHashQueue.java
@@ -6,4 +6,7 @@ import java.util.Iterator;
 import java.util.Queue;
 public class PriorityHashQueue<T> implements Queue<T> {
     private Map<byte[], PriorityHashQueue.Entry<T>> entryMap;
+    
+    private static class Entry<T> {
+    }
 }
