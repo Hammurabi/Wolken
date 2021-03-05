0c5bd0e062384b7b48bb27f50e684d0fdb02cdbf
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 19:27:38 +0100

    Update MochaVM.java

--- src/main/java/org/wolkenproject/core/script/MochaVM.java
+++ src/main/java/org/wolkenproject/core/script/MochaVM.java
@@ -4,4 +4,19 @@ import java.util.Set;
 
 public class MochaVM extends VirtualMachine {
     private Set<Opcode> opcodeSet;
+
+    @Override
+    public ExecutionResult execute(Script script) {
+        return null;
+    }
+
+    @Override
+    public void addOp(String name, boolean hasArgs, int lenArgs, int maxLenArgs, String desc, Opcode opcode) {
+        opcode.setIdentifier(opcodeSet.size());
+        opcode.setHasArguments(hasArgs);
+        opcode.setArgumentLength(lenArgs);
+        opcode.setMaxArgumentLength(maxLenArgs);
+        opcode.setDescription(desc);
+        opcodeSet.add(opcode);
+    }
 }
