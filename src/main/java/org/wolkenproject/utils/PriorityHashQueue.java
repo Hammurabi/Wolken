a0b1b673b999da08b3877c1b26cd3ac393e1264d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 02:04:32 +0100

    Update PriorityHashQueue.java

--- src/main/java/org/wolkenproject/utils/PriorityHashQueue.java
+++ src/main/java/org/wolkenproject/utils/PriorityHashQueue.java
@@ -1,6 +1,8 @@
 package org.wolkenproject.utils;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Queue;
 
-public class PriorityHashQueue implements Queue {
+public class PriorityHashQueue<T> implements Queue<T> {
 }
