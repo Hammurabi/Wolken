2c1a7b4c9f37df5650d957a95cc8d4a8d41fcab0
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 19:29:33 +0100

    Update VirtualMachine.java

--- src/main/java/org/wolkenproject/core/script/VirtualMachine.java
+++ src/main/java/org/wolkenproject/core/script/VirtualMachine.java
@@ -2,6 +2,8 @@ package org.wolkenproject.core.script;
 
 public abstract class VirtualMachine {
     public abstract ExecutionResult execute(Script script);
-    public abstract void addOp(String name, String desc, Opcode opcode);
+    public void addOp(String name, String desc, Opcode opcode) {
+        addOp(name, null, desc, opcode);
+    }
     public abstract void addOp(String name, BitFields args, String desc, Opcode opcode);
 }
