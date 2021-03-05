32cabbbf39b9e6f856640b3db97b2dfbe11d2c9c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-06 00:14:24 +0100

    Update install_maven.py

--- scripts/install_maven.py
+++ scripts/install_maven.py
@@ -2,3 +2,10 @@
 import os
 cwd = os.getcwd()
 print(cwd)
+
+# download maven from link
+import urllib
+url     = 'https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip'
+maven   = os.path.join(cwd, "tools", "maven.zip")
+print("downloading maven from '" + url + "'")
+print("installing maven to '" + maven + "'")
