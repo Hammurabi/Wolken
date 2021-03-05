c91de87a2749d212b90f9c83bbe4da42faffecd2
Hammurabi <abdullahafadhil@gmail.com>
2021-03-05 19:31:45 +0100

    Update BitFields.java

--- src/main/java/org/wolkenproject/core/script/BitFields.java
+++ src/main/java/org/wolkenproject/core/script/BitFields.java
@@ -1,4 +1,13 @@
 package org.wolkenproject.core.script;
 
 public class BitFields {
+    private int totalBits;
+
+    public int getTotalBits() {
+        return totalBits;
+    }
+
+    public int getTotalBytes() {
+        return (int) Math.ceil(totalBits / 8.0);
+    }
 }
