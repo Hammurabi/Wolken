a366b1ee071ed679a98a3826710bcd15cd0fe5f5
Hammurabi <abdullahafadhil@gmail.com>
2021-03-06 22:34:01 +0100

    Update OpcodeRegister.java

--- src/main/java/org/wolkenproject/core/script/OpcodeRegister.java
+++ src/main/java/org/wolkenproject/core/script/OpcodeRegister.java
@@ -1,12 +1,16 @@
 package org.wolkenproject.core.script;
 
+import java.util.List;
+
 public class OpcodeRegister {
+    private List<Opcode> opcodeList;
+
     public void registerOp(String name, String desc, Opcode opcode) {
         registerOp(name, null, desc, opcode);
     }
 
     // register an opcode into the vm
     public OpcodeRegister registerOp(String name, BitFields args, String desc, Opcode opcode) {
-        
+
     }
 }
