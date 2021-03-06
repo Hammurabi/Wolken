3124f971487ec03c6f5dc1f22eb76cc573c66c45
Hammurabi <abdullahafadhil@gmail.com>
2021-03-06 23:10:31 +0100

    Update MochaVM.java

--- src/main/java/org/wolkenproject/core/script/MochaVM.java
+++ src/main/java/org/wolkenproject/core/script/MochaVM.java
@@ -2,19 +2,5 @@ package org.wolkenproject.core.script;
 
 import java.util.Set;
 
-public class MochaVM extends OpcodeRegister {
-    private Set<Opcode> opcodeSet;
-
-    @Override
-    public ExecutionResult execute(Script script) {
-        return null;
-    }
-
-    @Override
-    public void registerOp(String name, BitFields args, String desc, Opcode opcode) {
-        opcode.setIdentifier(opcodeSet.size());
-        opcode.setArgs(args);
-        opcode.setDescription(desc);
-        opcodeSet.add(opcode);
-    }
+public class MochaVM extends VirtualMachine {
 }
