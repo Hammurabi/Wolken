bc1ed9f0629f9877597eae08ed9426b77badf806
Hammurabi <abdullahafadhil@gmail.com>
2021-03-06 23:19:46 +0100

    Update MemoryModule.java

--- src/main/java/org/wolkenproject/core/script/MemoryModule.java
+++ src/main/java/org/wolkenproject/core/script/MemoryModule.java
@@ -3,4 +3,8 @@ package org.wolkenproject.core.script;
 public class MemoryModule {
     public void setState(MemoryState memoryState) {
     }
+
+    public MemoryState getState() {
+        return null;
+    }
 }
