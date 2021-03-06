5c9111af18d3fef321bcf88f7c6f365b1fa44763
Hammurabi <abdullahafadhil@gmail.com>
2021-03-06 22:32:12 +0100

    Delete VirtualMachine.java

--- src/main/java/org/wolkenproject/core/script/VirtualMachine.java
+++
@@ -1,9 +0,0 @@
-package org.wolkenproject.core.script;
-
-public abstract class VirtualMachine {
-    public abstract ExecutionResult execute(Script script);
-    public void addOp(String name, String desc, Opcode opcode) {
-        addOp(name, null, desc, opcode);
-    }
-    public abstract void addOp(String name, BitFields args, String desc, Opcode opcode);
-}
