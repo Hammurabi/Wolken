4410dc8482670911231c52682087562e48f545dd
Hammurabi <abdullahafadhil@gmail.com>
2021-03-06 23:13:23 +0100

    Update MochaVM.java

--- src/main/java/org/wolkenproject/core/script/MochaVM.java
+++ src/main/java/org/wolkenproject/core/script/MochaVM.java
@@ -3,4 +3,8 @@ package org.wolkenproject.core.script;
 import java.util.Set;
 
 public class MochaVM extends VirtualMachine {
+    @Override
+    public MemoryState executeScript(Script script) {
+        return getMemoryState();
+    }
 }
