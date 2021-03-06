7843a819f4c61584f7b6133b664101d5bf44f822
Hammurabi <abdullahafadhil@gmail.com>
2021-03-06 23:07:28 +0100

    Update VirtualMachine.java

--- src/main/java/org/wolkenproject/core/script/VirtualMachine.java
+++ src/main/java/org/wolkenproject/core/script/VirtualMachine.java
@@ -1,9 +1,15 @@
 package org.wolkenproject.core.script;
 
 public abstract class VirtualMachine {
-    private OpcodeRegister opcodeRegister;
+    private OpcodeRegister  opcodeRegister;
+    private MemoryState     memoryState;
 
-    public abstract void executeScript(Script script);
+    public void swapMemoryState(MemoryState memoryState) {
+        this.memoryState = memoryState;
+    }
+    
+    public MemoryState executeScript(Script script);
+
     public OpcodeRegister getOpcodeRegister() {
         return opcodeRegister;
     }
