bb221b6691be9f20d5202217f7d854789f7484cc
Hammurabi <abdullahafadhil@gmail.com>
2021-03-06 01:20:55 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -12,6 +12,11 @@ echo python is installed
 java -version 2>NUL
 if errorlevel 1 goto errorNoJava
 echo java is installed
+if exist "C:\Program Files\Java\" goto setJavaHomex64
+if exist "C:\Program Files (x86)\Java\" goto setJavaHomex86
+
+echo java home set
+
 
 if not exist %~dp0tools\maven\bin\mvn.cmd goto installMavenTask
 echo maven is installed
@@ -45,4 +50,10 @@ pause
 
 :buildTask
 echo starting build
+%~dp0tools\maven\bin\mvn.cmd clean package
 pause
+
+:setJavaHomex64
+set JAVA_HOME="C:\Program Files\Java\%~java"
+:setJavaHomex86
+set JAVA_HOME="C:\Program Files (x86)\Java\%~java"
