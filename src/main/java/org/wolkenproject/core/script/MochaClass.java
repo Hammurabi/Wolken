ca5959e144c8101a4fcc396ff63fc5f75eb8c25d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-07 01:05:26 +0100

    Update MochaClass.java

--- src/main/java/org/wolkenproject/core/script/MochaClass.java
+++ src/main/java/org/wolkenproject/core/script/MochaClass.java
@@ -1,4 +1,7 @@
 package org.wolkenproject.core.script;
 
 public class MochaClass {
+    // call any non-default functions defined in this class
+    public MochaObject call(int functionPtr, MemoryModule memoryModule) {
+    }
 }
