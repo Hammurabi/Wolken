000fd9a6ff0b757817287ba63c7983e632928a34
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 00:59:33 +0100

    Update OpcodeList.java

--- src/main/java/org/wolkenproject/core/script/OpcodeList.java
+++ src/main/java/org/wolkenproject/core/script/OpcodeList.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class OpcodeList extends SerializableI {
+public class OpcodeList {
     // this class should be used to store opcodes
     // it will compact them in the most optimal
     // way for storage/network transfers to help
@@ -15,22 +15,5 @@ public class OpcodeList extends SerializableI {
 
     // contains bits from arguments
     private long argumentBits[];
-
-    @Override
-    public void write(OutputStream stream) throws IOException, WolkenException {
-    }
-
-    @Override
-    public void read(InputStream stream) throws IOException, WolkenException {
-    }
-
-    @Override
-    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
-        return null;
-    }
-
-    @Override
-    public int getSerialNumber() {
-        return 0;
-    }
+    private Opcode      opcodes[];
 }
