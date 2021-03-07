6ec083dffaff87fd479dde10d7aebffb3412c2c3
Hammurabi <abdullahafadhil@gmail.com>
2021-03-07 03:06:06 +0100

    Update MochaClass.java

--- src/main/java/org/wolkenproject/core/script/MochaClass.java
+++ src/main/java/org/wolkenproject/core/script/MochaClass.java
@@ -1,9 +1,18 @@
 package org.wolkenproject.core.script;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MochaClass {
-    private MochaFunction functions[];
+    private List<MochaFunction> functions;
 
     public MochaClass() {
+        functions = new ArrayList<>();
+        addFunction("hashCode", (mem)->{ return null; });
+    }
+
+    public void addFunction(String functionName, MochaFunction function) {
+        functions.add(function);
     }
 
     // call any functions defined in this class
