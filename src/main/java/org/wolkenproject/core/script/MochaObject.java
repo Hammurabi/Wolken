56f11852f3bdf6815d2e2c9d7a6fa57bfdad3ac0
Hammurabi <abdullahafadhil@gmail.com>
2021-03-07 05:06:23 +0100

    Update MochaObject.java

--- src/main/java/org/wolkenproject/core/script/MochaObject.java
+++ src/main/java/org/wolkenproject/core/script/MochaObject.java
@@ -1,14 +1,9 @@
 package org.wolkenproject.core.script;
 
-import org.wolkenproject.core.Context;
-import org.wolkenproject.exceptions.WolkenException;
-import org.wolkenproject.serialization.SerializableI;
-
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class MochaObject extends SerializableI {
+public class MochaObject {
     public static final Metadata DefaultMetadata = new Metadata();
     private Metadata                metadata;
     private MochaFunction           functions[];
@@ -26,26 +21,6 @@ public class MochaObject extends SerializableI {
 
     public Metadata getMetadata() {
         return metadata;
-    }
-
-    @Override
-    public void write(OutputStream stream) throws IOException, WolkenException {
-        metadata.write(stream);
-    }
-
-    @Override
-    public void read(InputStream stream) throws IOException, WolkenException {
-        metadata.read(stream);
-    }
-
-    @Override
-    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
-        return (Type) new MochaObject();
-    }
-
-    @Override
-    public int getSerialNumber() {
-        return Context.getInstance().getSerialFactory().getSerialNumber(MochaObject.class);
     }
 
     public static class Metadata {
