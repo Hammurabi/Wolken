522f04770e1ec8c000b9933fbcf770c03b09e89c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 00:12:31 +0100

    Update Context.java

--- src/main/java/org/wolkenproject/core/Context.java
+++ src/main/java/org/wolkenproject/core/Context.java
@@ -104,6 +104,7 @@ public class Context {
                                                     .addCond(12, (idx, prev, next, self)->{ return prev.getValue() == 2; }, "id")
                                                     .addCond(16, (idx, prev, next, self)->{ return prev.getValue() == 3; }, "id")
                                                     , "push an array of size x into the stack.", null);
+        virtualMachine.registerOp("iconst", "push an int of size [32] into the stack.", null);
         virtualMachine.registerOp("store", new BitFields()
                                                     .addField(4, "register")
                                                     , "pop x from stack and store it in register", null);
