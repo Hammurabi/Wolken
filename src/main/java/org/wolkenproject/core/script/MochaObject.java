92ccc6c712332de10893e980cc5fd6132bd8f103
Hammurabi <Riverssen@gmail.com>
2021-03-07 05:35:40 +0100

    Update MochaObject.java

--- src/main/java/org/wolkenproject/core/script/MochaObject.java
+++ src/main/java/org/wolkenproject/core/script/MochaObject.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class MochaObject {
-    public static final Metadata DefaultMetadata = new Metadata();
+    public static final char        DefaultMetadata = 0;
     private Metadata                metadata;
     private MochaFunction           functions[];
     private MochaObject             children[];
@@ -13,27 +13,13 @@ public class MochaObject {
         this(new Metadata(DefaultMetadata));
     }
 
-    public MochaObject(Metadata metadata) {
+    public MochaObject(VirtualMachine virtualMachine, Metadata metadata) {
         this.metadata   = new Metadata();
-        this.functions  = new MochaFunction[metadata.getFunctionCount()];
+        this.functions  = new MochaFunction[virtualMachine.getClassMetadata()];
         this.children   = new MochaObject[metadata.getChildCount()];
     }
 
     public Metadata getMetadata() {
         return metadata;
-    }
-
-    public static class Metadata {
-        private int parentClass;
-
-        public Metadata() {
-        }
-
-        public Metadata(Metadata metadata) {
-        }
-
-        public void write(OutputStream outputStream) {}
-
-        public void read(InputStream inputStream)  {}
     }
 }
