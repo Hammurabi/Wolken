c6d8e201673872d124b25406f9a3a2bd9b0bc08b
Hammurabi <Riverssen@gmail.com>
2021-03-07 05:42:03 +0100

    Update MetadataProvider.java

--- src/main/java/org/wolkenproject/core/script/MetadataProvider.java
+++ src/main/java/org/wolkenproject/core/script/MetadataProvider.java
@@ -8,6 +8,10 @@ public class MetadataProvider {
     Map<Character, Metadata> metadataMap;
 
     public Metadata getMetadata(char metadata) throws UndefClassException {
+        if (!metadataMap.containsKey(metadata)) {
+            throw new UndefClassException("no metadata found for '" + metadata + "'.");
+        }
+        
         return metadataMap.get(metadata);
     }
 }
