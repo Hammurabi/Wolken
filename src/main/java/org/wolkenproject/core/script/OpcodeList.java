affa73ca601980a282540d4dbcc38b476e343f5a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 00:56:47 +0100

    Create OpcodeList.java

---
+++ src/main/java/org/wolkenproject/core/script/OpcodeList.java
@@ -0,0 +1,11 @@
+package org.wolkenproject.core.script;
+
+public class OpcodeList {
+    // this class should be used to store opcodes
+    // it will compact them in the most optimal
+    // way for storage/network transfers to help
+    // reduce the cost of transactions.
+
+    // this contains SIGN information from const-push operations.
+    private int signBits[];
+}
