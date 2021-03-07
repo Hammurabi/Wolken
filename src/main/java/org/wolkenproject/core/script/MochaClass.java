f8adf2dbbb494e4eb1204ed366195d7479feec45
Hammurabi <abdullahafadhil@gmail.com>
2021-03-07 03:17:55 +0100

    Update MochaClass.java

--- src/main/java/org/wolkenproject/core/script/MochaClass.java
+++ src/main/java/org/wolkenproject/core/script/MochaClass.java
@@ -3,9 +3,7 @@ package org.wolkenproject.core.script;
 import org.wolkenproject.encoders.Base16;
 import org.wolkenproject.utils.Tuple;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MochaClass {
@@ -21,7 +19,14 @@ public class MochaClass {
         functions.put(functionName, new Tuple<>(functions.size(), function));
     }
 
-    // call any functions defined in this class
+    // call any functions defined in this class by name
+    public MochaObject call(String function, MemoryModule memoryModule) {
+        if (functions.containsKey(function)) {
+            return functions.get(function).getFirst();
+        }
+        return null;
+    }
+
     public MochaObject call(int functionPtr, MemoryModule memoryModule) {
         return null;
     }
