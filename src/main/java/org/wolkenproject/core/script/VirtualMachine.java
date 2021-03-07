118495801b1105f8a14c20cc67b232990b9cfced
Hammurabi <Riverssen@gmail.com>
2021-03-07 05:39:23 +0100

    Update VirtualMachine.java

--- src/main/java/org/wolkenproject/core/script/VirtualMachine.java
+++ src/main/java/org/wolkenproject/core/script/VirtualMachine.java
@@ -1,9 +1,9 @@
 package org.wolkenproject.core.script;
 
 public abstract class VirtualMachine {
-    private OpcodeRegister  opcodeRegister;
-    private Headers         headers;
-    private MemoryModule    memoryModule;
+    private OpcodeRegister      opcodeRegister;
+    private MetadataProvider    metadataProvider;
+    private MemoryModule        memoryModule;
 
     public abstract void executeScript(Script script);
 
@@ -17,5 +17,9 @@ public abstract class VirtualMachine {
 
     public MemoryState getMemoryState() {
         return memoryModule.getState();
+    }
+
+    public MetadataProvider getMetadataProvider() {
+        return metadataProvider;
     }
 }
