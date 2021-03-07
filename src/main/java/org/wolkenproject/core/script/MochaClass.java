4c96bbcb9c6cc4fe0f3a16bac11b07575241aa94
Hammurabi <abdullahafadhil@gmail.com>
2021-03-07 03:13:43 +0100

    Update MochaClass.java

--- src/main/java/org/wolkenproject/core/script/MochaClass.java
+++ src/main/java/org/wolkenproject/core/script/MochaClass.java
@@ -1,21 +1,24 @@
 package org.wolkenproject.core.script;
 
 import org.wolkenproject.encoders.Base16;
+import org.wolkenproject.utils.Tuple;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MochaClass {
-    private List<MochaFunction> functions;
+    private Map<String, Tuple<Integer, MochaFunction>> functions;
 
     public MochaClass() {
-        functions = new ArrayList<>();
+        functions = new HashMap<>();
         addFunction("hashCode", (mem)->{ return new MochaByteArray(mem.popStack().checksum()); });
         addFunction("toString", (mem)->{ return new MochaString(Base16.encode(mem.popStack().checksum())); });
     }
 
     public void addFunction(String functionName, MochaFunction function) {
-        functions.add(function);
+        functions.put(functionName, new Tuple<>(functions.size(), function));
     }
 
     // call any functions defined in this class
