557fe017b3e92277fab57c9c213f4b9053aaad0b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-07 11:41:21 +0100

    Update VirtualProcess.java

--- src/main/java/org/wolkenproject/core/script/VirtualProcess.java
+++ src/main/java/org/wolkenproject/core/script/VirtualProcess.java
@@ -5,6 +5,9 @@ public class VirtualProcess {
     private MemoryModule        memoryModule;
     private VirtualMachine      virtualMachine;
 
+    public void stopProcess(int signal) {
+    }
+
     public VirtualMachine getVirtualMachine() {
         return virtualMachine;
     }
