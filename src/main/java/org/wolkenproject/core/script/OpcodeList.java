4cef5817a8e1969e214e7c66fe28b424fb6f656b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 00:58:07 +0100

    Update OpcodeList.java

--- src/main/java/org/wolkenproject/core/script/OpcodeList.java
+++ src/main/java/org/wolkenproject/core/script/OpcodeList.java
@@ -1,6 +1,11 @@
 package org.wolkenproject.core.script;
 
+import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class OpcodeList extends SerializableI {
     // this class should be used to store opcodes
@@ -10,4 +15,22 @@ public class OpcodeList extends SerializableI {
 
     // contains bits from arguments
     private long argumentBits;
+
+    @Override
+    public void write(OutputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public void read(InputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return null;
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return 0;
+    }
 }
