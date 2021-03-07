ead52fcea9d0bbdf7e12b1f1e9d485d04443b28b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-07 11:42:52 +0100

    Update Opcode.java

--- src/main/java/org/wolkenproject/core/script/Opcode.java
+++ src/main/java/org/wolkenproject/core/script/Opcode.java
@@ -1,6 +1,12 @@
 package org.wolkenproject.core.script;
 
+import org.wolkenproject.core.Context;
+import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public abstract class Opcode extends SerializableI {
     private int identifier;
@@ -31,5 +37,23 @@ public abstract class Opcode extends SerializableI {
 
     public String getDesc() {
         return desc;
+    }
+
+    @Override
+    public void write(OutputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public void read(InputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return null;
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return Context.getInstance().getSerialFactory().getSerialNumber(Opcode.class);
     }
 }
