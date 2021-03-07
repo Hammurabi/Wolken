8270bb857ff2bb3fb74bc6ef2ad61c096297efca
Hammurabi <abdullahafadhil@gmail.com>
2021-03-07 10:50:56 +0100

    Update VirtualMachine.java

--- src/main/java/org/wolkenproject/core/script/VirtualMachine.java
+++ src/main/java/org/wolkenproject/core/script/VirtualMachine.java
@@ -2,24 +2,10 @@ package org.wolkenproject.core.script;
 
 public abstract class VirtualMachine {
     private OpcodeRegister      opcodeRegister;
-    private ClassProvider       metadataProvider;
-    private MemoryModule        memoryModule;
 
-    public abstract void executeScript(Script script);
+    public abstract VirtualProcess createProcess(Script script);
 
     public OpcodeRegister getOpcodeRegister() {
         return opcodeRegister;
-    }
-
-    public void swapMemoryState(MemoryState memoryState) {
-        memoryModule.setState(memoryState);
-    }
-
-    public MemoryState getMemoryState() {
-        return memoryModule.getState();
-    }
-
-    public ClassProvider getClassProvider() {
-        return metadataProvider;
     }
 }
