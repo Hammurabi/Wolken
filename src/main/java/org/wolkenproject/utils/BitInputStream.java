22125785b1110c913b9ebd92b1080f02057627d7
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 01:16:41 +0100

    Update BitInputStream.java

--- src/main/java/org/wolkenproject/utils/BitInputStream.java
+++ src/main/java/org/wolkenproject/utils/BitInputStream.java
@@ -1,6 +1,14 @@
 package org.wolkenproject.utils;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class BitInputStream extends InputStream {
+    private byte    array[];
+    private int     index;
+
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
 }
