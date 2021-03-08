b90f33c776c9e2a62f407bbd8864f3051e2a9b43
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 07:27:35 +0100

    Update OpHalt.java

--- src/main/java/org/wolkenproject/core/script/opcodes/OpHalt.java
+++ src/main/java/org/wolkenproject/core/script/opcodes/OpHalt.java
@@ -6,23 +6,21 @@ import org.wolkenproject.utils.BitInputStream;
 import org.wolkenproject.utils.BitOutputStream;
 
 public class OpHalt extends Opcode {
-    public OpHalt(String name, String desc, String usage) {
-        super(name, desc, usage);
+    public OpHalt() {
+        super("halt", "stop the process (and sub-processes).", "halt");
     }
 
     @Override
     public void execute(VirtualProcess virtualProcess) {
-
+        virtualProcess.stopProcess(0);
     }
 
     @Override
     public void write(BitOutputStream outputStream) {
-
     }
 
     @Override
     public void read(BitInputStream inputStream) {
-
     }
 
     @Override
