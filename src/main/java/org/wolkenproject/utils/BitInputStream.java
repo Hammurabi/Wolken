3604daa6c3be4437290f53a20ad3240d0082830f
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 01:20:18 +0100

    Update BitInputStream.java

--- src/main/java/org/wolkenproject/utils/BitInputStream.java
+++ src/main/java/org/wolkenproject/utils/BitInputStream.java
@@ -22,6 +22,6 @@ public class BitInputStream extends InputStream {
         int byteAddress = location / 8;
         int bitAddress  = location % 8;
 
-        return array[location];
+        return Utils.getBit(array[location], bitAddress);
     }
 }
