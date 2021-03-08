30e43c46f80590bb2fdf07bd98e7b580c38cabcb
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 07:25:20 +0100

    Update Opcode.java

--- src/main/java/org/wolkenproject/core/script/Opcode.java
+++ src/main/java/org/wolkenproject/core/script/Opcode.java
@@ -1,64 +1,42 @@
 package org.wolkenproject.core.script;
 
 import org.wolkenproject.utils.BitInputStream;
+import org.wolkenproject.utils.BitOutputStream;
 
-public class Opcode {
+public abstract class Opcode {
+    private String          name;
     private int             identifier;
-    private BitFields       args;
     private String          desc;
-    private OpcodeExecutor  executor;
+    private String          usage;
 
-    public void execute(VirtualProcess virtualProcess) {
-        executor.execute(virtualProcess);
+    public Opcode(String name, String desc, String usage) {
+        this.name = name;
+        this.desc = desc;
+        this.usage= usage;
     }
+
+    public abstract void execute(VirtualProcess virtualProcess);
+    public abstract void write(BitOutputStream outputStream);
+    public abstract void read(BitInputStream inputStream);
+    public abstract Opcode makeCopy();
 
     protected void setIdentifier(int id) {
         this.identifier = id;
     }
 
-    protected void setArgs(BitFields args) {
-        this.args = args;
-    }
-
-    protected void setDescription(String desc) {
-        this.desc = desc;
+    public String getName() {
+        return name;
     }
 
     public int getIdentifier() {
         return identifier;
     }
 
-    public BitFields getArgs() {
-        return args;
-    }
-
     public String getDesc() {
         return desc;
     }
 
-    public void setExecutor(OpcodeExecutor executor) {
-        this.executor = executor;
+    public String getUsage() {
+        return usage;
     }
-
-    public OpArgs takeArguments(BitInputStream inputStream) {
-        return args.getArguments(inputStream);
-    }
-
-//    @Override
-//    public void write(OutputStream stream) throws IOException, WolkenException {
-//    }
-//
-//    @Override
-//    public void read(InputStream stream) throws IOException, WolkenException {
-//    }
-//
-//    @Override
-//    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
-//        return null;
-//    }
-//
-//    @Override
-//    public int getSerialNumber() {
-//        return Context.getInstance().getSerialFactory().getSerialNumber(Opcode.class);
-//    }
 }
