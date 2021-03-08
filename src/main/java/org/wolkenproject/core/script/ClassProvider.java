acd5c35659d969fe3bd1117b45abed0ce086a5f9
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 08:30:33 +0100

    Update ClassProvider.java

--- src/main/java/org/wolkenproject/core/script/ClassProvider.java
+++ src/main/java/org/wolkenproject/core/script/ClassProvider.java
@@ -38,14 +38,38 @@ public class ClassProvider {
     private void setDefaultMochaClass() {
         MochaClass defaultClass = new MochaClass(null);
         defaultClass.setName("Object");
+        // (self)
         defaultClass.addFunction("hashCode", (proc)->{ return proc.getClassProvider().getArrayMochaClass().newInstanceNative(proc, proc.getMemoryModule().getStack().pop().getHash()); });
         defaultClass.addFunction("toString", (proc)->{ return null; });
         defaultClass.addFunction("getClassName", (proc)->{ return null; });
+
+        // (self, other)
+        defaultClass.addFunction("add", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("sub", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("mul", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("div", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("mod", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("shiftL", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("shiftR", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("arithmeticShift", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("and", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("or", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("not", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("xor", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+        defaultClass.addFunction("pow", (proc)->{ proc.getMemoryModule().getStack().pop(); return proc.getMemoryModule().getStack().pop(); });
+
+        // (self)
+        defaultClass.addFunction("negate", (proc)->{ return proc.getMemoryModule().getStack().pop(); });
 
         registerClass(defaultClass);
 
         MochaClass arrayClass = new MochaClass(defaultClass);
         arrayClass.setName("Array");
+        arrayClass.addFunction("append", (proc)->{ return null; });
+        arrayClass.addFunction("pop", (proc)->{ return null; });
+        arrayClass.addFunction("len", (proc)->{ return null; });
+        arrayClass.addFunction("reshape", (proc)->{ return null; });
+        arrayClass.addFunction("shape", (proc)->{ return null; });
         registerClass(arrayClass);
     }
 }
