af4d964bfc01c373573664c0be1afc1a8c456ee7
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 07:30:08 +0100

    Update OpPush.java

--- src/main/java/org/wolkenproject/core/script/opcodes/OpPush.java
+++ src/main/java/org/wolkenproject/core/script/opcodes/OpPush.java
@@ -6,19 +6,20 @@ import org.wolkenproject.utils.BitInputStream;
 import org.wolkenproject.utils.BitOutputStream;
 
 public class OpPush extends Opcode {
+    public OpPush() {
+        super(name, desc, usage);
+    }
+
     @Override
     public void execute(VirtualProcess virtualProcess) {
-
     }
 
     @Override
     public void write(BitOutputStream outputStream) {
-
     }
 
     @Override
     public void read(BitInputStream inputStream) {
-
     }
 
     @Override
