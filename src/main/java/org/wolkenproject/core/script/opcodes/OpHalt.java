81caeda61efee0e50fa10e417a972f518b743e35
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 07:25:37 +0100

    Update OpHalt.java

--- src/main/java/org/wolkenproject/core/script/opcodes/OpHalt.java
+++ src/main/java/org/wolkenproject/core/script/opcodes/OpHalt.java
@@ -1,4 +1,28 @@
 package org.wolkenproject.core.script.opcodes;
 
-public class OpHalt {
+import org.wolkenproject.core.script.Opcode;
+import org.wolkenproject.core.script.VirtualProcess;
+import org.wolkenproject.utils.BitInputStream;
+import org.wolkenproject.utils.BitOutputStream;
+
+public class OpHalt extends Opcode {
+    @Override
+    public void execute(VirtualProcess virtualProcess) {
+        
+    }
+
+    @Override
+    public void write(BitOutputStream outputStream) {
+
+    }
+
+    @Override
+    public void read(BitInputStream inputStream) {
+
+    }
+
+    @Override
+    public Opcode makeCopy() {
+        return null;
+    }
 }
