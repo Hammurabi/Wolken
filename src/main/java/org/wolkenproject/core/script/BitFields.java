6e3147754af6491b20e2d29c0b7e5f727f41d7c0
Hammurabi <abdullahafadhil@gmail.com>
2021-03-08 01:52:15 +0100

    Update BitFields.java

--- src/main/java/org/wolkenproject/core/script/BitFields.java
+++ src/main/java/org/wolkenproject/core/script/BitFields.java
@@ -6,30 +6,10 @@ import org.wolkenproject.utils.Tuple;
 import java.util.List;
 
 public class BitFields {
-    private List<Tuple<Integer, String>>    fields;
-    private int                             totalBits;
-    private int                             conditionalBits;
-
-    public int getTotalBits() {
-        return totalBits;
-    }
-    public int getConditionalBits() {
-        return conditionalBits;
-    }
-
-    public int getTotalBytes() {
-        return (int) Math.ceil(totalBits / 8.0);
-    }
+    private List<BitField>                  fields;
 
     public BitFields addField(int length, String name) {
-        totalBits += length;
-        fields.add(new Tuple<>(totalBits, name));
-
-        return this;
-    }
-
-    public BitFields addCond(int length, BitCondition condition, String name) {
-        conditionalBits += length;
+        fields.add();
         fields.add(new Tuple<>(totalBits, name));
 
         return this;
@@ -40,7 +20,12 @@ public class BitFields {
     }
 
     public static class BitField {
-        public int getValue() {
+        private BitCondition conditions[];
+        public void setConditions(BitCondition conditions[]) {
+            this.conditions = conditions;
+        }
+
+        public int getValue(BitInputStream inputStream) {
             return 0;
         }
     }
