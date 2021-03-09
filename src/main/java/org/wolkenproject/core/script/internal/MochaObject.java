49dc930c12acc7e3c5e1f0e650877035d7db27b8
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 00:57:24 +0100

    Create MochaObject.java

---
+++ src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
@@ -0,0 +1,12 @@
+package org.wolkenproject.core.script.internal;
+
+public class MochaObject {
+    public MochaObject add(MochaObject other) { return null; }
+    public MochaObject sub(MochaObject other) { return null; }
+    public MochaObject mul(MochaObject other) { return null; }
+    public MochaObject div(MochaObject other, boolean sign) { return null; }
+    public MochaObject mod(MochaObject other, boolean sign) { return null; }
+    public MochaObject shiftRight(MochaObject other, boolean sign) { return null; }
+    public MochaObject shiftLeft(MochaObject other, boolean sign) { return null; }
+    public MochaObject arithmeticShift(MochaObject other, boolean sign) { return null; }
+}
