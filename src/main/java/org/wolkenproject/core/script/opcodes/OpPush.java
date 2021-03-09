0e2b3c046d66f73001c0d05cef9fe9af7757c300
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 00:32:28 +0100

    Update OpPush.java

--- src/main/java/org/wolkenproject/core/script/opcodes/OpPush.java
+++ src/main/java/org/wolkenproject/core/script/opcodes/OpPush.java
@@ -1,14 +1,14 @@
 package org.wolkenproject.core.script.opcodes;
 
-import org.wolkenproject.core.script.MochaObject;
 import org.wolkenproject.core.script.Opcode;
 import org.wolkenproject.core.script.VirtualProcess;
 import org.wolkenproject.core.script.objects.MochaArray;
 import org.wolkenproject.exceptions.MochaException;
-import org.wolkenproject.exceptions.UndefClassException;
-import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.utils.BitInputStream;
 import org.wolkenproject.utils.BitOutputStream;
+import org.wolkenproject.utils.Utils;
+
+import java.io.IOException;
 
 public class OpPush extends Opcode {
     private byte array[];
@@ -34,7 +34,40 @@ public class OpPush extends Opcode {
     }
 
     @Override
-    public void write(BitOutputStream outputStream) {
+    public void write(BitOutputStream outputStream) throws IOException {
+        // 2 bits to define the type ( 5bit, 7bit, 10bit, 12bit )
+        int type = 0;
+        int bitLength = 0;
+
+        if (array.length <= 32) {
+            type = 0;
+            bitLength = 5;
+        }
+        else if (array.length <= 128) {
+            type = 1;
+            bitLength = 7;
+        }
+        else if (array.length <= 1024) {
+            type = 2;
+            bitLength = 10;
+        } else if (array.length <= 4096) {
+            type = 3;
+            bitLength = 12;
+        }
+
+        outputStream.write(Utils.getBit(type, 0));
+        outputStream.write(Utils.getBit(type, 1));
+
+        int arrayLength = array.length;
+        for (int i = 0; i < bitLength; i ++) {
+            outputStream.write(Utils.getBit(arrayLength, i));
+        }
+
+        for (byte b : array) {
+            for (int i = 0; i < 8; i ++) {
+                outputStream.write(Utils.getBit(b, i));
+            }
+        }
     }
 
     @Override
