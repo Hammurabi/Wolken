b94fcb67ae3a67336e2e3769c194a46fa603d8aa
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 01:08:05 +0100

    Update MochaObject.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
@@ -13,17 +13,30 @@ public class MochaObject {
     public MochaObject shiftRight(MochaObject other, boolean sign) { return this; }
     public MochaObject shiftLeft(MochaObject other, boolean sign) { return this; }
     public MochaObject arithmeticShift(MochaObject other, boolean sign) { return this; }
+    // equals (x==y)
     public MochaObject equal(MochaObject other) { return this; }
+    // and (x&y)
     public MochaObject and(MochaObject other) { return this; }
+    // or (x|y)
     public MochaObject or(MochaObject other) { return this; }
+    // xor (x^y)
     public MochaObject xor(MochaObject other) { return this; }
+    // not (!x)
     public MochaObject not() { return this; }
+    // negate (~x)
     public MochaObject negate() { return this; }
+    // get the length of an array
     public MochaObject length() { return this; }
+    // get the shape of an array
     public MochaObject shape() { return this; }
+    // reshape an array
     public MochaObject reshape(MochaObject shape) { return this; }
+    // add an element to the back
     public MochaObject append(MochaObject object) { return this; }
+    // add an element to the front
     public MochaObject prepend(MochaObject object) { return this; }
+    // pop the last element
     public MochaObject pop() { return this; }
+    // poll the first element
     public MochaObject poll() { return this; }
 }
