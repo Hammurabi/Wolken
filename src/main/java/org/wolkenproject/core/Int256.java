b827010cd8d467870ab343ac120160c85b4112ac
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 07:10:18 +0100

    Update Int256.java

--- src/main/java/org/wolkenproject/core/Int256.java
+++ src/main/java/org/wolkenproject/core/Int256.java
@@ -8,4 +8,14 @@ public class Int256 {
         this.data   = data;
         this.signed = signed;
     }
+
+    public void add(Int256 other) {
+        int carry   = 0;
+        int sum     = 0;
+
+        for (int i = 0; i < 4; i ++) {
+            for (int b = 0; b < 32; b ++) {
+            }
+        }
+    }
 }
