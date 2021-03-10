60545193dff029a677d9262b9e3d58fc760c436e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 01:02:35 +0100

    Update MochaObject.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
@@ -1,6 +1,25 @@
 package org.wolkenproject.core.script.internal;
 
+import org.wolkenproject.core.script.MochaFunction;
+
 public class MochaObject {
+    public static final int
+    FN_ADD = 0,
+    FN_SUB = 1,
+    FN_MUL = 2,
+    FN_DIV = 3,
+    FN_MOD = 4,
+    FN_SHR = 5,
+    FN_SHL = 6,
+    FN_ASH = 7,
+    FN_EQL = 8,
+    FN_AND = 9,
+    FN_OR = 10,
+    FN_XOR = 11,
+    FN_NOT = 12,
+    FN_NGT = 13;
+    private MochaFunction functions[];
+
     public MochaObject add(MochaObject other) { return this; }
     public MochaObject sub(MochaObject other) { return this; }
     public MochaObject mul(MochaObject other) { return this; }
