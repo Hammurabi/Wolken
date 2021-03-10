899368d32c8c501d6b1d21960a2b95714a3cf57d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 01:00:35 +0100

    Update MochaObject.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
@@ -1,12 +1,18 @@
 package org.wolkenproject.core.script.internal;
 
 public class MochaObject {
-    public MochaObject add(MochaObject other) { return null; }
-    public MochaObject sub(MochaObject other) { return null; }
-    public MochaObject mul(MochaObject other) { return null; }
-    public MochaObject div(MochaObject other, boolean sign) { return null; }
-    public MochaObject mod(MochaObject other, boolean sign) { return null; }
-    public MochaObject shiftRight(MochaObject other, boolean sign) { return null; }
-    public MochaObject shiftLeft(MochaObject other, boolean sign) { return null; }
-    public MochaObject arithmeticShift(MochaObject other, boolean sign) { return null; }
+    public MochaObject add(MochaObject other) { return this; }
+    public MochaObject sub(MochaObject other) { return this; }
+    public MochaObject mul(MochaObject other) { return this; }
+    public MochaObject div(MochaObject other, boolean sign) { return this; }
+    public MochaObject mod(MochaObject other, boolean sign) { return this; }
+    public MochaObject shiftRight(MochaObject other, boolean sign) { return this; }
+    public MochaObject shiftLeft(MochaObject other, boolean sign) { return this; }
+    public MochaObject arithmeticShift(MochaObject other, boolean sign) { return this; }
+    public MochaObject equal(MochaObject other) { return this; }
+    public MochaObject and(MochaObject other) { return this; }
+    public MochaObject or(MochaObject other) { return this; }
+    public MochaObject xor(MochaObject other) { return this; }
+    public MochaObject not() { return this; }
+    public MochaObject negate() { return this; }
 }
