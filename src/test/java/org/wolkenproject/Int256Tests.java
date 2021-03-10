baa4164b1c6a7d89bd532c04c1ace5d2b81c823a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 07:36:44 +0100

    Update Int256Tests.java

--- src/test/java/org/wolkenproject/Int256Tests.java
+++ src/test/java/org/wolkenproject/Int256Tests.java
@@ -6,7 +6,8 @@ import org.wolkenproject.core.Int256;
 public class Int256Tests {
     @Test
     void testInt256() {
-        Int256 first    = new Int256();
-        Int256 second   = new Int256();
+        Int256 first    = new Int256(1);
+        Int256 second   = new Int256(2);
+        long value      = first.add(second).asLong();
     }
 }
