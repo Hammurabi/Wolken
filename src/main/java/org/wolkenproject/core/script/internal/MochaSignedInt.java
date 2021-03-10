bab02d3730e3b919bda7fa6bf6330e625edb4dcf
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 06:47:58 +0100

    Update MochaSignedInt.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaSignedInt.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaSignedInt.java
@@ -1,4 +1,19 @@
 package org.wolkenproject.core.script.internal;
 
+import org.wolkenproject.exceptions.MochaException;
+import org.wolkenproject.utils.Utils;
+
+import java.math.BigInteger;
+
 public class MochaSignedInt extends MochaNumber {
+    public MochaSignedInt(long value) {
+        this.value = new BigInteger(Utils.takeApartLong(value));
+    }
+
+    public MochaSignedInt(BigInteger integer) throws MochaException {
+        this.value = integer;
+        if (integer.bitLength() > 256) {
+            throw new MochaException("the maximum size of int is 256 bits.");
+        }
+    }
 }
