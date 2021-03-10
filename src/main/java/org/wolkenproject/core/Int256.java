9bf22543be9269606eac600385ee39d168bd64d4
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 07:31:07 +0100

    Update Int256.java

--- src/main/java/org/wolkenproject/core/Int256.java
+++ src/main/java/org/wolkenproject/core/Int256.java
@@ -6,6 +6,13 @@ public class Int256 {
     public int      data[];
     public boolean  signed;
 
+    public Int256(long value) {
+        this(value, true);
+    }
+
+    public Int256(long value, boolean signed) {
+    }
+
     private Int256(int data[], boolean signed) {
         this.data   = data;
         this.signed = signed;
