5d1462052018059e191bb5885cc5891fa5fd7788
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 07:45:27 +0100

    Update Int256Tests.java

--- src/test/java/org/wolkenproject/Int256Tests.java
+++ src/test/java/org/wolkenproject/Int256Tests.java
@@ -1,13 +1,22 @@
 package org.wolkenproject;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.wolkenproject.core.Int256;
 
 public class Int256Tests {
     @Test
     void testInt256() {
-        Int256 first    = new Int256(1);
-        Int256 second   = new Int256(2);
-        long value      = first.add(second).asLong();
+        {
+            for (int i = 0; i < 20; i ++) {
+                long f          = (i * 1000) * 1000;
+                long s          = (i * 1000) * 1000 + 1;
+
+                Int256 first    = new Int256(f);
+                Int256 second   = new Int256(s);
+                long value      = first.add(second).asLong();
+                Assertions.assertEquals(f + s, value);
+            }
+        }
     }
 }
