05e30f2b862782581a466b920670724cacb69d70
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 06:47:00 +0100

    Update MochaNumber.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaNumber.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaNumber.java
@@ -1,24 +1,8 @@
 package org.wolkenproject.core.script.internal;
 
 import org.wolkenproject.exceptions.MochaException;
-import org.wolkenproject.utils.Utils;
-
-import java.math.BigInteger;
 
 public class MochaNumber extends MochaObject {
-    private BigInteger value;
-
-    public MochaNumber(long value, boolean signed) {
-        this.value = new BigInteger(signed ? 0 : 1, Utils.takeApartLong(value));
-    }
-
-    public MochaNumber(BigInteger integer) throws MochaException {
-        this.value = integer;
-        if (integer.bitLength() > 256) {
-            throw new MochaException("the maximum size of int is 256 bits.");
-        }
-    }
-
     @Override
     public MochaObject add(MochaObject other) throws MochaException {
         if (other instanceof MochaNumber) {
