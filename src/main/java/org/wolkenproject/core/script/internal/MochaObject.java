6a07c802e9221b37162ff2db0624490d76bee490
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 07:29:25 +0100

    Update MochaObject.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaObject.java
@@ -76,7 +76,8 @@ public class MochaObject {
     // negate (~x)
     public MochaObject negate() { return this; }
     // get the length of an array
-    public MochaObject length() { return new MochaNumber(members.length, false); }
+    public MochaObject length() { return null;//new MochaNumber(members.length, false);
+    }
     // get the shape of an array
     public MochaObject shape() { return this; }
     // reshape an array
