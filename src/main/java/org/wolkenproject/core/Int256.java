0828e0a97f904169dc3c254461185c418bc27741
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 07:19:47 +0100

    Update Int256.java

--- src/main/java/org/wolkenproject/core/Int256.java
+++ src/main/java/org/wolkenproject/core/Int256.java
@@ -30,4 +30,24 @@ public class Int256 {
 
         return new Int256(result, !(other.signed & signed));
     }
+
+    public Int256 sub(Int256 other) {
+        int carry       = 0;
+        int result[]    = new int[8];
+
+        for (int i = 0; i < 4; i ++) {
+            for (int b = 0; b < 32; b ++) {
+                int bit0    = Utils.getBit(data[i], b);
+                int bit1    = Utils.getBit(other.data[i], b);
+
+                int sum0    = (bit0 ^ bit1);
+                int sum1    = sum0 ^ carry;
+                carry       = ((bit0 & bit1) & sum1) | carry;
+
+                result[i]   = Utils.setBit(result[i], b, sum1);
+            }
+        }
+
+        return new Int256(result, !(other.signed & signed));
+    }
 }
