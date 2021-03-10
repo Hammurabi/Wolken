142c13514c54e7ba5da7bf0e044bb462d9b0ab1f
Hammurabi <abdullahafadhil@gmail.com>
2021-03-10 06:52:27 +0100

    Update MochaNumber.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaNumber.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaNumber.java
@@ -7,6 +7,14 @@ import java.math.BigInteger;
 public class MochaNumber extends MochaObject {
     protected BigInteger value;
 
+    public MochaNumber(BigInteger integer) throws MochaException {
+        this.value = integer;
+
+        if (integer.bitLength() > 256) {
+            throw new MochaException("the maximum size of Number is 256 bits.");
+        }
+    }
+
     @Override
     public MochaObject add(MochaObject other) throws MochaException {
         if (other instanceof MochaNumber) {
@@ -18,7 +26,6 @@ public class MochaNumber extends MochaObject {
     @Override
     public MochaObject sub(MochaObject other) throws MochaException {
         if (other instanceof MochaNumber) {
-            return new MochaNumber(value.subtract(((MochaNumber) other).value));
         }
 
         throw new MochaException("cannot perform 'sub' on object of type 'Number' and '"+other.getClass().getName()+"' ");
@@ -27,7 +34,6 @@ public class MochaNumber extends MochaObject {
     @Override
     public MochaObject mul(MochaObject other) throws MochaException {
         if (other instanceof MochaNumber) {
-            return new MochaNumber(value.multiply(((MochaNumber) other).value));
         }
 
         throw new MochaException("cannot perform 'mul' on object of type 'Number' and '"+other.getClass().getName()+"' ");
@@ -36,7 +42,6 @@ public class MochaNumber extends MochaObject {
     @Override
     public MochaObject div(MochaObject other, boolean sign) throws MochaException {
         if (other instanceof MochaNumber) {
-            return new MochaNumber(value.divide(((MochaNumber) other).value));
         }
 
         throw new MochaException("cannot perform 'div' on object of type 'Number' and '"+other.getClass().getName()+"' ");
@@ -45,7 +50,6 @@ public class MochaNumber extends MochaObject {
     @Override
     public MochaObject mod(MochaObject other, boolean sign) throws MochaException {
         if (other instanceof MochaNumber) {
-            return new MochaNumber(value.mod(((MochaNumber) other).value));
         }
 
         throw new MochaException("cannot perform 'mod' on object of type 'Number' and '"+other.getClass().getName()+"' ");
