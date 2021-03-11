39a741d12504189c357d2b4d99d88bbd49b4465e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-11 09:01:09 +0100

    Update MochaCallable.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaCallable.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaCallable.java
@@ -1,7 +1,8 @@
 package org.wolkenproject.core.script.internal;
 
+import org.wolkenproject.core.script.Contract;
 import org.wolkenproject.core.script.VirtualProcess;
 
 public interface MochaCallable {
-    MochaObject call(VirtualProcess process);
+    MochaObject call(Contract contract, );
 }
