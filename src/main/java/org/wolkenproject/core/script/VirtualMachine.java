681eea61b70d7af741dedad6f0f9929815092fa4
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 03:24:11 +0100

    Update VirtualMachine.java

--- src/main/java/org/wolkenproject/core/script/VirtualMachine.java
+++ src/main/java/org/wolkenproject/core/script/VirtualMachine.java
@@ -2,9 +2,6 @@ package org.wolkenproject.core.script;
 
 public abstract class VirtualMachine {
     private OpcodeRegister      opcodeRegister;
-
-    public abstract VirtualProcess createProcess(Script script);
-
     public OpcodeRegister getOpcodeRegister() {
         return opcodeRegister;
     }
