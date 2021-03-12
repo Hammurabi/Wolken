59bcc0e25819771ca2847c28a986112256405344
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 13:05:13 +0100

    Create OpIConst_4bits.java

---
+++ src/main/java/org/wolkenproject/core/script/opcodes/OpIConst_4bits.java
@@ -0,0 +1,18 @@
+package org.wolkenproject.core.script.opcodes;
+
+import org.wolkenproject.core.script.Contract;
+import org.wolkenproject.core.script.Opcode;
+import org.wolkenproject.core.script.Scope;
+import org.wolkenproject.core.script.internal.MochaNumber;
+import org.wolkenproject.core.script.internal.MochaObject;
+import org.wolkenproject.exceptions.MochaException;
+import org.wolkenproject.utils.BitInputStream;
+import org.wolkenproject.utils.BitOutputStream;
+import org.wolkenproject.utils.Utils;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Stack;
+
+public class OpIConst_4bits extends Opcode {
+}
