ccf1f380ada6b56558c1382f5bd4d03452a7d29c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 03:24:35 +0100

    Delete MochaString.java

--- src/main/java/org/wolkenproject/core/script/MochaString.java
+++
@@ -1,9 +0,0 @@
-package org.wolkenproject.core.script;
-
-import org.wolkenproject.exceptions.UndefClassException;
-
-public class MochaString extends MochaObject {
-    public MochaString(VirtualProcess virtualProcess) throws UndefClassException {
-        super(virtualProcess);
-    }
-}
