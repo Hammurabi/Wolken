40e4fe2371435a1a85a13b743095c8e44c34cc41
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 05:55:30 +0100

    Update Contract.java

--- src/main/java/org/wolkenproject/core/script/Contract.java
+++ src/main/java/org/wolkenproject/core/script/Contract.java
@@ -2,7 +2,7 @@ package org.wolkenproject.core.script;
 
 import org.wolkenproject.core.script.internal.MochaObject;
 
-public class Contract {
+public class Contract extends MochaObject {
     public void call(int functionAddress, MochaObject...arguments) {
     }
 }
