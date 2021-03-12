e7afd1221e6a3b8709f98fd09d129fdae23a86b1
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 03:19:36 +0100

    Update OpIConst.java

--- src/main/java/org/wolkenproject/core/script/opcodes/OpIConst.java
+++ src/main/java/org/wolkenproject/core/script/opcodes/OpIConst.java
@@ -1,14 +1,18 @@
 package org.wolkenproject.core.script.opcodes;
 
+import org.wolkenproject.core.script.Contract;
 import org.wolkenproject.core.script.Opcode;
 import org.wolkenproject.core.script.VirtualProcess;
 import org.wolkenproject.core.script.MochaArray;
+import org.wolkenproject.core.script.internal.MochaNumber;
+import org.wolkenproject.core.script.internal.MochaObject;
 import org.wolkenproject.exceptions.MochaException;
 import org.wolkenproject.utils.BitInputStream;
 import org.wolkenproject.utils.BitOutputStream;
 import org.wolkenproject.utils.Utils;
 
 import java.io.IOException;
+import java.util.Stack;
 
 public class OpIConst extends Opcode {
     private long integer;
@@ -23,9 +27,8 @@ public class OpIConst extends Opcode {
     }
 
     @Override
-    public void execute(VirtualProcess virtualProcess) throws MochaException {
-        MochaArray array = new MochaArray(virtualProcess, virtualProcess.getClassProvider().getArrayMochaClass());
-        virtualProcess.getMemoryModule().getStack().push(array);
+    public void execute(Contract contract, Stack<MochaObject> stack) throws MochaException {
+        stack.push(new MochaNumber(integer));
     }
 
     private static boolean isSigned(long integer) {
@@ -41,20 +44,34 @@ public class OpIConst extends Opcode {
 
         outputStream.write(isSigned ? 1 : 0);
 
-        if (integer >= 0 && integer <= 255) {
+        if (integer <= 15) {
             type = 0;
+            bitLength = 4;
+        }
+        else if (integer <= 255) {
+            type = 1;
             bitLength = 8;
         }
-        else if (integer >= 0 && integer <= 65535) {
-            type = 1;
+        else if (integer <= 4096) {
+            type = 2;
+            bitLength = 12;
+        }
+        else if (integer <= 65535) {
+            type = 3;
             bitLength = 16;
         }
         else if (integer >= 0 && integer <= 4294967295L) {
-            type = 2;
+            type = 4;
             bitLength = 32;
         } else if (integer >= 0 && integer <= 4096) {
-            type = 3;
+            type = 5;
             bitLength = 64;
+        } else if (integer >= 0 && integer <= 4096) {
+            type = 6;
+            bitLength = 128;
+        } else if (integer >= 0 && integer <= 4096) {
+            type = 7;
+            bitLength = 256;
         }
 
         outputStream.write(Utils.getBit(type, 0));
