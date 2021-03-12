31fe9a75d27ac6193d699189265279367223d1c5
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 03:26:38 +0100

    Update Scope.java

--- src/main/java/org/wolkenproject/core/script/Scope.java
+++ src/main/java/org/wolkenproject/core/script/Scope.java
@@ -3,12 +3,18 @@ package org.wolkenproject.core.script;
 import org.wolkenproject.core.script.internal.MochaObject;
 
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Scope {
     // the contract in which the point of entry exists
     private Contract            contract;
     // the stack
     private Stack<MochaObject>  stack;
+    // tell all subprocesses to continue running
+    private AtomicBoolean       keepRunning;
+    // an interrupt signal (if any)
+    private AtomicInteger       interruptSignal;
 
     public Scope(VirtualMachine virtualMachine, Contract contract, Stack<MochaObject> stack) {
         this.contract   = contract;
@@ -21,5 +27,10 @@ public class Scope {
 
     public Stack<MochaObject> getStack() {
         return stack;
+    }
+
+    public void stopProcesses(int signal) {
+        keepRunning.set(false);
+        interruptSignal.set(signal);
     }
 }
