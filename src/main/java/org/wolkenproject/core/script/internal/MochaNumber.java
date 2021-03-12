7a7290ca3b069f2b368ef52fe5a000a008c8953c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 06:07:18 +0100

    Update MochaNumber.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaNumber.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaNumber.java
@@ -63,6 +63,18 @@ public class MochaNumber extends MochaObject {
         return new MochaNumber(result, nSign);
     }
 
+    public MochaObject do_div(MochaNumber other) throws MochaException {
+        BigInteger result   = value.divide(other.value);
+        boolean nSign       = sign || other.sign;
+
+        // overflow
+        if (value.signum() < 0 && !nSign) {
+            return new MochaNumber(MaxUnsignedInt, false);
+        }
+
+        return new MochaNumber(result, nSign);
+    }
+
     @Override
     public MochaObject add(MochaObject other) throws MochaException {
         if (other instanceof MochaNumber) {
