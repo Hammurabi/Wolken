729837f849d61f91bea72222d1dc3e748cb194ce
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 13:02:34 +0100

    Update OpIConst.java

--- src/main/java/org/wolkenproject/core/script/opcodes/OpIConst.java
+++ src/main/java/org/wolkenproject/core/script/opcodes/OpIConst.java
@@ -11,17 +11,18 @@ import org.wolkenproject.utils.BitOutputStream;
 import org.wolkenproject.utils.Utils;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Stack;
 
 public class OpIConst extends Opcode {
-    private long    integer;
-    private boolean signed;
+    private byte        integer[];
+    private boolean     signed;
 
     public OpIConst() {
-        this(0);
+        this(new byte[] { 0 });
     }
 
-    public OpIConst(long integer) {
+    public OpIConst(byte integer[]) {
         super("iconst", "push (u)int8(8-64) into the stack", "push [arg] [value]");
         this.integer = integer;
     }
@@ -31,54 +32,44 @@ public class OpIConst extends Opcode {
         scope.getStack().push(new MochaNumber(integer, signed));
     }
 
-    private static boolean isSigned(long integer) {
-        return integer < 0;
-    }
-
     @Override
     public void write(BitOutputStream outputStream) throws IOException {
-        // 2 bits to define the type ( 8bit, 16bit, 32bit, 64bit )
-        boolean isSigned = isSigned(integer);
+        // 3 bits to define the type ( 4bit, 8bit, 12bit, , 16bit, 32bit, 64bit )
         int type = 0;
-        int bitLength = 0;
+        BigInteger integer = new BigInteger(this.integer);
+        int bitLength = integer.bitLength();
 
-        outputStream.write(isSigned ? 1 : 0);
+        outputStream.write(signed ? 1 : 0);
 
-        if (integer <= 15) {
+        if (bitLength <= 4) {
             type = 0;
-            bitLength = 4;
         }
-        else if (integer <= 255) {
+        else if (bitLength <= 8) {
             type = 1;
-            bitLength = 8;
         }
-        else if (integer <= 4096) {
+        else if (bitLength <= 12) {
             type = 2;
-            bitLength = 12;
         }
-        else if (integer <= 65535) {
+        else if (bitLength <= 16) {
             type = 3;
-            bitLength = 16;
         }
-        else if (integer >= 0 && integer <= 4294967295L) {
+        else if (bitLength <= 32) {
             type = 4;
-            bitLength = 32;
-        } else if (integer >= 0 && integer <= 4096) {
+        }
+        else if (bitLength <= 64) {
             type = 5;
-            bitLength = 64;
-        } else if (integer >= 0 && integer <= 4096) {
+        }
+        else if (bitLength <= 128) {
             type = 6;
-            bitLength = 128;
-        } else if (integer >= 0 && integer <= 4096) {
+        } else if (bitLength <= 256) {
             type = 7;
-            bitLength = 256;
         }
 
         outputStream.write(Utils.getBit(type, 0));
         outputStream.write(Utils.getBit(type, 1));
 
         for (int i = 0; i < bitLength; i ++) {
-            outputStream.write(Utils.getBit(integer, i));
+            outputStream.write(integer.testBit(i) ? 1 : 0);
         }
     }
 
