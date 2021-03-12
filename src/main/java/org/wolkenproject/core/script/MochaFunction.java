de11c575167f04e9352188eab88998a12bb4c7e2
Hammurabi <abdullahafadhil@gmail.com>
2021-03-12 03:21:27 +0100

    Delete MochaFunction.java

--- src/main/java/org/wolkenproject/core/script/MochaFunction.java
+++
@@ -1,7 +0,0 @@
-package org.wolkenproject.core.script;
-
-import org.wolkenproject.exceptions.MochaException;
-
-public interface MochaFunction {
-    public MochaObject execute(VirtualProcess virtualProcess) throws MochaException;
-}
