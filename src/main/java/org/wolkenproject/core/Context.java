513782da192210ec0926a7e17554fd52236ffa3c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-13 18:52:59 +0100

    Update Context.java

--- src/main/java/org/wolkenproject/core/Context.java
+++ src/main/java/org/wolkenproject/core/Context.java
@@ -119,7 +119,7 @@ public class Context {
 
         virtualMachine.registerOp("aconst200", "push an address of size '200'.", 25, scope -> { throw new MochaException("address is not supported at the moment."); });
         virtualMachine.registerOp("aconst256", "push a hash of size '256'.", 32, scope -> { throw new MochaException("hash256 is not supported at the moment."); });
-        virtualMachine.registerOp("ecpubk", "push a public key of size '264' (compressed).", 33, scope -> { throw new MochaException("pubkey is not supported at the moment."); });
+        virtualMachine.registerOp("ecpub", "push a public key of size '264' (compressed).", 33, scope -> { throw new MochaException("pubkey is not supported at the moment."); });
         virtualMachine.registerOp("ecsig", "push a signature of size '~'.", 73, scope -> { throw new MochaException("sig is not supported at the moment."); });
 
         virtualMachine.registerOp("verify", "throws an 'InvalidTransactionException' if the top stack item is not true.", Scope::verify);
