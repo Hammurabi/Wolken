b825e0fc684ea50715bf1cdf6120c1c41ec696d3
Hammurabi <abdullahafadhil@gmail.com>
2021-03-13 17:30:03 +0100

    Create MochaStack.java

---
+++ src/main/java/org/wolkenproject/core/script/MochaStack.java
@@ -0,0 +1,45 @@
+package org.wolkenproject.core.script;
+
+import java.util.Stack;
+
+public class MochaStack<T> {
+    private Stack<T> stack;
+
+    public MochaStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(T element) {
+        stack.push(element);
+    }
+
+    public T peek() {
+        return stack.peek();
+    }
+
+    public T pop() {
+        return stack.pop();
+    }
+
+    public void dup() {
+        stack.push(stack.peek());
+    }
+
+    public void dup(int element) {
+        stack.push(stack.get(stack.size() - element));
+    }
+
+    public void rot(int a, int b) {
+        T temp = stack.get(a);
+        stack.set(a, stack.get(b));
+        stack.set(b, temp);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public int getSize() {
+        return stack.size();
+    }
+}
