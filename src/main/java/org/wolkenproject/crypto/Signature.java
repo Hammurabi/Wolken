25c3b56e30148a75cd053c110ea05bfe6b3b27cf
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 11:51:07 +0100

    Update Signature.java

--- src/main/java/org/wolkenproject/crypto/Signature.java
+++ src/main/java/org/wolkenproject/crypto/Signature.java
@@ -1,12 +1,13 @@
 package org.wolkenproject.crypto;
 
+import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
 
 import java.math.BigInteger;
 
 public abstract class Signature extends SerializableI {
     public abstract boolean checkSignature(byte originalMessage[]);
-    public BigInteger recover() {
+    public BigInteger recover(byte originalMessage[]) throws WolkenException {
         return null;
     }
 }
