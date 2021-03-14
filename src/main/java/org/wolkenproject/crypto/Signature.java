af62fce3ab5f17fa33060f88986d7cbbe3541cb9
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:31:57 +0100

    Update Signature.java

--- src/main/java/org/wolkenproject/crypto/Signature.java
+++ src/main/java/org/wolkenproject/crypto/Signature.java
@@ -1,12 +1,6 @@
 package org.wolkenproject.crypto;
 
-import org.wolkenproject.core.Context;
-import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public abstract class Signature extends SerializableI {
 }
