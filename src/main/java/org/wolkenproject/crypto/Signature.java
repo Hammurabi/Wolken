ccad225ce2881ca40c30ba463cab89942a512a7b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:23:50 +0100

    Create Signature.java

---
+++ src/main/java/org/wolkenproject/crypto/Signature.java
@@ -0,0 +1,25 @@
+package org.wolkenproject.crypto;
+
+public class Signature {
+    private final byte v[];
+    private final byte r[];
+    private final byte s[];
+
+    public Signature(byte[] v, byte[] r, byte[] s) {
+        this.v = v;
+        this.r = r;
+        this.s = s;
+    }
+
+    public byte[] getV() {
+        return v;
+    }
+
+    public byte[] getR() {
+        return r;
+    }
+
+    public byte[] getS() {
+        return s;
+    }
+}
