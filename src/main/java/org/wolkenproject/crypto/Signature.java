06f8735d278e373220e64948e37e972b6ee0a24f
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:31:50 +0100

    Update Signature.java

--- src/main/java/org/wolkenproject/crypto/Signature.java
+++ src/main/java/org/wolkenproject/crypto/Signature.java
@@ -8,22 +8,5 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class Signature extends SerializableI {
-    @Override
-    public void write(OutputStream stream) throws IOException, WolkenException {
-    }
-
-    @Override
-    public void read(InputStream stream) throws IOException, WolkenException {
-    }
-
-    @Override
-    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
-        return null;
-    }
-
-    @Override
-    public int getSerialNumber() {
-        return Context.getInstance().getSerialFactory().getSerialNumber(Signature.class);
-    }
+public abstract class Signature extends SerializableI {
 }
