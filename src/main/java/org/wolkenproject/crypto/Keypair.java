e5aae8c56a00dc44bb08171fad0218943b5a4f05
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:29:28 +0100

    Update Keypair.java

--- src/main/java/org/wolkenproject/crypto/Keypair.java
+++ src/main/java/org/wolkenproject/crypto/Keypair.java
@@ -8,5 +8,21 @@ import org.bouncycastle.crypto.signers.HMacDSAKCalculator;
 import java.math.BigInteger;
 
 public abstract class Keypair {
+    private final BigInteger privateKey;
+    private final BigInteger publicKey;
+
+    public Keypair(BigInteger priv, BigInteger pubk) {
+        this.privateKey = priv;
+        this.publicKey  = pubk;
+    }
+
+    public BigInteger getPrivateKey() {
+        return privateKey;
+    }
+
+    public BigInteger getPublicKey() {
+        return publicKey;
+    }
+
     public abstract Signature sign(byte message[]);
 }
