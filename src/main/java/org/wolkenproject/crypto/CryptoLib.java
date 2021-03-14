37d90bb83edff30c22060a6c5269431cc97a0b19
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:51:39 +0100

    Update CryptoLib.java

--- src/main/java/org/wolkenproject/crypto/CryptoLib.java
+++ src/main/java/org/wolkenproject/crypto/CryptoLib.java
@@ -2,6 +2,7 @@ package org.wolkenproject.crypto;
 
 import org.bouncycastle.asn1.sec.SECNamedCurves;
 import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.asn1.x9.X9IntegerConverter;
 import org.bouncycastle.crypto.ec.CustomNamedCurves;
 import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
@@ -11,13 +12,18 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jce.spec.ECParameterSpec;
 import org.bouncycastle.jce.spec.ECPrivateKeySpec;
 import org.bouncycastle.jce.spec.ECPublicKeySpec;
+import org.bouncycastle.math.ec.ECAlgorithms;
 import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.math.ec.FixedPointCombMultiplier;
+import org.bouncycastle.math.ec.custom.sec.SecP256K1Curve;
+import org.wolkenproject.crypto.ec.ECSig;
 import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.utils.Assertions;
 
 import java.math.BigInteger;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 
 public class CryptoLib {
     private static final CryptoLib secureLib = new CryptoLib();
@@ -34,6 +40,72 @@ public class CryptoLib {
         PARAMS = CustomNamedCurves.getByName("secp256k1");//SECNamedCurves.getByName("secp256k1");
         CURVE = new ECDomainParameters(PARAMS.getCurve(), PARAMS.getG(), PARAMS.getN(), PARAMS.getH());
         HALF_CURVE_ORDER = PARAMS.getN().shiftRight(1);
+    }
+
+    public static BigInteger recoverFromSignature(int recId, ECSig sig, byte[] message) throws WolkenException {
+        Assertions.assertTrue(recId >= 0, "recId must be positive");
+        Assertions.assertTrue(sig.getR().signum() >= 0, "r must be positive");
+        Assertions.assertTrue(sig.getS().signum() >= 0, "s must be positive");
+        Assertions.assertTrue(message != null, "message cannot be null");
+
+        // 1.0 For j from 0 to h   (h == recId here and the loop is outside this function)
+        //   1.1 Let x = r + jn
+        BigInteger n = CURVE.getN(); // Curve order.
+        BigInteger i = BigInteger.valueOf((long) recId / 2);
+        BigInteger x = sig.getR().add(i.multiply(n));
+        //   1.2. Convert the integer x to an octet string X of length mlen using the conversion
+        //        routine specified in Section 2.3.7, where mlen = ⌈(log2 p)/8⌉ or mlen = ⌈m/8⌉.
+        //   1.3. Convert the octet string (16 set binary digits)||X to an elliptic curve point R
+        //        using the conversion routine specified in Section 2.3.4. If this conversion
+        //        routine outputs "invalid", then do another iteration of Step 1.
+        //
+        // More concisely, what these points mean is to use X as a compressed public key.
+        BigInteger prime = SecP256K1Curve.q;
+        if (x.compareTo(prime) >= 0) {
+            // Cannot have point co-ordinates larger than this as everything takes place modulo Q.
+            return null;
+        }
+        // Compressed keys require you to know an extra bit of data about the y-coord as there are
+        // two possibilities. So it's encoded in the recId.
+        ECPoint R = decompressKey(x, (recId & 1) == 1);
+        //   1.4. If nR != point at infinity, then do another iteration of Step 1 (callers
+        //        responsibility).
+        if (!R.multiply(n).isInfinity()) {
+            return null;
+        }
+        //   1.5. Compute e from M using Steps 2 and 3 of ECDSA signature verification.
+        BigInteger e = new BigInteger(1, message);
+        //   1.6. For k from 1 to 2 do the following.   (loop is outside this function via
+        //        iterating recId)
+        //   1.6.1. Compute a candidate public key as:
+        //               Q = mi(r) * (sR - eG)
+        //
+        // Where mi(x) is the modular multiplicative inverse. We transform this into the following:
+        //               Q = (mi(r) * s ** R) + (mi(r) * -e ** G)
+        // Where -e is the modular additive inverse of e, that is z such that z + e = 0 (mod n).
+        // In the above equation ** is point multiplication and + is point addition (the EC group
+        // operator).
+        //
+        // We can find the additive inverse by subtracting e from zero then taking the mod. For
+        // example the additive inverse of 3 modulo 11 is 8 because 3 + 8 mod 11 = 0, and
+        // -3 mod 11 = 8.
+        BigInteger eInv = BigInteger.ZERO.subtract(e).mod(n);
+        BigInteger rInv = sig.getR().modInverse(n);
+        BigInteger srInv = rInv.multiply(sig.getS()).mod(n);
+        BigInteger eInvrInv = rInv.multiply(eInv).mod(n);
+        ECPoint q = ECAlgorithms.sumOfTwoMultiplies(CURVE.getG(), eInvrInv, R, srInv);
+
+        byte[] qBytes = q.getEncoded(false);
+        // We remove the prefix
+        return new BigInteger(1, Arrays.copyOfRange(qBytes, 1, qBytes.length));
+    }
+
+    /** Decompress a compressed public key (x co-ord and low-bit of y-coord). */
+    private static ECPoint decompressKey(BigInteger xBN, boolean yBit) {
+        X9IntegerConverter x9 = new X9IntegerConverter();
+        byte[] compEnc = x9.integerToBytes(xBN, 1 + x9.getByteLength(CURVE.getCurve()));
+        compEnc[0] = (byte) (yBit ? 0x03 : 0x02);
+        return CURVE.getCurve().decodePoint(compEnc);
     }
 
     /**
