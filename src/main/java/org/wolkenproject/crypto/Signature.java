175cd3313493324beb91dd656f04e0b3f7448dd9
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:34:56 +0100

    Update Signature.java

--- src/main/java/org/wolkenproject/crypto/Signature.java
+++ src/main/java/org/wolkenproject/crypto/Signature.java
@@ -3,4 +3,5 @@ package org.wolkenproject.crypto;
 import org.wolkenproject.serialization.SerializableI;
 
 public abstract class Signature extends SerializableI {
+    public abstract boolean checkSignature(byte originalMessage[]);
 }
