9b5e7987673e8e9bebc5f2f86f63113e16efcd46
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 11:51:03 +0100

    Update RecoverableSignature.java

--- src/main/java/org/wolkenproject/crypto/ec/RecoverableSignature.java
+++ src/main/java/org/wolkenproject/crypto/ec/RecoverableSignature.java
@@ -1,13 +1,17 @@
 package org.wolkenproject.crypto.ec;
 
 import org.wolkenproject.core.Context;
+import org.wolkenproject.crypto.CryptoLib;
 import org.wolkenproject.crypto.Signature;
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
+import org.wolkenproject.utils.Assertions;
+import org.wolkenproject.utils.HashUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
 
 public class RecoverableSignature extends Signature {
     private final byte v;
@@ -57,5 +61,26 @@ public class RecoverableSignature extends Signature {
     @Override
     public boolean checkSignature(byte[] originalMessage) {
         return false;
+    }
+
+    @Override
+    public BigInteger recover(byte originalMessage[]) throws WolkenException {
+        Assertions.assertTrue(r != null && r.length == 32, "r must be 32 bytes in length");
+        Assertions.assertTrue(s != null && s.length == 32, "s must be 32 bytes in length");
+
+        int header = v & 0xFF;
+
+        if (header < 27 || header > 34) {
+            throw new WolkenException("header byte out of range: " + header);
+        }
+
+        ECSig sig = new ECSig(new BigInteger(1, r), new BigInteger(1, s));
+        BigInteger result = CryptoLib.recoverFromSignature(v - 27, sig, HashUtil.sha256d(originalMessage));
+
+        if (result == null) {
+            throw new WolkenException("could not recover public key.");
+        }
+
+        return result;
     }
 }
