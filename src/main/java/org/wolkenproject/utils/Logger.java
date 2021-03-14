fbeb6cdbe20b6932250f48b5c4d42bdcb647a2fb
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 08:52:41 +0100

    Update Logger.java

--- src/main/java/org/wolkenproject/utils/Logger.java
+++ src/main/java/org/wolkenproject/utils/Logger.java
@@ -1,12 +1,24 @@
 package org.wolkenproject.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Logger {
     public static void faterr(String msg)
     {
         System.err.println(msg);
     }
 
-    public static void alert(String msg) {
+    public static void alert(String msg, Object ...arguments) {
+        Pattern pattern = Pattern.compile("\\$\\{[A-z]\\}");
+        Matcher matcher = pattern.matcher(msg);
+        int index       = 0;
+
+        while (matcher.find()) {
+            msg = matcher.replaceFirst(Utils.toString(arguments[index ++]));
+            matcher = pattern.matcher(msg);
+        }
+
         System.out.println(msg);
     }
 }
