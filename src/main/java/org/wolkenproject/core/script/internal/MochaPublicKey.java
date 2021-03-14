7faa48784304a7a8c17d754760c0145674873ed0
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 13:02:06 +0100

    Create MochaPublicKey.java

---
+++ src/main/java/org/wolkenproject/core/script/internal/MochaPublicKey.java
@@ -0,0 +1,28 @@
+package org.wolkenproject.core.script.internal;
+
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
+import org.wolkenproject.core.script.MochaBool;
+import org.wolkenproject.crypto.CryptoLib;
+import org.wolkenproject.exceptions.WolkenException;
+
+public class MochaPublicKey extends MochaObject {
+    private BCECPublicKey   publicKey;
+    private boolean         compressed;
+
+    public MochaPublicKey(byte key[]) {
+        this.publicKey = (BCECPublicKey) CryptoLib.getInstance().generatePublicKey(CryptoLib.ec_point_point2oct(key, key[0] == 0x3), "secp256k1");
+        this.compressed= key[0] == 0x3;
+    }
+
+    public MochaObject checkSignature(MochaCryptoSignature signature, byte signatureData[]) {
+        try {
+            return new MochaBool(CryptoLib.getInstance().verifySignature(publicKey, signatureData, signature.getSignature()));
+        } catch (WolkenException e) {
+            return new MochaBool(false);
+        }
+    }
+
+    public boolean isCompressed() {
+        return compressed;
+    }
+}
