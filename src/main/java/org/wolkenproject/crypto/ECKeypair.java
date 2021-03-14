661af7386ea5ee27fb96e766078754f0b4bb7dc0
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:30:07 +0100

    Update ECKeypair.java

--- src/main/java/org/wolkenproject/crypto/ECKeypair.java
+++ src/main/java/org/wolkenproject/crypto/ECKeypair.java
@@ -1,10 +1,26 @@
 package org.wolkenproject.crypto;
 
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
+import org.bouncycastle.crypto.signers.ECDSASigner;
+import org.bouncycastle.crypto.signers.HMacDSAKCalculator;
+
 import java.math.BigInteger;
 
 public class ECKeypair extends Keypair {
     public ECKeypair(BigInteger priv, BigInteger pubk) {
         super(priv, pubk);
+    }
+
+    public ECSig ellipticCurveSign(byte message[]) {
+        ECDSASigner signer = new ECDSASigner(new HMacDSAKCalculator(new SHA256Digest()));
+
+        ECPrivateKeyParameters privateKeyParameters = new ECPrivateKeyParameters(getPrivateKey(), CryptoLib.CURVE);
+        signer.init(true, privateKeyParameters);
+
+        BigInteger components[] = signer.generateSignature(message);
+
+        return new ECSig(components[0], components[1]).toCanonicalised();
     }
 
     @Override
