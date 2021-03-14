7c7ff7cf644aa170ddbdafb109f47ef747a90821
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 09:49:22 +0100

    Update Context.java

--- src/main/java/org/wolkenproject/core/Context.java
+++ src/main/java/org/wolkenproject/core/Context.java
@@ -2,6 +2,7 @@ package org.wolkenproject.core;
 
 import org.wolkenproject.core.script.*;
 import org.wolkenproject.core.script.internal.ByteArray;
+import org.wolkenproject.core.script.internal.MochaECPubKey;
 import org.wolkenproject.core.script.internal.MochaNumber;
 import org.wolkenproject.core.script.internal.MochaObject;
 import org.wolkenproject.core.transactions.Transaction;
@@ -130,7 +131,7 @@ public class Context {
 
         opcodeRegister.registerOp("aconst200", "push an address of size '200'.", 25, scope -> { throw new MochaException("address is not supported at the moment."); });
         opcodeRegister.registerOp("aconst256", "push a hash of size '256'.", 32, scope -> { throw new MochaException("hash256 is not supported at the moment."); });
-        opcodeRegister.registerOp("ecpub", "push a public key of size '264' (compressed).", 33, scope -> { throw new MochaException("pubkey is not supported at the moment."); });
+        opcodeRegister.registerOp("ecpub", "push a public key of size '264' (compressed).", 33, scope -> scope.getStack().push(new MochaECPubKey(scope.getProgramCounter().next(33))));
         opcodeRegister.registerOp("ecsig", "push a signature of size '~'.", 73, scope -> { throw new MochaException("sig is not supported at the moment."); });
 
         opcodeRegister.registerOp("verify", "throws an 'InvalidTransactionException' if the top stack item is not true.", Scope::verify);
