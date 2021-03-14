4484cb9fac3d29d237bc47f6888d5db364399606
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 20:24:47 +0100

    Update Utils.java

--- src/main/java/org/wolkenproject/utils/Utils.java
+++ src/main/java/org/wolkenproject/utils/Utils.java
@@ -211,6 +211,17 @@ public class Utils {
                 (byte) ((integer) & 0xFF)};
     }
 
+    public static byte[] takeApartInt48(long integer)
+    {
+        return new byte[] {
+                (byte) ((integer >> 40) & 0xFF),
+                (byte) ((integer >> 32) & 0xFF),
+                (byte) ((integer >> 24) & 0xFF),
+                (byte) ((integer >> 16) & 0xFF),
+                (byte) ((integer >>  8) & 0xFF),
+                (byte) ((integer) & 0xFF)};
+    }
+
     public static byte[] fromSet(Set<byte[]> hashes) {
         byte bytes[] = new byte[0];
 
