b645b28cb75d4495057253882399cb716d415c84
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:31:01 +0100

    Update Signature.java

--- src/main/java/org/wolkenproject/crypto/Signature.java
+++ src/main/java/org/wolkenproject/crypto/Signature.java
@@ -1,25 +1,6 @@
 package org.wolkenproject.crypto;
 
-public class Signature {
-    private final byte v[];
-    private final byte r[];
-    private final byte s[];
+import org.wolkenproject.serialization.SerializableI;
 
-    public Signature(byte[] v, byte[] r, byte[] s) {
-        this.v = v;
-        this.r = r;
-        this.s = s;
-    }
-
-    public byte[] getV() {
-        return v;
-    }
-
-    public byte[] getR() {
-        return r;
-    }
-
-    public byte[] getS() {
-        return s;
-    }
+public class Signature implements SerializableI {
 }
