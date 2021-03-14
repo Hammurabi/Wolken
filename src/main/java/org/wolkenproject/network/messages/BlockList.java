0807608433a4556fde30a93d9ec8c4d62a6c5f5c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 19:30:43 +0100

    Update BlockList.java

--- src/main/java/org/wolkenproject/network/messages/BlockList.java
+++ src/main/java/org/wolkenproject/network/messages/BlockList.java
@@ -2,9 +2,7 @@ package org.wolkenproject.network.messages;
 
 import org.wolkenproject.core.BlockIndex;
 import org.wolkenproject.core.Context;
-import org.wolkenproject.core.TransactionI;
 import org.wolkenproject.exceptions.WolkenException;
-import org.wolkenproject.network.ResponseMetadata;
 import org.wolkenproject.serialization.SerializableI;
 import org.wolkenproject.utils.Utils;
 
