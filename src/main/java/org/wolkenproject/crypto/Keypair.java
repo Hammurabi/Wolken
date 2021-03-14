bc44cfbb862fef6900bd6ab53622c9a44e59cc1f
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 10:23:47 +0100

    Update Keypair.java

--- src/main/java/org/wolkenproject/crypto/Keypair.java
+++ src/main/java/org/wolkenproject/crypto/Keypair.java
@@ -4,7 +4,6 @@ import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
 import org.bouncycastle.crypto.signers.ECDSASigner;
 import org.bouncycastle.crypto.signers.HMacDSAKCalculator;
-import org.wolkenproject.encoders.ECSig;
 
 import java.math.BigInteger;
 
@@ -17,15 +16,7 @@ public class Keypair {
         this.publicKey  = pubk;
     }
 
-    public ECSig sign(byte message[]) {
-        ECDSASigner signer = new ECDSASigner(new HMacDSAKCalculator(new SHA256Digest()));
-
-        ECPrivateKeyParameters privateKeyParameters = new ECPrivateKeyParameters(getPrivateKey(), CryptoLib.CURVE);
-        signer.init(true, privateKeyParameters);
-
-        BigInteger components[] = signer.generateSignature(message);
-
-        return new ECSig(components[0], components[1]).toCanonicalised();;
+    public Signature sign(byte message[]) {
     }
 
     public BigInteger getPrivateKey() {
