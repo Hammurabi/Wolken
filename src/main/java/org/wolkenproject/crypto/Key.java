5e0b3b7c138f133452ded1e9db7f49f12d3895c5
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 12:21:29 +0100

    Update Key.java

--- src/main/java/org/wolkenproject/crypto/Key.java
+++ src/main/java/org/wolkenproject/crypto/Key.java
@@ -1,4 +1,10 @@
 package org.wolkenproject.crypto;
 
 public abstract class Key {
+    // should return only the significant key bytes
+    public abstract byte[] getRaw();
+    // should return a neatly packed version of this key
+    public abstract byte[] getEncoded();
+    // should return a compressed version of this key
+    public abstract Key getCompressed();
 }
