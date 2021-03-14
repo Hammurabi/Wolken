307540abf7f6dfec25ce1e6408ca87a1633ecac4
Hammurabi <abdullahafadhil@gmail.com>
2021-03-14 09:47:10 +0100

    Update MochaECSig.java

--- src/main/java/org/wolkenproject/core/script/internal/MochaECSig.java
+++ src/main/java/org/wolkenproject/core/script/internal/MochaECSig.java
@@ -1,7 +1,13 @@
 package org.wolkenproject.core.script.internal;
 
 public class MochaECSig extends MochaObject {
+    private byte signature[];
+
+    public MochaECSig(byte signature[]) {
+        this.signature = signature;
+    }
+
     public byte[] getSignature() {
-        return new byte[0];
+        return signature;
     }
 }
