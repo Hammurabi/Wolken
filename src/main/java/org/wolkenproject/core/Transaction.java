685a72a963fbf8e4f5cb4bd73d897a2028a60d25
Hammurabi <abdullahafadhil@gmail.com>
2021-03-15 08:12:15 +0100

    Update Transaction.java

--- src/main/java/org/wolkenproject/core/Transaction.java
+++ src/main/java/org/wolkenproject/core/Transaction.java
@@ -80,7 +80,7 @@ public class Transaction extends SerializableI implements Comparable<Transaction
 
     @Override
     public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
-        return null;
+        return new Transaction();
     }
 
     @Override
