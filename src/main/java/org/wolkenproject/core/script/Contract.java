2944a8e7ba03db2ec8acc234af5681bad7e4aa83
Hammurabi <Riverssen@gmail.com>
2021-03-16 05:48:39 +0100

    Update Contract.java

--- src/main/java/org/wolkenproject/core/script/Contract.java
+++ src/main/java/org/wolkenproject/core/script/Contract.java
@@ -1,8 +1,25 @@
 package org.wolkenproject.core.script;
 
+import org.wolkenproject.core.Transaction;
 import org.wolkenproject.core.script.internal.MochaObject;
 
 public class Contract extends MochaObject {
     public void call(int functionAddress, MochaObject...arguments) {
+    }
+
+    // create contract from a transaction payload
+    public static final Contract create(Transaction transaction, ProgramCounter programCounter) {
+        // create the contract object
+        Contract contract = new Contract();
+
+        // create the stack and populate it
+        MochaStack<MochaObject> stack = new MochaStack<>();
+        stack.push(contract);
+        stack.push(transaction.getSender());
+
+        // create a scope
+        Scope scope = new Scope(transaction, contract, stack, programCounter);
+
+        return contract;
     }
 }
