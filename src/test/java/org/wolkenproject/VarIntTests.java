9eee061558cae859bde9b9ce17080c0101ed3164
Hammurabi <abdullahafadhil@gmail.com>
2021-03-16 22:35:18 +0100

    Update VarIntTests.java

--- src/test/java/org/wolkenproject/VarIntTests.java
+++ src/test/java/org/wolkenproject/VarIntTests.java
@@ -57,4 +57,52 @@ public class VarIntTests {
             Assertions.assertTrue(Utils.isEmpty(empty), "array should consist of zeros.");
         }
     }
+
+    @Test
+    public void testReadWriteLossy64() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        int ints[] = new int[] {
+                0, 1, 2, 5, 10, 20, 22, 30, 32, 40, 60, 120, 12000, 24000, 124000, 1000000, 1000000000
+        };
+
+        byte empty[] = new byte[12];
+
+        for (int i : ints) {
+            VarInt.writeCompactUInt32(i, false, outputStream);
+            outputStream.write(empty);
+        }
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+        for (int i : ints) {
+            Assertions.assertEquals(i, VarInt.readCompactUInt32(false, inputStream));
+            Assertions.assertEquals(inputStream.read(empty), empty.length);
+            Assertions.assertTrue(Utils.isEmpty(empty), "array should consist of zeros.");
+        }
+    }
+
+    @Test
+    public void testReadWriteLossless64() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        int ints[] = new int[] {
+                0, 1, 2, 5, 10, 20, 22, 30, 32, 40, 60, 120, 12000, 24000, 124000, 1000000, 1000000000
+        };
+
+        byte empty[] = new byte[12];
+
+        for (int i : ints) {
+            VarInt.writeCompactUInt32(i, true, outputStream);
+            outputStream.write(empty);
+        }
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+        for (int i : ints) {
+            Assertions.assertEquals(i, VarInt.readCompactUInt32(true, inputStream));
+            Assertions.assertEquals(inputStream.read(empty), empty.length);
+            Assertions.assertTrue(Utils.isEmpty(empty), "array should consist of zeros.");
+        }
+    }
 }
