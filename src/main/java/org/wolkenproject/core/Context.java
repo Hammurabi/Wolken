71def9f073d724c448dcc96e2e751e6bcf08e105
Hammurabi <abdullahafadhil@gmail.com>
2021-03-16 12:03:09 +0100

    Update Context.java

--- src/main/java/org/wolkenproject/core/Context.java
+++ src/main/java/org/wolkenproject/core/Context.java
@@ -75,8 +75,8 @@ public class Context {
         serializationFactory.registerClass(TransactionList.class, new TransactionList(0, new LinkedHashSet<>(), new byte[Message.UniqueIdentifierLength]));
         serializationFactory.registerClass(AddressList.class, new AddressList(0, new LinkedHashSet<>()));
 
-        opcodeRegister.registerOp("halt", "stop virtual process (and sub-processes).", 1, scope -> scope.stopProcesses(scope.getProgramCounter().nextByte()));
-        opcodeRegister.registerOp("pop", "pop the top element from the stack.", scope -> scope.getStack().pop());
+        opcodeRegister.registerOp("halt", "stop virtual process (and sub-processes).", 1, 1, scope -> scope.stopProcesses(scope.getProgramCounter().nextByte()));
+        opcodeRegister.registerOp("pop", "pop the top element from the stack.", 1, scope -> scope.getStack().pop());
 
         opcodeRegister.registerOp("call", "pop the top stack element and call it.", 2, scope -> scope.getStack().push(scope.getStack().pop().call(scope)));
 
