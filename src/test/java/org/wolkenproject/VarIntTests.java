b938e80305bba2aed74594e0f784eb1f7856cdae
Hammurabi <abdullahafadhil@gmail.com>
2021-03-16 22:34:21 +0100

    Update VarIntTests.java

--- src/test/java/org/wolkenproject/VarIntTests.java
+++ src/test/java/org/wolkenproject/VarIntTests.java
@@ -33,6 +33,7 @@ public class VarIntTests {
             Assertions.assertTrue(Utils.isEmpty(empty), "array should consist of zeros.");
         }
     }
+    
     @Test
     public void testReadWriteLossless32() throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -44,14 +45,14 @@ public class VarIntTests {
         byte empty[] = new byte[12];
 
         for (int i : ints) {
-            VarInt.writeCompactUInt32(i, false, outputStream);
+            VarInt.writeCompactUInt32(i, true, outputStream);
             outputStream.write(empty);
         }
 
         ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
 
         for (int i : ints) {
-            Assertions.assertEquals(i, VarInt.readCompactUInt32(false, inputStream));
+            Assertions.assertEquals(i, VarInt.readCompactUInt32(true, inputStream));
             Assertions.assertEquals(inputStream.read(empty), empty.length);
             Assertions.assertTrue(Utils.isEmpty(empty), "array should consist of zeros.");
         }
