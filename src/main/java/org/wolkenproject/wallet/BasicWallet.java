6debe606f7b646fb8babd37393bdac12f809faa8
Hammurabi <abdullahafadhil@gmail.com>
2021-03-17 22:36:42 +0100

    Update BasicWallet.java

--- src/main/java/org/wolkenproject/wallet/BasicWallet.java
+++ src/main/java/org/wolkenproject/wallet/BasicWallet.java
@@ -6,6 +6,7 @@ import org.wolkenproject.crypto.Key;
 import org.wolkenproject.crypto.Keypair;
 import org.wolkenproject.crypto.ec.ECKeypair;
 import org.wolkenproject.crypto.ec.ECPrivateKey;
+import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.utils.FileService;
 import org.wolkenproject.utils.HashUtil;
 import org.wolkenproject.utils.Tuple;
@@ -37,10 +38,25 @@ public class BasicWallet {
         return getPrivateKey(null);
     }
 
-    public Key getPrivateKey(byte password[]) {
+    public Key getPrivateKey(char password[]) {
         try {
             InputStream stream = fileService.openFileInputStream();
+            int version = VarInt.readCompactUInt32(false, stream);
+            byte pub[]  = new byte[65];
+            int read = stream.read(pub);
+            if (read != pub.length) {
+                throw new WolkenException("could not read entire public key.");
+            }
+            byte salt[] = new byte[8];
+            read = stream.read(salt);
+            byte iv[] = new byte[16];
+            read = stream.read(iv);
+            byte enc[] = new byte[48];
+            read = stream.read(enc);
 
+            byte key[] = CryptoUtil.aesDecrypt(enc, CryptoUtil.generateSecretForAES(password, salt), iv);
+
+            return new ECPrivateKey(key);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -64,7 +80,7 @@ public class BasicWallet {
             AESResult result            = CryptoUtil.aesEncrypt(privateKey, encryptionKey);
 
             OutputStream stream         = service.openFileOutputStream();
-            
+
             // write a version number
             VarInt.writeCompactUInt32(1, false, stream);
             // does not need to be encrypted (65 bytes)
