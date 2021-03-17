dd078e86a685d858b763ca93b6030a55b8557626
Hammurabi <abdullahafadhil@gmail.com>
2021-03-17 10:45:25 +0100

    Update CheckoutMessage.java

--- src/main/java/org/wolkenproject/network/messages/CheckoutMessage.java
+++ src/main/java/org/wolkenproject/network/messages/CheckoutMessage.java
@@ -1,7 +1,16 @@
 package org.wolkenproject.network.messages;
 
 import org.wolkenproject.core.Context;
+import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.network.Message;
+import org.wolkenproject.network.Node;
+import org.wolkenproject.network.ResponseMetadata;
+import org.wolkenproject.network.Server;
+import org.wolkenproject.serialization.SerializableI;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class CheckoutMessage extends Message {
     public static final class Reason {
@@ -14,5 +23,40 @@ public class CheckoutMessage extends Message {
 
     public CheckoutMessage(int reason) {
         super(Flags.Notify, Context.getInstance().getNetworkParameters().getVersion());
+    }
+
+    @Override
+    public void executePayload(Server server, Node node) {
+
+    }
+
+    @Override
+    public void writeContents(OutputStream stream) throws IOException, WolkenException {
+
+    }
+
+    @Override
+    public void readContents(InputStream stream) throws IOException, WolkenException {
+
+    }
+
+    @Override
+    public <Type> Type getPayload() {
+        return null;
+    }
+
+    @Override
+    public ResponseMetadata getResponseMetadata() {
+        return null;
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return null;
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return 0;
     }
 }
