182e495c9c24708234e6c62256d1a034c3e4553d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-17 16:23:52 +0100

    Create ServerSocket.java

---
+++ src/main/java/org/wolkenproject/network/ServerSocket.java
@@ -0,0 +1,33 @@
+package org.wolkenproject.network;
+
+import java.io.IOException;
+import java.net.SocketAddress;
+
+public class ServerSocket {
+    private java.net.ServerSocket   socket;
+    private boolean                 isOpen;
+
+    public ServerSocket() throws IOException {
+        socket = new java.net.ServerSocket(213);
+        isOpen = false;
+    }
+
+    public void bind(SocketAddress address) throws IOException {
+        socket.bind(address);
+        isOpen = true;
+    }
+
+    public Socket accept() throws IOException {
+        java.net.Socket socket = this.socket.accept();
+
+        if (socket != null) {
+            return new Socket(socket);
+        }
+
+        return null;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+}
