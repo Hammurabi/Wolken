4ebbcc6891e560ab7152f1032840d05fb4a560c9
Hammurabi <abdullahafadhil@gmail.com>
2021-03-18 11:19:28 +0100

    Create Miner.java

---
+++ src/main/java/org/wolkenproject/core/Miner.java
@@ -0,0 +1,37 @@
+package org.wolkenproject.core;
+
+import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.utils.ChainMath;
+
+public abstract class Miner implements Runnable {
+    private Address miningAddress;
+
+    public Miner(Address miningAddress) {
+        this.miningAddress = miningAddress;
+    }
+
+    public abstract void mine();
+
+    @Override
+    public void run() {
+        while (Context.getInstance().isRunning()) {
+            // generate a block
+            try {
+                Block block = new Block();
+                // mint coins to our address
+                block.addTransaction(Transaction.newCoinbase("", ChainMath.getReward(blockIndex.getHeight()), miningAddress));
+                // add transactions
+                addTransactions(block);
+
+                // create a block-index
+                BlockIndex parent = Context.getInstance().getBlockChain().getTip();
+                block.setParent(parent.getHash());
+                block.setBits(ChainMath.calculateNewTarget(block, parent.getHeight() + 1))
+            } catch (WolkenException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected abstract void addTransactions(Block block);
+}
