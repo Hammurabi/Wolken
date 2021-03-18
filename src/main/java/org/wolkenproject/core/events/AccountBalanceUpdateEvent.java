e36b051b29c5fbb4377bb9a85d80aab5ff3f5e03
Hammurabi <abdullahafadhil@gmail.com>
2021-03-18 19:26:25 +0100

    Update AccountBalanceUpdateEvent.java

--- src/main/java/org/wolkenproject/core/events/AccountBalanceUpdateEvent.java
+++ src/main/java/org/wolkenproject/core/events/AccountBalanceUpdateEvent.java
@@ -1,9 +1,26 @@
 package org.wolkenproject.core.events;
 
 import org.wolkenproject.core.Event;
+import org.wolkenproject.utils.Utils;
 
 public class AccountBalanceUpdateEvent extends Event {
+    private byte[]  recipient;
+    private long    value;
+
     public AccountBalanceUpdateEvent(byte[] recipient, long value) {
         super();
+    }
+
+    @Override
+    public void apply() {
+    }
+
+    @Override
+    public void undo() {
+    }
+
+    @Override
+    public byte[] getEventBytes() {
+        return Utils.concatenate(recipient, Utils.takeApartLong(value));
     }
 }
