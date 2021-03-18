c9ed0e5068896a7478946af5b02b02c2dc1dea7d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-18 19:33:04 +0100

    Update NewAccountEvent.java

--- src/main/java/org/wolkenproject/core/events/NewAccountEvent.java
+++ src/main/java/org/wolkenproject/core/events/NewAccountEvent.java
@@ -1,9 +1,26 @@
 package org.wolkenproject.core.events;
 
+import org.wolkenproject.core.Account;
 import org.wolkenproject.core.Event;
 
 public class NewAccountEvent extends Event {
-    public NewAccountEvent(byte[] address) {
+    private Account account;
+    
+    public NewAccountEvent(byte[] address, Account account) {
         super();
+        this.account = account;
+    }
+
+    @Override
+    public void apply() {
+    }
+
+    @Override
+    public void undo() {
+    }
+
+    @Override
+    public byte[] getEventBytes() {
+        return new byte[0];
     }
 }
