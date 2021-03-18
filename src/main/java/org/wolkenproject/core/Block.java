f4346fd4c7404040b26d60d84e300beb12fee899
Hammurabi <abdullahafadhil@gmail.com>
2021-03-18 19:18:51 +0100

    Update Block.java

--- src/main/java/org/wolkenproject/core/Block.java
+++ src/main/java/org/wolkenproject/core/Block.java
@@ -99,6 +99,8 @@ public class Block extends BlockHeader implements Iterable<Transaction> {
     }
 
     public boolean verify(int blockHeight) throws WolkenException {
+        // PoW check
+        if (!ChainMath.validSolution(getHashCode(), getBits())) return false;
         // shallow transaction checks
         if (!shallowVerifyTransactions()) return false;
         // deeper transaction checks
