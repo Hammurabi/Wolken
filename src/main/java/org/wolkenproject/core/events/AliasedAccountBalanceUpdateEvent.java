2fc09839d2090b368a54c1f07bba610d20fb1bb8
Hammurabi <abdullahafadhil@gmail.com>
2021-03-19 00:11:32 +0100

    Create AliasedAccountBalanceUpdateEvent.java

---
+++ src/main/java/org/wolkenproject/core/events/AliasedAccountBalanceUpdateEvent.java
@@ -0,0 +1,9 @@
+package org.wolkenproject.core.events;
+
+import org.wolkenproject.core.Event;
+
+public class AliasedAccountBalanceUpdateEvent extends Event {
+    public AliasedAccountBalanceUpdateEvent(long alias, long value) {
+        super();
+    }
+}
