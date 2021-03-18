ddd1f40227594f68f7962ae655600d48131908b6
Hammurabi <abdullahafadhil@gmail.com>
2021-03-18 11:36:37 +0100

    Update Transaction.java

--- src/main/java/org/wolkenproject/core/Transaction.java
+++ src/main/java/org/wolkenproject/core/Transaction.java
@@ -186,7 +186,7 @@ public abstract class Transaction extends SerializableI implements Comparable<Tr
 
         @Override
         public long calculateSize() {
-            return VarInt.;
+            return VarInt.sizeOfCompactUin32(getVersion(), false) + recipient.length + dump.length;
         }
 
         @Override
