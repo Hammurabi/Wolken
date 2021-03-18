3fc6d1e511321012a16fc04492064cf225ab4928
Hammurabi <abdullahafadhil@gmail.com>
2021-03-18 16:52:34 +0100

    Update BlockStateChangeResult.java

--- src/main/java/org/wolkenproject/core/BlockStateChangeResult.java
+++ src/main/java/org/wolkenproject/core/BlockStateChangeResult.java
@@ -3,16 +3,17 @@ package org.wolkenproject.core;
 import org.wolkenproject.utils.Utils;
 
 import java.util.List;
+import java.util.Queue;
 
 public class BlockStateChangeResult {
-    private final List<byte[]>  transactionIds;
-    private final List<byte[]>  transactionEventIds;
+    private final Queue<byte[]> transactionIds;
+    private final Queue<byte[]> transactionEventIds;
     private final List<Event>   transactionEvents;
     private final byte          merkleRoot[];
     private final byte          transactionMerkleRoot[];
     private final byte          transactionEventMerkleRoot[];
 
-    public BlockStateChangeResult(List<byte[]> transactionIds, List<byte[]> transactionEventIds, List<Event> transactionEvents) {
+    public BlockStateChangeResult(Queue<byte[]> transactionIds, Queue<byte[]> transactionEventIds, List<Event> transactionEvents) {
         this.transactionIds             = transactionIds;
         this.transactionEventIds        = transactionEventIds;
         this.transactionEvents          = transactionEvents;
