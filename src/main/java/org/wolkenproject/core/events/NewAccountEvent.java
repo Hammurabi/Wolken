e61f1b7f2b4aca45a9364267f4453ac216c4dc8a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-18 19:32:38 +0100

    Create NewAccountEvent.java

---
+++ src/main/java/org/wolkenproject/core/events/NewAccountEvent.java
@@ -0,0 +1,9 @@
+package org.wolkenproject.core.events;
+
+import org.wolkenproject.core.Event;
+
+public class NewAccountEvent extends Event {
+    public NewAccountEvent(byte[] address) {
+        super();
+    }
+}
