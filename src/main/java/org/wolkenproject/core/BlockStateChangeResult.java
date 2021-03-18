5891331b096422c3f812d89f7ecc3529808a39b0
Hammurabi <abdullahafadhil@gmail.com>
2021-03-18 16:41:40 +0100

    Update BlockStateChangeResult.java

--- src/main/java/org/wolkenproject/core/BlockStateChangeResult.java
+++ src/main/java/org/wolkenproject/core/BlockStateChangeResult.java
@@ -1,4 +1,12 @@
 package org.wolkenproject.core;
 
+import java.util.List;
+
 public class BlockStateChangeResult {
+    private final List<byte[]> transactionIds;
+    private final List<byte[]> transactionEventIds;
+    private final List<Event>  transactionEvents;
+
+    public BlockStateChangeResult() {
+    }
 }
