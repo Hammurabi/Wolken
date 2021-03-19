462c52d1025e98da7e35789fbc1b3290d3ae46e7
Hammurabi <abdullahafadhil@gmail.com>
2021-03-19 15:20:31 +0100

    Update Block.java

--- src/main/java/org/wolkenproject/core/Block.java
+++ src/main/java/org/wolkenproject/core/Block.java
@@ -100,7 +100,7 @@ public class Block extends BlockHeader implements Iterable<Transaction> {
         // must have at least one transaction
         if (transactions.isEmpty()) return false;
         // first transaction must be a minting transaction
-        if (transactions.iterator().next() instanceof Transaction.MintTransaction == false) return false;
+        if (transactions.iterator().next() instanceof MintTransaction == false) return false;
         // shallow transaction checks
         if (!shallowVerifyTransactions()) return false;
         // deeper transaction checks
