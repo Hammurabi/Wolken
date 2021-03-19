991882dbb37b0ff89836c6ab76e92d24e3308272
Hammurabi <abdullahafadhil@gmail.com>
2021-03-19 21:16:13 +0100

    Update AbstractBlockChain.java

--- src/main/java/org/wolkenproject/core/AbstractBlockChain.java
+++ src/main/java/org/wolkenproject/core/AbstractBlockChain.java
@@ -1,7 +1,19 @@
 package org.wolkenproject.core;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public abstract class AbstractBlockChain implements Runnable {
     protected static final int                MaximumOrphanBlockQueueSize = 250_000_000;
     protected static final int                MaximumStaleBlockQueueSize  = 500_000_000;
     protected static final int                MaximumPoolBlockQueueSize   = 1_250_000_000;
+
+    private ReentrantLock lock;
+
+    public AbstractBlockChain() {
+        this.lock = new ReentrantLock();
+    }
+
+    @Override
+    public void run() {
+    }
 }
