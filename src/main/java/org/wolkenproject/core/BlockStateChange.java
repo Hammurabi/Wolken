aa441290716128d4fd4556970ea86cca8b2dca56
Hammurabi <Riverssen@gmail.com>
2021-03-19 11:52:38 +0100

    Update BlockStateChange.java

--- src/main/java/org/wolkenproject/core/BlockStateChange.java
+++ src/main/java/org/wolkenproject/core/BlockStateChange.java
@@ -1,5 +1,6 @@
 package org.wolkenproject.core;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -7,6 +8,12 @@ public class BlockStateChange {
     private Queue<byte[]>   transactionIds;
     private Queue<byte[]>   transactionEventIds;
     private List<Event>     transactionEvents;
+
+    public BlockStateChange() {
+        this.transactionIds         = new LinkedList<>();
+        this.transactionEventIds    = new LinkedList<>();
+        this.transactionEvents      = new LinkedList<>();
+    }
 
     public BlockStateChangeResult getResult() {
         return new BlockStateChangeResult(transactionIds, transactionEventIds, transactionEvents);
