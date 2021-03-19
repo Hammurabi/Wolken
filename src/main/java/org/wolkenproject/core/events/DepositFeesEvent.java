6f5cdb5b906ca54c8d8a36645af6596278db6430
Hammurabi <Riverssen@gmail.com>
2021-03-19 13:53:26 +0100

    Create DepositFeesEvent.java

---
+++ src/main/java/org/wolkenproject/core/events/DepositFeesEvent.java
@@ -0,0 +1,32 @@
+package org.wolkenproject.core.events;
+
+import org.wolkenproject.core.Context;
+import org.wolkenproject.core.Event;
+import org.wolkenproject.utils.Utils;
+
+public class DepositFeesEvent extends Event {
+    private byte address[];
+    private long amount;
+
+    public DepositFeesEvent(byte address[], long amount) {
+        this.address    = address;
+        this.amount     = amount;
+    }
+
+    @Override
+    public void apply() {
+        Context.getInstance().getDatabase().updateAccount(address,
+                Context.getInstance().getDatabase().getAccount(address).deposit(amount));
+    }
+
+    @Override
+    public void undo() {
+        Context.getInstance().getDatabase().updateAccount(address,
+                Context.getInstance().getDatabase().getAccount(address).withdraw(amount));
+    }
+
+    @Override
+    public byte[] getEventBytes() {
+        return Utils.concatenate("Deposit Fees".getBytes(), address, Utils.takeApartLong(amount));
+    }
+}
