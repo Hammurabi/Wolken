a54a439164e0b0b6ab6c617b8f63795a88d60d70
Hammurabi <Riverssen@gmail.com>
2021-03-19 13:31:35 +0100

    Update WithdrawFundsEvent.java

--- src/main/java/org/wolkenproject/core/events/WithdrawFundsEvent.java
+++ src/main/java/org/wolkenproject/core/events/WithdrawFundsEvent.java
@@ -1,8 +1,12 @@
 package org.wolkenproject.core.events;
 
 import org.wolkenproject.core.Event;
+import org.wolkenproject.utils.Utils;
 
 public class WithdrawFundsEvent extends Event {
+    private byte address[];
+    private long amount;
+    
     @Override
     public void apply() {
     }
@@ -13,6 +17,6 @@ public class WithdrawFundsEvent extends Event {
 
     @Override
     public byte[] getEventBytes() {
-        return new byte[0];
+        return Utils.concatenate("Withdraw".getBytes(), address, Utils.concatenate(amount));
     }
 }
