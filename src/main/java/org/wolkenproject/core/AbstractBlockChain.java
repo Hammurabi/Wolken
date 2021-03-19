f2ecc4149343c303c313983e435ce77e6e95cb5d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-19 21:20:07 +0100

    Update AbstractBlockChain.java

--- src/main/java/org/wolkenproject/core/AbstractBlockChain.java
+++ src/main/java/org/wolkenproject/core/AbstractBlockChain.java
@@ -1,5 +1,7 @@
 package org.wolkenproject.core;
 
+import org.wolkenproject.utils.PriorityHashQueue;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class AbstractBlockChain implements Runnable {
@@ -7,13 +9,28 @@ public abstract class AbstractBlockChain implements Runnable {
     protected static final int                MaximumStaleBlockQueueSize  = 500_000_000;
     protected static final int                MaximumPoolBlockQueueSize   = 1_250_000_000;
 
+    // the current higest block in the chain
+    private BlockIndex                      tip;
+    // contains blocks that have no parents.
+    private PriorityHashQueue<BlockIndex> orphanedBlocks;
+    // contains blocks that were valid pre-fork.
+    private PriorityHashQueue<BlockIndex>   staleBlocks;
+    // contains blocks sent from peers.
+    private PriorityHashQueue<BlockIndex>   blockPool;
+    // a mutex
     private ReentrantLock lock;
 
     public AbstractBlockChain() {
-        this.lock = new ReentrantLock();
+        lock            = new ReentrantLock();
+        orphanedBlocks  = new PriorityHashQueue<>(BlockIndex.class);
+        staleBlocks     = new PriorityHashQueue<>(BlockIndex.class);
+        blockPool       = new PriorityHashQueue<>(BlockIndex.class);
     }
 
     @Override
     public void run() {
+    }
+
+    public void suggest(BlockIndex index) {
     }
 }
