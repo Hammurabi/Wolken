acae80c071fb0aaaf921656723f910f59da5b256
Hammurabi <Riverssen@gmail.com>
2021-03-19 13:39:39 +0100

    Create MintRewardEvent.java

---
+++ src/main/java/org/wolkenproject/core/events/MintRewardEvent.java
@@ -0,0 +1,32 @@
+package org.wolkenproject.core.events;
+
+import org.wolkenproject.core.Context;
+import org.wolkenproject.core.Event;
+import org.wolkenproject.utils.Utils;
+
+public class MintRewardEvent extends Event {
+    private byte address[];
+    private long amount;
+
+    public MintRewardEvent(byte address[], long amount) {
+        this.address    = address;
+        this.amount     = amount;
+    }
+
+    @Override
+    public void apply() {
+        Context.getInstance().getDatabase().updateAccount(address,
+                Context.getInstance().getDatabase().getAccount(address).deposit(amount));
+    }
+
+    @Override
+    public void undo() {
+        Context.getInstance().getDatabase().updateAccount(address,
+                Context.getInstance().getDatabase().getAccount(address).withdraw(amount));
+    }
+
+    @Override
+    public byte[] getEventBytes() {
+        return Utils.concatenate("Deposit".getBytes(), address, Utils.takeApartLong(amount));
+    }
+}
