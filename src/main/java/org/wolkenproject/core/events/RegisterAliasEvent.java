95c31420a3926a32c414bb695bdb44bdf09b33d5
Hammurabi <Riverssen@gmail.com>
2021-03-19 13:47:36 +0100

    Create RegisterAliasEvent.java

---
+++ src/main/java/org/wolkenproject/core/events/RegisterAliasEvent.java
@@ -0,0 +1,32 @@
+package org.wolkenproject.core.events;
+
+import org.wolkenproject.core.Context;
+import org.wolkenproject.core.Event;
+import org.wolkenproject.utils.Utils;
+
+public class RegisterAliasEvent extends Event {
+    private byte    address[];
+    private long    alias;
+
+    public RegisterAliasEvent(byte[] address, long alias) {
+        super();
+    }
+
+    @Override
+    public void apply() {
+        Context.getInstance().getDatabase().registerAlias(address, alias);
+    }
+
+    @Override
+    public void undo() {
+    }
+
+    @Override
+    public byte[] getEventBytes() {
+        return Utils.concatenate("Register Alias".getBytes(), address, Utils.takeApartLong(alias));
+    }
+
+    public long getAlias() {
+        return alias;
+    }
+}
