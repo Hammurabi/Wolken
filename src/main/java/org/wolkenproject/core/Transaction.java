69510223945e1df7ccc50c9554eaa776217d90ce
Hammurabi <Riverssen@gmail.com>
2021-03-19 14:07:58 +0100

    Update Transaction.java

--- src/main/java/org/wolkenproject/core/Transaction.java
+++ src/main/java/org/wolkenproject/core/Transaction.java
@@ -3,6 +3,7 @@ package org.wolkenproject.core;
 import org.wolkenproject.core.events.*;
 import org.wolkenproject.core.transactions.BasicTransaction;
 import org.wolkenproject.core.transactions.MintTransaction;
+import org.wolkenproject.core.transactions.RegisterAliasTransaction;
 import org.wolkenproject.crypto.Keypair;
 import org.wolkenproject.crypto.Signature;
 import org.wolkenproject.crypto.ec.RecoverableSignature;
