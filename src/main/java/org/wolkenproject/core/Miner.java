278bb3ee310c489955baeaeaa582e769ae875608
Hammurabi <Riverssen@gmail.com>
2021-03-19 14:10:01 +0100

    Update Miner.java

--- src/main/java/org/wolkenproject/core/Miner.java
+++ src/main/java/org/wolkenproject/core/Miner.java
@@ -1,5 +1,6 @@
 package org.wolkenproject.core;
 
+import org.wolkenproject.core.transactions.Transaction;
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.utils.ChainMath;
 
