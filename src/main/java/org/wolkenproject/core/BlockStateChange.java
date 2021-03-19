9573090fb97d5da5484d21cf1917b17c7bb9e0e2
Hammurabi <Riverssen@gmail.com>
2021-03-19 11:50:23 +0100

    Update BlockStateChange.java

--- src/main/java/org/wolkenproject/core/BlockStateChange.java
+++ src/main/java/org/wolkenproject/core/BlockStateChange.java
@@ -1,4 +1,14 @@
 package org.wolkenproject.core;
 
+import java.util.List;
+import java.util.Queue;
+
 public class BlockStateChange {
+    private Queue<byte[]>   transactionIds;
+    private Queue<byte[]>   transactionEventIds;
+    private List<Event>     transactionEvents;
+
+    public BlockStateChangeResult getResult() {
+        return new BlockStateChangeResult(txids, txeids, events);
+    }
 }
