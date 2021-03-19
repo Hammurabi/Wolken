df4e66888dddff431381ff81909b206b026f6e0a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-19 21:15:12 +0100

    Update AbstractBlockChain.java

--- src/main/java/org/wolkenproject/core/AbstractBlockChain.java
+++ src/main/java/org/wolkenproject/core/AbstractBlockChain.java
@@ -1,4 +1,7 @@
 package org.wolkenproject.core;
 
-public abstract class AbstractBlockChain {
+public abstract class AbstractBlockChain implements Runnable {
+    protected static final int                MaximumOrphanBlockQueueSize = 250_000_000;
+    protected static final int                MaximumStaleBlockQueueSize  = 500_000_000;
+    protected static final int                MaximumPoolBlockQueueSize   = 1_250_000_000;
 }
