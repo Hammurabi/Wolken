bc00c279d417fa6b036361fecd173bcf0f066878
Hammurabi <Riverssen@gmail.com>
2021-03-19 13:30:34 +0100

    Update WithdrawFundsEvent.java

--- src/main/java/org/wolkenproject/core/events/WithdrawFundsEvent.java
+++ src/main/java/org/wolkenproject/core/events/WithdrawFundsEvent.java
@@ -3,4 +3,16 @@ package org.wolkenproject.core.events;
 import org.wolkenproject.core.Event;
 
 public class WithdrawFundsEvent extends Event {
+    @Override
+    public void apply() {
+    }
+
+    @Override
+    public void undo() {
+    }
+
+    @Override
+    public byte[] getEventBytes() {
+        return new byte[0];
+    }
 }
