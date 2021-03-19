4858dda6415a5824eac4eeea9a2937de77ed49a7
Hammurabi <Riverssen@gmail.com>
2021-03-19 13:16:12 +0100

    Update Transaction.java

--- src/main/java/org/wolkenproject/core/Transaction.java
+++ src/main/java/org/wolkenproject/core/Transaction.java
@@ -88,11 +88,11 @@ public abstract class Transaction extends SerializableI implements Comparable<Tr
     public abstract void getStateChange(Block block, int blockHeight, BlockStateChange stateChange) throws WolkenException;
 
     protected void createAccountIfDoesNotExist(byte address[], BlockStateChange stateChange) {
-        if (Context.getInstance().getDatabase().checkAccountExists(address)) {
+        if (stateChange.checkAccountExists(address)) {
             return;
         }
 
-        stateChangeEvents.add(new NewAccountEvent(address));
+        stateChange.addEvent(new NewAccountEvent(address));
     }
 
     public Transaction sign(Keypair keypair) throws WolkenException {
