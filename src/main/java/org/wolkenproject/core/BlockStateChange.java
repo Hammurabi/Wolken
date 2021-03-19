bc17ecd563d28af810fd12f55fce09b29d1a0418
Hammurabi <Riverssen@gmail.com>
2021-03-19 11:59:14 +0100

    Update BlockStateChange.java

--- src/main/java/org/wolkenproject/core/BlockStateChange.java
+++ src/main/java/org/wolkenproject/core/BlockStateChange.java
@@ -1,7 +1,9 @@
 package org.wolkenproject.core;
 
 import org.wolkenproject.core.events.AliasRegistrationEvent;
+import org.wolkenproject.core.events.NewAccountEvent;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -36,6 +38,14 @@ public class BlockStateChange {
     public boolean checkAccountExists(byte address[]) {
         if (Context.getInstance().getDatabase().checkAccountExists(address)) {
             return true;
+        }
+
+        for (Event event : transactionEvents) {
+            if (event instanceof NewAccountEvent) {
+                if (Arrays.equals(((NewAccountEvent) event).getAddress(), address)) {
+                    return true;
+                }
+            }
         }
 
         return false;
