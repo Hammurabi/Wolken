2332069d4ee75a7ca7bb0a9c6059a9a88b136b64
Hammurabi <Riverssen@gmail.com>
2021-03-19 14:09:15 +0100

    Update BlockChain.java

--- src/main/java/org/wolkenproject/core/BlockChain.java
+++ src/main/java/org/wolkenproject/core/BlockChain.java
@@ -1,5 +1,6 @@
 package org.wolkenproject.core;
 
+import org.wolkenproject.core.transactions.Transaction;
 import org.wolkenproject.encoders.Base16;
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.network.Message;
