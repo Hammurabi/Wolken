c6876a01ef855085dabc52dd446a112fb0ed263f
Hammurabi <abdullahafadhil@gmail.com>
2021-03-20 11:48:15 +0100

    Update LinkedHashQueue.java

--- src/main/java/org/wolkenproject/utils/LinkedHashQueue.java
+++ src/main/java/org/wolkenproject/utils/LinkedHashQueue.java
@@ -1,6 +1,11 @@
 package org.wolkenproject.utils;
 
+import java.util.HashSet;
 import java.util.Queue;
 
 public class LinkedHashQueue<T> implements HashQueue<T> {
+    private Queue<T>    queue;
+    private HashSet<T>  set;
+
+    
 }
