99ed740f52c1e265087a9e42b8898c0c88129b16
Hammurabi <abdullahafadhil@gmail.com>
2021-03-20 11:50:57 +0100

    Update LinkedHashQueue.java

--- src/main/java/org/wolkenproject/utils/LinkedHashQueue.java
+++ src/main/java/org/wolkenproject/utils/LinkedHashQueue.java
@@ -1,11 +1,115 @@
 package org.wolkenproject.utils;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Queue;
 
 public class LinkedHashQueue<T> implements HashQueue<T> {
     private Queue<T>    queue;
     private HashSet<T>  set;
 
-    
+    @Override
+    public boolean containsKey(byte[] hash) {
+        return false;
+    }
+
+    @Override
+    public void removeTails(int newLength) {
+    }
+
+    @Override
+    public T getByHash(byte[] hash) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] t1s) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean offer(T t) {
+        return false;
+    }
+
+    @Override
+    public T remove() {
+        return null;
+    }
+
+    @Override
+    public T poll() {
+        return null;
+    }
+
+    @Override
+    public T element() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
 }
