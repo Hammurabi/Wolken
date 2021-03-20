4a7c834a5003a06ccff30bf518fd465158d8e7be
Hammurabi <abdullahafadhil@gmail.com>
2021-03-20 11:39:58 +0100

    Update HashQueue.java

--- src/main/java/org/wolkenproject/utils/HashQueue.java
+++ src/main/java/org/wolkenproject/utils/HashQueue.java
@@ -2,8 +2,10 @@ package org.wolkenproject.utils;
 
 import org.wolkenproject.serialization.SerializableI;
 
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public interface HashQueue<T extends SerializableI & Comparable<T>> extends Queue<T> {
     boolean containsKey(byte[] hash);
+    void removeTails(int newLength);
 }
