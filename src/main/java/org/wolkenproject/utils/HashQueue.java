0b5f2cad90935575df850416effd51244aacd824
Hammurabi <abdullahafadhil@gmail.com>
2021-03-20 11:36:25 +0100

    Update HashQueue.java

--- src/main/java/org/wolkenproject/utils/HashQueue.java
+++ src/main/java/org/wolkenproject/utils/HashQueue.java
@@ -4,5 +4,5 @@ import org.wolkenproject.serialization.SerializableI;
 
 import java.util.Queue;
 
-public abstract class HashQueue<T extends SerializableI & Comparable<T>> implements Queue<T> {
+public interface HashQueue<T extends SerializableI & Comparable<T>> implements Queue<T> {
 }
