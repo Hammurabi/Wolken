cf46dd35aacf06cd539228701d5d1945a11272ba
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 16:15:49 +0100

    Update Context.java

--- src/main/java/org/wolkenproject/core/Context.java
+++ src/main/java/org/wolkenproject/core/Context.java
@@ -36,7 +36,7 @@ public class Context {
     private Address                 payList[];
     private BlockChain              blockChain;
     private OpcodeRegister          opcodeRegister;
-    private RpcServer       rpcServer;
+    private RpcServer               rpcServer;
     private FileService fileService;
 
     public Context(FileService service, boolean testNet, Address[] payList, Set<NetAddress> forceConnections) throws WolkenException, IOException {
