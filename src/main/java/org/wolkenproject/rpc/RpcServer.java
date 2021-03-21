424246fb4772530eb83ed4bb08d804c03efc6bab
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 14:32:04 +0100

    Update RpcServer.java

--- src/main/java/org/wolkenproject/rpc/RpcServer.java
+++ src/main/java/org/wolkenproject/rpc/RpcServer.java
@@ -10,11 +10,19 @@ public class RpcServer {
     private HttpServer server;
     public RpcServer(int port) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/submit", RpcServer::onSubmitMsg);
+        server.createContext("/tx", RpcServer::onTransactionMsg);
         server.createContext("/block", RpcServer::onBlockMsg);
         server.setExecutor(null);
         server.start();
     }
 
     public static void onBlockMsg(HttpExchange exchange) {
+    }
+
+    public static void onTransactionMsg(HttpExchange exchange) {
+    }
+
+    public static void onSubmitMsg(HttpExchange exchange) {
     }
 }
