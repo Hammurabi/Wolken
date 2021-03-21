4b7a0a5d8ebb0df57c745955b53106ef478b2482
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 13:22:30 +0100

    Update Null.java

--- src/main/java/org/wolkenproject/utils/Null.java
+++ src/main/java/org/wolkenproject/utils/Null.java
@@ -46,4 +46,44 @@ public class Null {
 
         return a;
     }
+
+    public static byte[] notNull(byte a[]) {
+        if (a == null) {
+            return b_array;
+        }
+
+        return a;
+    }
+
+    public static short[] notNull(short a[]) {
+        if (a == null) {
+            return s_array;
+        }
+
+        return a;
+    }
+
+    public static char[] notNull(char a[]) {
+        if (a == null) {
+            return c_array;
+        }
+
+        return a;
+    }
+
+    public static int[] notNull(int a[]) {
+        if (a == null) {
+            return i_array;
+        }
+
+        return a;
+    }
+
+    public static long[] notNull(long a[]) {
+        if (a == null) {
+            return l_array;
+        }
+
+        return a;
+    }
 }
