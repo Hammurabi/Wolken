700b75ce46e7755ed131d1ef833d0883e433c91a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 14:50:51 +0100

    Update RpcServer.java

--- src/main/java/org/wolkenproject/rpc/RpcServer.java
+++ src/main/java/org/wolkenproject/rpc/RpcServer.java
@@ -62,6 +62,6 @@ public class RpcServer {
 
     private static final void sendResponse(int responseCode, JSONObject response, HttpExchange exchange) {
         String actualResponse = response.toString();
-        exchange.sendResponseHeaders(responseCode, response.toString().length());
+        exchange.sendResponseHeaders(responseCode, actualResponse.length());
     }
 }
