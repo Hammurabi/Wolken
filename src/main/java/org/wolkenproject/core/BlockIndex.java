7c797861773d4c743123799ce63a12e9f54abdce
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 14:57:22 +0100

    Update BlockIndex.java

--- src/main/java/org/wolkenproject/core/BlockIndex.java
+++ src/main/java/org/wolkenproject/core/BlockIndex.java
@@ -217,5 +217,6 @@ public class BlockIndex extends SerializableI implements Comparable<BlockIndex> 
 
         header.put("version", getBlock().getVersion());
         header.put("timestamp", getBlock().getTimestamp());
+        header.put("parentHash", Base16.encode(getBlock().getParentHash()));
     }
 }
