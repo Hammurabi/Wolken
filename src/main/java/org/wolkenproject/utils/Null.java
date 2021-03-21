237c935d35d87022ed9cd4e33a0d2149960f4c92
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 13:17:42 +0100

    Create Null.java

---
+++ src/main/java/org/wolkenproject/utils/Null.java
@@ -0,0 +1,9 @@
+package org.wolkenproject.utils;
+
+public class Null {
+    private static final byte[] b_array = new byte[0];
+
+    public static byte[] notNull(byte a[]) {
+        return b_array;
+    }
+}
