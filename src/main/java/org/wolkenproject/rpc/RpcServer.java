d7d242b133b515e86f183cc118df6a68b92ffbac
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 14:30:35 +0100

    Update RpcServer.java

--- src/main/java/org/wolkenproject/rpc/RpcServer.java
+++ src/main/java/org/wolkenproject/rpc/RpcServer.java
@@ -1,9 +1,20 @@
 package org.wolkenproject.rpc;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class RpcServer {
     private HttpServer server;
-    public RpcServer(int port) {
+    public RpcServer(int port) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/block", RpcServer::onBlockMsg);
+        server.setExecutor(null);
+        server.start();
+    }
+
+    public static void onBlockMsg(HttpExchange exchange) {
     }
 }
