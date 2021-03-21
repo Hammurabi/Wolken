6c45f64bd407539934dd322a0419b2f0e78a6e94
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 16:48:10 +0100

    Update rpc.py

--- rpc/rpc.py
+++ rpc/rpc.py
@@ -1,9 +1,15 @@
 # import requests to make our lives easier
 import requests
 
+# valid commands
+# getblock  <hash>
+# gettx     <hash>
+
 def scan_commands():
     # get inputs from the command line
     text = input(">")
+
+    # 
 
 def start():
     # enter an infinite loop
