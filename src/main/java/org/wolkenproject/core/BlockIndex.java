f56e68bda4851b852b6a415801433604997e8d6b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 15:18:35 +0100

    Update BlockIndex.java

--- src/main/java/org/wolkenproject/core/BlockIndex.java
+++ src/main/java/org/wolkenproject/core/BlockIndex.java
@@ -248,7 +248,7 @@ public class BlockIndex extends SerializableI implements Comparable<BlockIndex> 
             block.put("content", body);
         }
 
-        if (!txHash && evList) {
+        if (!txEvt && evList) {
             int index = 0;
             for (Event event : getStateChange()) {
                 state.put(index ++, event.toJson());
