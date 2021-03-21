b8cebf6e3ab07c72cf750ce54ab0ce17af80f437
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 20:06:56 +0100

    Update rpc.py

--- rpc/rpc.py
+++ rpc/rpc.py
@@ -7,6 +7,46 @@ import requests
 # getbalance    <address>
 # quit
 
+def getblock_parse(command, arguments):
+    # check correct amount of arguments exists
+    if len(arguments < 2):
+        print("error: 'getblock' command requires a minimum of two arguments.")
+        pass
+
+    # check that the block-id is base16 encoded
+    if is_base16_encoded(arguments[1]):
+        print("error: 'getblock' command requires second argument to be base16 encoded.")
+        pass
+
+    # getblock <hash>
+    # getblock <hash> includeTx txAsHash includeEv evAsHash format
+    if len(arguments) > 2 and len(arguments) < 7:
+        print("error: 'getblock' command missing arguments.")
+        pass
+
+class Command:
+    pass
+
+
+def new_command(name, parse):
+    command = Command()
+    command.name    = name
+    command.parse   = parse
+    return command
+    
+
+commands_list = [ new_command('getblock', getblock_parse) ]
+base16   = ['0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+base58   = ['1', '2', '3', '4', '5', '6', '7', '8',
+            '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q',
+            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+            'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+            'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z']
+
 def is_base16_encoded(text):
     for c in text.lower():
         for x in base16:
@@ -39,51 +79,6 @@ def start():
                 if command.name == arguments[0]:
                     # attempt to parse the command
                     command.parse(arguments)
-
-
-
-
-
-
-def getblock_parse(command, arguments):
-    # check correct amount of arguments exists
-    if len(arguments < 2):
-        print("error: 'getblock' command requires a minimum of two arguments.")
-        pass
-
-    # check that the block-id is base16 encoded
-    if is_base16_encoded(arguments[1]):
-        print("error: 'getblock' command requires second argument to be base16 encoded.")
-        pass
-
-    # getblock <hash>
-    # getblock <hash> includeTx txAsHash includeEv evAsHash format
-    if len(arguments) > 2 and len(arguments) < 7:
-        print("error: 'getblock' command missing arguments.")
-        pass
-
-class Command:
-    pass
-
-
-def new_command(name, parse):
-    command = Command()
-    command.name    = name
-    command.parse   = parse
-    return command
-    
-
-commands = [ new_command('getblock', getblock_parse) ]
-base16   = ['0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-base58   = ['1', '2', '3', '4', '5', '6', '7', '8',
-            '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-            'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q',
-            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-            'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-            'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-            'y', 'z']
 
 # start the loop
 start()
