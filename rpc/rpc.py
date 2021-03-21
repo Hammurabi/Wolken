b6cd145acbc732ff1c33cb323ded826f13187f83
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 20:16:17 +0100

    Update rpc.py

--- rpc/rpc.py
+++ rpc/rpc.py
@@ -84,7 +84,8 @@ def new_command(name, parse):
     
 
 # define a basic command list
-commands_list = [   new_command('quit', quit_parse),
+commands_list = [   new_command('exit', exit_parse),
+                    new_command('quit', quit_parse),
                     new_command('getblock', getblock_parse) ]
 
 # define 'base16' characters
