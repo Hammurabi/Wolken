525c44891346c70ef7c71c4702ed429a151801c1
Hammurabi <abdullahafadhil@gmail.com>
2021-03-21 13:18:11 +0100

    Update Null.java

--- src/main/java/org/wolkenproject/utils/Null.java
+++ src/main/java/org/wolkenproject/utils/Null.java
@@ -2,6 +2,10 @@ package org.wolkenproject.utils;
 
 public class Null {
     private static final byte[] b_array = new byte[0];
+    private static final int[]  s_array = new byte[0];
+    private static final byte[] c_array = new byte[0];
+    private static final byte[] i_array = new byte[0];
+    private static final byte[] l_array = new byte[0];
 
     public static byte[] notNull(byte a[]) {
         if (a == null) {
