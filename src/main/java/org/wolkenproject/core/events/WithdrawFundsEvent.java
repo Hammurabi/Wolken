730a6d12b3de9c15f1140a0cf5effe0793188cbe
Hammurabi <abdullahafadhil@gmail.com>
2021-03-22 18:49:45 +0100

    Update WithdrawFundsEvent.java

--- src/main/java/org/wolkenproject/core/events/WithdrawFundsEvent.java
+++ src/main/java/org/wolkenproject/core/events/WithdrawFundsEvent.java
@@ -3,6 +3,7 @@ package org.wolkenproject.core.events;
 import org.json.JSONObject;
 import org.wolkenproject.core.Context;
 import org.wolkenproject.core.Event;
+import org.wolkenproject.encoders.Base58;
 import org.wolkenproject.utils.Utils;
 
 public class WithdrawFundsEvent extends Event {
@@ -33,6 +34,6 @@ public class WithdrawFundsEvent extends Event {
 
     @Override
     public JSONObject toJson() {
-        return null;
+        return new JSONObject().put("event", this.getClass().getName()).put("address", Base58.encode(address)).put("amount", amount);
     }
 }
