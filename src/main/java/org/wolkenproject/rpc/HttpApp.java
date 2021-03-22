2ddbefe6294b58ac4194d1ce333c9135592a7775
Hammurabi <abdullahafadhil@gmail.com>
2021-03-22 20:11:35 +0100

    Update HttpApp.java

--- src/main/java/org/wolkenproject/rpc/HttpApp.java
+++ src/main/java/org/wolkenproject/rpc/HttpApp.java
@@ -1,6 +1,28 @@
 package org.wolkenproject.rpc;
 
+import com.sun.net.httpserver.HttpServer;
+import org.wolkenproject.core.Context;
+import org.wolkenproject.utils.Logger;
+
+import java.net.InetSocketAddress;
+
 public class HttpApp {
+    protected HttpApp() {
+        Logger.alert("=============================================");
+        Logger.alert("Starting HTTP server");
+        Logger.alert("=============================================");
+
+        server = HttpServer.create(new InetSocketAddress(port), 12);
+        server.createContext("/", RpcServer::listen);
+        server.setExecutor(null);
+
+        paths = new UrlPath[] {
+                new UrlPath("content", messenger -> messenger.sendFile("text/html", Context.getInstance().getResourceManager().get("/index.html")), new UrlPath[] {
+                }),
+        };
+        server.start();
+    }
+
     public void stop(int i) {
     }
 }
