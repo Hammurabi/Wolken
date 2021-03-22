d39848657edd07718f17cf50dfb54958a6d53305
Hammurabi <abdullahafadhil@gmail.com>
2021-03-22 19:31:30 +0100

    Update Messenger.java

--- src/main/java/org/wolkenproject/rpc/Messenger.java
+++ src/main/java/org/wolkenproject/rpc/Messenger.java
@@ -1,8 +1,16 @@
 package org.wolkenproject.rpc;
 
+import com.sun.net.httpserver.HttpExchange;
+
 import java.io.InputStream;
 
 public class Messenger {
+    private HttpExchange exchange;
+    
+    public Messenger(HttpExchange exchange) {
+        this.exchange = exchange;
+    }
+
     public void sendFile(InputStream inputStream) {
         sendFile("text/html", inputStream);
     }
