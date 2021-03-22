f99d684ef11df926489cd8cc6d4285d68b5df856
Hammurabi <abdullahafadhil@gmail.com>
2021-03-22 19:34:54 +0100

    Update Messenger.java

--- src/main/java/org/wolkenproject/rpc/Messenger.java
+++ src/main/java/org/wolkenproject/rpc/Messenger.java
@@ -5,9 +5,11 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.InputStream;
 
 public class Messenger {
-    private HttpExchange exchange;
+    private HttpExchange    exchange;
+    private String          url;
+    private String          query;
 
-    public Messenger(HttpExchange exchange) {
+    public Messenger(HttpExchange exchange, String url, String query) {
         this.exchange = exchange;
     }
 
