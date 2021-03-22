6b542c3825cf823be9e86d4e0f0d09bdad7ee975
Hammurabi <abdullahafadhil@gmail.com>
2021-03-22 19:24:06 +0100

    Update UrlPath.java

--- src/main/java/org/wolkenproject/rpc/UrlPath.java
+++ src/main/java/org/wolkenproject/rpc/UrlPath.java
@@ -1,4 +1,18 @@
 package org.wolkenproject.rpc;
 
 public class UrlPath {
+    private String      path;
+    private UrlPath[]   paths;
+    public UrlPath(String path, UrlPath paths[]) {
+        this.path = path;
+        this.paths = paths;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public UrlPath[] getPaths() {
+        return paths;
+    }
 }
