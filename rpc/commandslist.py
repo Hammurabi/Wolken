bdd370da534297cfb3973e73d942d8c5b087cadc
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 14:09:33 +0100

    Create commandslist.py

---
+++ rpc/commandslist.py
@@ -0,0 +1,9 @@
+# import requests to make our lives easier
+import requests
+# import socket to do IP address checks
+import util
+# import commands
+import commands
+
+def register_all(cmdManager):
+    
