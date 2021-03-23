2f2f5c5f557e29181d1c1745535b046c45923019
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 19:27:55 +0100

    Update cmd_node.py

--- rpc/cmd_node.py
+++ rpc/cmd_node.py
@@ -8,7 +8,7 @@ def parse(command, arguments, connection):
         print("error: '%s' requires two arguments.", command.name)
     else:
         if not util.is_valid_ip(arguments[1]):
-            print("error: 'node' requires the first argument to be a valid IP address.")
+            print("error: '%s' requires the first argument to be a valid IP address.", command.name)
             pass
         if not arguments[2].isnumeric():
             print("error: 'node' requires the second argument to be a valid port.")
