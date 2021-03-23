3500dc1f2119b693b74e106b433abae836e2d4c3
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 21:59:25 +0100

    Update cmd_newpasswallet.py

--- rpc/cmd_newpasswallet.py
+++ rpc/cmd_newpasswallet.py
@@ -4,14 +4,14 @@ import commands
 from getpass import getpass
 
 
-# define 'createwallet' command
+# define 'newpasswallet' command
 def parse(cmd, arguments, connection):
     if len(arguments) != 3:
         print("error: '"+cmd.name+"' requires two arguments.")
     else:
         name    = arguments[1]
 
-        response = connection.send_request('dumpwallet', {'name':name})
+        response = connection.send_request('newpasswallet', {'name':name})
         print("alert: server responded with '"+response.response+"'.")
         if response.response == 'failed':
             print("reason: " + response.reason)
