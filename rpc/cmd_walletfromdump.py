29c44ac0f0993fb1f953ff74d53766276c031624
Hammurabi <abdullahafadhil@gmail.com>
2021-03-24 00:37:45 +0100

    Update cmd_walletfromdump.py

--- rpc/cmd_walletfromdump.py
+++ rpc/cmd_walletfromdump.py
@@ -8,9 +8,9 @@ def parse(cmd, arguments, connection):
     if len(arguments) != 2:
         print("error: '"+cmd.name+"' requires one argument.")
     else:
-        address  = arguments[1]
+        dump     = arguments[1]
 
-        response = connection.send_request('setnoncewallet', {'address':address})
+        response = connection.send_request(cmd.name, {'dump':dump})
         print("alert: server responded with '"+response.response+"'.")
         if response.response == 'failed':
             print("reason: " + response.reason)
