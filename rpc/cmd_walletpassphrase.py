c6ded72c40e3f5b056cc42161aa99454b2a74dba
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 22:32:11 +0100

    Update cmd_walletpassphrase.py

--- rpc/cmd_walletpassphrase.py
+++ rpc/cmd_walletpassphrase.py
@@ -3,7 +3,7 @@ import commands
 # password helper
 from getpass import getpass
 
-# define 'walletpassphrasechange' command
+# define 'walletpassphrase' command
 def parse(cmd, arguments, connection):
     if len(arguments) != 1:
         print("error: '"+cmd.name+"' does not take arguments.")
@@ -11,7 +11,7 @@ def parse(cmd, arguments, connection):
         old     = getpass('old password>')
         new     = getpass('new password>')
 
-        response = connection.send_request('walletpassphrasechange', {'name':name, 'old':old, 'new':new})
+        response = connection.send_request('walletpassphrase', {'name':name, 'old':old, 'new':new})
         print("alert: server responded with '"+response.response+"'.")
         if response.response == 'failed':
             print("reason: " + response.reason)
