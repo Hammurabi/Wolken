e7999ddd947daf50cd8417e54ad481698830e130
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 14:15:49 +0100

    Create cmd_node.py

---
+++ rpc/cmd_node.py
@@ -0,0 +1,14 @@
+# import commands
+import commands
+
+# registers all commands to the command manager
+def register_all(cmdManager):
+    register_node(cmdManager)
+    register_getblock(cmdManager)
+    register_gettx(cmdManager)
+def register_node(cmdManager):
+    pass
+def register_getblock(cmdManager):
+    pass
+def register_gettx(cmdManager):
+    pass
