8b574d389dedaf17210d8448834d187766c2240c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-24 00:40:11 +0100

    Update cmd_loadwallet.py

--- rpc/cmd_loadwallet.py
+++ rpc/cmd_loadwallet.py
@@ -3,7 +3,7 @@ import commands
 # password helper
 from getpass import getpass
 
-# define 'walletpassphrase' command
+# define 'loadwallet' command
 def parse(cmd, arguments, connection):
     if len(arguments) != 2:
         print("error: '"+cmd.name+"' requires one argument.")
