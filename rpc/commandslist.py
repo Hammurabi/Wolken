f2d4f9efe9865e0faa3126b14c26f7da43a23e5e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 19:13:56 +0100

    Update commandslist.py

--- rpc/commandslist.py
+++ rpc/commandslist.py
@@ -4,15 +4,10 @@ import requests
 import util
 # import commands
 import commands
+import cmd_node
 
 # registers all commands to the command manager
 def register_all(cmdManager):
     register_node(cmdManager)
     register_getblock(cmdManager)
     register_gettx(cmdManager)
-def register_node(cmdManager):
-    pass
-def register_getblock(cmdManager):
-    pass
-def register_gettx(cmdManager):
-    pass
