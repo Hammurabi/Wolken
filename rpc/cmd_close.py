a64faa042e89462f126a30bb6860b76629fe9add
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 19:53:59 +0100

    Update cmd_close.py

--- rpc/cmd_close.py
+++ rpc/cmd_close.py
@@ -7,6 +7,7 @@ def parse(cmd, arguments, connection):
     if len(arguments) != 1:
         print("error: '"+cmd.name+"' requires only one argument.")
     else:
+        response = connection.send_request()
         print("alert: closing down client.")
         quit(0)
 
