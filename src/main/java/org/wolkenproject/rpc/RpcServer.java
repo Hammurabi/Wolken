c05a4b0b94cc76c67ffd5443249a6adc66f92512
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 20:09:24 +0100

    Update RpcServer.java

--- src/main/java/org/wolkenproject/rpc/RpcServer.java
+++ src/main/java/org/wolkenproject/rpc/RpcServer.java
@@ -79,7 +79,10 @@ public class RpcServer {
         JSONObject request = msg.getFormattedQuery();
         JSONObject response= new JSONObject();
 
-        if (request.getString("request").equals("getblock")) {
+        if (request.getString("request").equals("close")) {
+
+        }
+        else if (request.getString("request").equals("getblock")) {
         } else if (request.getString("request").equals("gettx")) {
         } else if (request.getString("request").equals("server")) {
             response.put("response", "success");
