873c81701380d5cc0ff94a0add41f190f3bfdbcc
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 21:36:54 +0100

    Create cmd_createwallet.py

---
+++ rpc/cmd_createwallet.py
@@ -0,0 +1,17 @@
+# import commands
+import commands
+# password helper
+from getpass import getpass
+
+
+# define 'createwallet' command
+def parse(cmd, arguments, connection):
+    if len(arguments) != 1:
+        print("error: '"+cmd.name+"' requires only one argument.")
+    else:
+        password = getpass('password>')
+        response = connection.send_request('close', {'password':password})
+        print("alert: server responded with '"+response.response+"'.")
+        print("alert: closing down client.")
+        quit(0)
+
