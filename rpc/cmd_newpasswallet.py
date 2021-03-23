f2dd4e96fb4db1c7ed957265f955fdb26e19d5c6
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 22:01:19 +0100

    Update cmd_newpasswallet.py

--- rpc/cmd_newpasswallet.py
+++ rpc/cmd_newpasswallet.py
@@ -5,14 +5,14 @@ from getpass import getpass
 
 # define 'newpasswallet' command
 def parse(cmd, arguments, connection):
-    if len(arguments) != 3:
-        print("error: '"+cmd.name+"' requires two arguments.")
+    if len(arguments) != 2:
+        print("error: '"+cmd.name+"' requires one argument.")
     else:
         name    = arguments[1]
 
         old     = getpass('old password>')
         new     = getpass('new password>')
-        
+
         response = connection.send_request('newpasswallet', {'name':name, 'old':old, 'new':new})
         print("alert: server responded with '"+response.response+"'.")
         if response.response == 'failed':
