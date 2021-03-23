09afac046128fd49aa7d313c418f14cec6ad89c4
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 21:40:12 +0100

    Update cmd_createwallet.py

--- rpc/cmd_createwallet.py
+++ rpc/cmd_createwallet.py
@@ -6,9 +6,11 @@ from getpass import getpass
 
 # define 'createwallet' command
 def parse(cmd, arguments, connection):
-    if len(arguments) != 2:
-        print("error: '"+cmd.name+"' requires two argument.")
+    if len(arguments) != 2 and len(arguments) != 3:
+        print("error: '"+cmd.name+"' requires atleast one argument.")
     else:
+        encrypt = arguments[2].lower() == 'false'
+
         password = getpass('password>')
         response = connection.send_request('close', {'password':password})
         print("alert: server responded with '"+response.response+"'.")
