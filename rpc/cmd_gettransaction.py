6357429e7a6903451672c1d16be1ec2257dbf50b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-24 00:59:56 +0100

    Update cmd_gettransaction.py

--- rpc/cmd_gettransaction.py
+++ rpc/cmd_gettransaction.py
@@ -1,21 +1,22 @@
 # import commands
 import commands
 # password helper
+import json
 from getpass import getpass
 
-# define 'signtransaction' command
+# define 'gettransaction' command
 def parse(cmd, arguments, connection):
     if len(arguments) != 2:
         print("error: '"+cmd.name+"' requires one argument.")
     else:
         transaction = arguments[1]
 
-        response    = connection.send_request(cmd.name, {'transaction':transaction})
+        response    = connection.send_request(cmd.name, {'txid':transaction})
         print("alert: server responded with '"+response.response+"'.")
         if response.response == 'failed':
             print("reason: " + response.reason)
         else:
             print("---------------------------------")
-            print(response.response)
+            print()
             print("---------------------------------")
 
