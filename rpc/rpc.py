e6dc4679c14c0fc01d0d11b8729494e6a7d8717e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 14:15:52 +0100

    Update rpc.py

--- rpc/rpc.py
+++ rpc/rpc.py
@@ -29,23 +29,6 @@ def start():
             return lambda x : None
         # parse the command
         arguments   = text.split(" ")
-# define 'connect' command
-def node_parse(command, arguments):
-    if len(arguments) != 3:
-        print("error: 'auth' requires two arguments.")
-    else:
-        if not util.is_valid_ip(arguments[1]):
-            print("error: 'node' requires the first argument to be a valid IP address.")
-            pass
-        if not arguments[2].isnumeric():
-            print("error: 'node' requires the second argument to be a valid port.")
-            pass
-        global ip
-        global port
-        ip      = arguments[1]
-        port    = arguments[2]
-
-        print("alert: node connection data set to ('"+arguments[1]+":"+arguments[2]+"')")
 # define 'exit' command
 def exit_parse(command, arguments):
     print("alert: terminating process")
@@ -73,7 +56,6 @@ def getblock_parse(command, arguments):
         print("error: 'getblock' command missing arguments.")
         pass
     
-
 # define a basic command list
 commands_list = [   
                     new_command('node', node_parse),
