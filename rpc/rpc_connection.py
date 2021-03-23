0ff3ee9256e9adab316057221baf3f1ead6422f8
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 19:38:39 +0100

    Update rpc_connection.py

--- rpc/rpc_connection.py
+++ rpc/rpc_connection.py
@@ -1,6 +1,7 @@
 # import requests to make our lives easier
 import requests
 
+# define a 'connection' class to hold all the connection information
 class rpc_connection:
     def __init__(self):
         self.ip      = 'localhost'
