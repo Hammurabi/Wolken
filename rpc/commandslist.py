f45383953a4a5a1e67cc76518918821db433a2ae
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 14:12:55 +0100

    Update commandslist.py

--- rpc/commandslist.py
+++ rpc/commandslist.py
@@ -10,3 +10,9 @@ def register_all(cmdManager):
     register_node(cmdManager)
     register_getblock(cmdManager)
     register_gettx(cmdManager)
+def register_node(cmdManager):
+    pass
+def register_getblock(cmdManager):
+    pass
+def register_gettx(cmdManager):
+    pass
