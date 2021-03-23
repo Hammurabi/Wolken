ab16ce7e9c00ba22426ef49d0b04255ef333b57b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-24 00:36:58 +0100

    Create cmd_walletfromdump.py

---
+++ rpc/cmd_walletfromdump.py
@@ -0,0 +1,17 @@
+# import commands
+import commands
+# password helper
+from getpass import getpass
+
+# define 'getaccount' command
+def parse(cmd, arguments, connection):
+    if len(arguments) != 2:
+        print("error: '"+cmd.name+"' requires one argument.")
+    else:
+        address  = arguments[1]
+
+        response = connection.send_request('setnoncewallet', {'address':address})
+        print("alert: server responded with '"+response.response+"'.")
+        if response.response == 'failed':
+            print("reason: " + response.reason)
+
