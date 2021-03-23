afebc7d6844f5c44c3c0c0b5b0328f03f334e298
Hammurabi <abdullahafadhil@gmail.com>
2021-03-23 20:03:06 +0100

    Update cmd_close.py

--- rpc/cmd_close.py
+++ rpc/cmd_close.py
@@ -4,13 +4,14 @@ import commands
 from getpass import getpass
 
 
-# define 'node' command
+# define 'close' command
 def parse(cmd, arguments, connection):
     if len(arguments) != 1:
         print("error: '"+cmd.name+"' requires only one argument.")
     else:
         password = getpass('password>')
-        response = connection.send_request('close', {'password':password})
+        args     = {'password':password}
+        response = connection.send_request('close', args)
         print("alert: server responded with '"+response.response+"'.")
         print("alert: closing down client.")
         quit(0)
