b618d46b937c36615a6fdb20ae7db33a0936f91a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-24 21:25:42 +0100

    Update cmd_broadcastblock.py

--- rpc/cmd_broadcastblock.py
+++ rpc/cmd_broadcastblock.py
@@ -3,14 +3,14 @@ import commands
 # password helper
 from getpass import getpass
 
-# define 'broadcasttransaction' command
+# define 'broadcastblock' command
 def parse(cmd, arguments, connection):
     if len(arguments) != 2:
         print("error: '"+cmd.name+"' requires one argument.")
     else:
         transaction = arguments[1]
 
-        response    = connection.send_request(cmd.name, {'transaction':transaction})
+        response    = connection.post_request(cmd.name, {'transaction':transaction})
         print("alert: server responded with '"+response.response+"'.")
         if response.response == 'failed':
             print("reason: " + response.reason)
