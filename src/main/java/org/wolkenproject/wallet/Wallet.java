a1d0e1a714780765b46f7c8bbe70ede615529d5d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-24 02:26:58 +0100

    Update Wallet.java

--- src/main/java/org/wolkenproject/wallet/Wallet.java
+++ src/main/java/org/wolkenproject/wallet/Wallet.java
@@ -7,6 +7,8 @@ import org.wolkenproject.crypto.Key;
 import org.wolkenproject.crypto.Keypair;
 import org.wolkenproject.crypto.ec.ECKeypair;
 import org.wolkenproject.crypto.ec.ECPrivateKey;
+import org.wolkenproject.encoders.Base16;
+import org.wolkenproject.encoders.Base58;
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.utils.Utils;
 
@@ -75,5 +77,13 @@ public class Wallet {
     }
 
     public JSONObject toJson() {
+        JSONObject json = new JSONObject();
+        json.put("name", name);
+        json.put("privatekey", Base16.encode(privateKey));
+        json.put("publickey", Base16.encode(publicKey.getEncoded()));
+        json.put("address", Base58.encode(address.getFormatted()));
+        json.put("nonce", nonce);
+
+        return json;
     }
 }
