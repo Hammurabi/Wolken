f58b6ab2520ac7887b091e12b66454e8e34f0e7f
Hammurabi <abdullahafadhil@gmail.com>
2021-03-24 03:00:51 +0100

    Update ECPrivateKey.java

--- src/main/java/org/wolkenproject/crypto/ec/ECPrivateKey.java
+++ src/main/java/org/wolkenproject/crypto/ec/ECPrivateKey.java
@@ -1,5 +1,10 @@
 package org.wolkenproject.crypto.ec;
 
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+import org.bouncycastle.jce.spec.ECPrivateKeySpec;
+import org.wolkenproject.crypto.CryptoLib;
 import org.wolkenproject.crypto.CryptoUtil;
 import org.wolkenproject.crypto.Key;
 import org.wolkenproject.exceptions.WolkenException;
@@ -7,12 +12,13 @@ import org.wolkenproject.utils.HashUtil;
 import org.wolkenproject.utils.Utils;
 
 import java.math.BigInteger;
+import java.security.KeyFactory;
 
 public class ECPrivateKey extends Key {
     private final byte key[];
 
     public ECPrivateKey() {
-        this(HashUtil.sha256(CryptoUtil.makeSecureBytes(1024)));
+        this(generateValidKey());
     }
 
     public ECPrivateKey(byte key[]) {
@@ -42,5 +48,28 @@ public class ECPrivateKey extends Key {
     @Override
     public Key getDecompressed() throws WolkenException {
         return this;
+    }
+
+    public static byte[] generateValidKey() {
+        byte bytes[] = HashUtil.sha256(CryptoUtil.makeSecureBytes(1024));
+
+        try {
+            ECParameterSpec ecParameterSpec = ECNamedCurveTable.getParameterSpec("secp256k1");
+
+            ECPrivateKeySpec privateKeySpec = new ECPrivateKeySpec(new BigInteger(1, bytes), ecParameterSpec);
+            KeyFactory keyFactory = KeyFactory.getInstance("EC");
+            BCECPrivateKey key = (BCECPrivateKey) keyFactory.generatePrivate(privateKeySpec);
+
+            while (key.getD().bitLength() > CryptoLib.getCurve().getN().bitLength()) {
+                bytes = HashUtil.sha256(CryptoUtil.makeSecureBytes(1024));
+
+                privateKeySpec = new ECPrivateKeySpec(new BigInteger(1, bytes), ecParameterSpec);
+                keyFactory = KeyFactory.getInstance("EC");
+                key = (BCECPrivateKey) keyFactory.generatePrivate(privateKeySpec);
+            }
+        } catch (Exception e) {
+        }
+
+        return bytes;
     }
 }
