c318112d473147ce03133a1d20cc8c9047e4db5a
Hammurabi <abdullahafadhil@gmail.com>
2021-03-24 20:24:21 +0100

    Update Transaction.java

--- src/main/java/org/wolkenproject/core/transactions/Transaction.java
+++ src/main/java/org/wolkenproject/core/transactions/Transaction.java
@@ -22,6 +22,10 @@ import java.util.List;
 public abstract class Transaction extends SerializableI implements Comparable<Transaction> {
     public static int UniqueIdentifierLength = 32;
 
+    public static Transaction fromJson(JSONObject transaction) {
+        return null;
+    }
+
     public static final class Flags
     {
         public static final int
