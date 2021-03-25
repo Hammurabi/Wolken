632399363b3ace462fde4b4f51794344a8020f70
Hammurabi <abdullahafadhil@gmail.com>
2021-03-26 00:18:56 +0100

    Update Miner.java

--- src/main/java/org/wolkenproject/core/Miner.java
+++ src/main/java/org/wolkenproject/core/Miner.java
@@ -2,6 +2,8 @@ package org.wolkenproject.core;
 
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.utils.ChainMath;
+import org.wolkenproject.utils.HashUtil;
+import org.wolkenproject.utils.Utils;
 
 public class Miner extends AbstractMiner {
     public Miner(Address miningAddress) {
@@ -12,10 +14,18 @@ public class Miner extends AbstractMiner {
     public void mine(Block block) throws WolkenException {
         BlockHeader header  = block;
         byte hash[]         = header.getHashCode();
+        int nonce           = 0;
+        byte headerBytes[]  = header.asByteArray();
 
         while (!ChainMath.validSolution(hash, header.getBits())) {
-            header.setNonce(block.getNonce() + 1);
-            hash            = header.getHashCode();
+            ++ nonce;
+            headerBytes[76] = (byte) (nonce >> 24 & 0xFF);
+            headerBytes[77] = (byte) (nonce >> 16 & 0xFF);
+            headerBytes[78] = (byte) (nonce >> 8 & 0xFF);
+            headerBytes[79] = (byte) (nonce & 0xFF);
+            hash            = HashUtil.sha256d(headerBytes);
         }
+
+        header.setNonce(nonce);
     }
 }
