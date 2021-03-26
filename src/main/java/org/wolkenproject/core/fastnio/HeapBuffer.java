a564f03244f463e2d3c9e204042fdac5bb56acc4
Hammurabi <abdullahafadhil@gmail.com>
2021-03-26 18:15:24 +0100

    Update HeapBuffer.java

--- src/main/java/org/wolkenproject/core/fastnio/HeapBuffer.java
+++ src/main/java/org/wolkenproject/core/fastnio/HeapBuffer.java
@@ -48,6 +48,14 @@ class HeapBuffer extends Buffer {
 
     @Override
     public void putLong(int offset, long value) {
+        buffer[offset] = (byte) (value >> 56 & 0xFF);
+        buffer[offset + 1] = (byte) (value >> 48 & 0xFF);
+        buffer[offset + 2] = (byte) (value >> 40 & 0xFF);
+        buffer[offset + 3] = (byte) (value >> 32 & 0xFF);
+        buffer[offset + 4] = (byte) (value >> 24 & 0xFF);
+        buffer[offset + 5] = (byte) (value >> 16 & 0xFF);
+        buffer[offset + 6] = (byte) (value >> 8 & 0xFF);
+        buffer[offset + 7] = (byte) (value & 0xFF);
     }
 
     @Override
