a7c5b802a61dd24e70270025d0ec7eed9d694974
Hammurabi <abdullahafadhil@gmail.com>
2021-03-26 18:08:12 +0100

    Update Buffer.java

--- src/main/java/org/wolkenproject/core/fastnio/Buffer.java
+++ src/main/java/org/wolkenproject/core/fastnio/Buffer.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Field;
 
 public abstract class Buffer {
     static final Unsafe UnsafeInstance = getUnsafe();
+    private int position;
 
     private static final Unsafe getUnsafe() {
         try {
@@ -26,4 +27,26 @@ public abstract class Buffer {
     public static Buffer createUnsafeBuffer(int length) {
         return null;
     }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void flip() {
+        setPosition(0);
+    }
+
+    public abstract void put(int offset, int value);
+    public abstract void put(int offset, byte value[]);
+    public abstract void putShort(int offset, int value);
+    public abstract void putChar(int offset, int value);
+    public abstract void putInt(int offset, int value);
+    public abstract void putLong(int offset, long value);
+
+    public abstract int get(int offset);
+    public abstract void get(int offset, byte out[]);
+    public abstract int getShort(int offset);
+    public abstract int getChar(int offset);
+    public abstract int getInt(int offset);
+    public abstract long getLong(int offset);
 }
