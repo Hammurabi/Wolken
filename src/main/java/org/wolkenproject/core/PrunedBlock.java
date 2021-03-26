5b22c3d5379e7c1964e13f9099cbee0e335cfc59
Hammurabi <abdullahafadhil@gmail.com>
2021-03-26 02:49:10 +0100

    Update PrunedBlock.java

--- src/main/java/org/wolkenproject/core/PrunedBlock.java
+++ src/main/java/org/wolkenproject/core/PrunedBlock.java
@@ -6,4 +6,10 @@ public class PrunedBlock {
     private final BlockHeader   blockHeader;
     private final List<byte[]>  transactions;
     private final List<byte[]>  events;
+
+    public PrunedBlock(BlockHeader blockHeader, List<byte[]> transactions, List<byte[]> events) {
+        this.blockHeader = blockHeader;
+        this.transactions = transactions;
+        this.events = events;
+    }
 }
