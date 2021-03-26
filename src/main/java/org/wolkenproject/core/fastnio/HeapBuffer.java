843a83d4806c2785b857ac5d30f41b30a6104759
Hammurabi <abdullahafadhil@gmail.com>
2021-03-26 18:08:33 +0100

    Update HeapBuffer.java

--- src/main/java/org/wolkenproject/core/fastnio/HeapBuffer.java
+++ src/main/java/org/wolkenproject/core/fastnio/HeapBuffer.java
@@ -1,4 +1,56 @@
 package org.wolkenproject.core.fastnio;
 
 public class HeapBuffer extends Buffer {
+    @Override
+    public void put(int offset, int value) {
+    }
+
+    @Override
+    public void put(int offset, byte[] value) {
+    }
+
+    @Override
+    public void putShort(int offset, int value) {
+    }
+
+    @Override
+    public void putChar(int offset, int value) {
+    }
+
+    @Override
+    public void putInt(int offset, int value) {
+    }
+
+    @Override
+    public void putLong(int offset, long value) {
+    }
+
+    @Override
+    public int get(int offset) {
+        return 0;
+    }
+
+    @Override
+    public void get(int offset, byte[] out) {
+    }
+
+    @Override
+    public int getShort(int offset) {
+        return 0;
+    }
+
+    @Override
+    public int getChar(int offset) {
+        return 0;
+    }
+
+    @Override
+    public int getInt(int offset) {
+        return 0;
+    }
+
+    @Override
+    public long getLong(int offset) {
+        return 0;
+    }
 }
