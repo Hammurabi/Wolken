79dce3c9d70e503ef816c9dd647cfc0a03c1b2aa
Hammurabi <abdullahafadhil@gmail.com>
2021-03-26 18:39:04 +0100

    Create BlockStore.java

---
+++ src/main/java/org/wolkenproject/core/BlockStore.java
@@ -0,0 +1,63 @@
+package org.wolkenproject.core;
+
+import org.wolkenproject.core.fastnio.Buffer;
+
+public class BlockStore {
+    private static final int MaxLength = 128_000_000;
+    private final Buffer data;
+
+    public BlockStore() {
+        this(0, 0, 0, 0, 0, 0);
+    }
+
+    public BlockStore(byte data[]) {
+        this.data = Buffer.wrap(data);
+    }
+
+    public BlockStore(int numberOfBlocks, int lowestHeightBlock, int highestBlock, int lowestTimestampBlock, int highestTimestampBlock, int index) {
+        int maxBlocks = MaxLength / Context.getInstance().getNetworkParameters().getMaxBlockSize();
+        this.data = Buffer.createBuffer(maxBlocks * 8 + 22);
+        this.data.putInt(0, numberOfBlocks);
+        this.data.putInt(4, lowestHeightBlock);
+        this.data.putInt(8, highestBlock);
+        this.data.putInt(12, lowestTimestampBlock);
+        this.data.putInt(16, highestTimestampBlock);
+        this.data.putShort(20, index);
+    }
+
+    public int getBlockLocation(int block) {
+        return data.getInt(block * 8 + 22);
+    }
+
+    public int getBlockSize(int block) {
+        return data.getInt(block * 8 + 26);
+    }
+
+    public boolean hasSpaceRemaining(int length) {
+        return (getIndex() + length) < MaxLength;
+    }
+
+    public int getNumberOfBlocks() {
+        return data.getInt(0);
+    }
+
+    public int getLowestBlock() {
+        return data.getInt(4);
+    }
+
+    public int getHighestBlock() {
+        return data.getInt(8);
+    }
+
+    public int getLowestTimestamp() {
+        return data.getInt(12);
+    }
+
+    public int getHighestTimestamp() {
+        return data.getInt(16);
+    }
+
+    public int getIndex() {
+        return data.getShort(20);
+    }
+}
