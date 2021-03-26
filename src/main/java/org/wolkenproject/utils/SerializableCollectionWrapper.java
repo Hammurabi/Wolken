7e5cec21be1773526319e2b15ff623e8418aa7e7
Hammurabi <abdullahafadhil@gmail.com>
2021-03-26 04:37:29 +0100

    Update SerializableCollectionWrapper.java

--- src/main/java/org/wolkenproject/utils/SerializableCollectionWrapper.java
+++ src/main/java/org/wolkenproject/utils/SerializableCollectionWrapper.java
@@ -1,12 +1,35 @@
 package org.wolkenproject.utils;
 
+import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collection;
 
 public class SerializableCollectionWrapper<T> extends SerializableI {
     private Collection<T> collection;
 
     public SerializableCollectionWrapper(Collection<T> collection) {
+        this.collection = collection;
+    }
+
+    @Override
+    public void write(OutputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public void read(InputStream stream) throws IOException, WolkenException {
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return null;
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return 0;
     }
 }
