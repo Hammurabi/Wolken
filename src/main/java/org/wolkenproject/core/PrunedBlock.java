6937c5a85275b81ed9f19c82f22bffe874cf302c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-27 16:44:19 +0100

    Update PrunedBlock.java

--- src/main/java/org/wolkenproject/core/PrunedBlock.java
+++ src/main/java/org/wolkenproject/core/PrunedBlock.java
@@ -1,23 +1,24 @@
 package org.wolkenproject.core;
 
+import java.util.Collection;
 import java.util.Set;
 
 public class PrunedBlock {
-    private final BlockHeader   blockHeader;
-    private final Set<byte[]>   transactions;
-    private final Set<byte[]>   events;
+    private final BlockHeader           blockHeader;
+    private final Collection<byte[]>    transactions;
+    private final Collection<byte[]>    events;
 
-    public PrunedBlock(BlockHeader blockHeader, Set<byte[]> transactions, Set<byte[]> events) {
+    public PrunedBlock(BlockHeader blockHeader, Collection<byte[]> transactions, Collection<byte[]> events) {
         this.blockHeader = blockHeader;
         this.transactions = transactions;
         this.events = events;
     }
 
-    public Set<byte[]> getTransactions() {
+    public Collection<byte[]> getTransactions() {
         return transactions;
     }
 
-    public Set<byte[]> getEvents() {
+    public Collection<byte[]> getEvents() {
         return events;
     }
 
