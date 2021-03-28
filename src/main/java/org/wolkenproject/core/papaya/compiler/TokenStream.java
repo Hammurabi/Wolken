c9d338d2716288fb7fe0b5759da14452969aa6b8
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 01:27:08 +0200

    Update TokenStream.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/TokenStream.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/TokenStream.java
@@ -1,10 +1,7 @@
 package org.wolkenproject.core.papaya.compiler;
 
-import org.wolkenproject.exceptions.WolkenException;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class TokenStream {
     private List<Token> tokenList;
@@ -13,6 +10,7 @@ public class TokenStream {
         this.tokenList = new ArrayList<>();
     }
 
-    public void add(StringBuilder builder, int line, int offset, Map<String, TokenType> typeMap) throws WolkenException {
+    public void add(Token token) {
+        tokenList.add(token);
     }
 }
