493e46e243282302a79e5984a65aa39da32d7508
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 00:58:16 +0200

    Update PapayaLexer.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
@@ -1,21 +1,32 @@
 package org.wolkenproject.core.papaya.compiler;
 
-import static org.wolkenproject.core.papaya.compiler.TokenType.None;
-
 public class PapayaLexer {
     public TokenStream ingest(String data) {
         TokenStream tokenStream = new TokenStream();
 
         StringBuilder   builder = new StringBuilder();
-        TokenType       type    = None;
         char            lastChar= '\0';
+        int             line    = 1;
+        int             offset  = 0;
 
         for (char character : data.toCharArray()) {
-            if (lastChar == ' ') {
-            } else {
+            offset ++;
+
+            if (character == '\n') {
+                if (builder.length() != 0) {
+                    tokenStream.add(builder, line, offset);
+                    builder = new StringBuilder();
+                }
+
+                offset = 1;
             }
 
-            lastChar = character;
+            if (character == ' ') {
+                if (builder.length() != 0) {
+                    tokenStream.add(builder, line, offset);
+                    builder = new StringBuilder();
+                }
+            }
         }
     }
 }
