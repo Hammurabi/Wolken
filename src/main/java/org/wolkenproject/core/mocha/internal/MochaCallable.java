b3c16c245c8411a5f9a0121cf27be6750dcbd261
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 00:24:55 +0200

    Delete MochaCallable.java

--- src/main/java/org/wolkenproject/core/mocha/internal/MochaCallable.java
+++
@@ -1,9 +0,0 @@
-package org.wolkenproject.core.mocha.internal;
-
-import org.wolkenproject.core.mocha.Scope;
-import org.wolkenproject.exceptions.InvalidTransactionException;
-import org.wolkenproject.exceptions.MochaException;
-
-public interface MochaCallable {
-    MochaObject call(Scope scope) throws MochaException, InvalidTransactionException;
-}
