7a35a81f29bf4ea4923fd2e12727bb69bcc4d683
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 00:28:18 +0200

    Update ResourceManager.java

--- src/main/java/org/wolkenproject/core/ResourceManager.java
+++ src/main/java/org/wolkenproject/core/ResourceManager.java
@@ -1,12 +1,15 @@
 package org.wolkenproject.core;
 
-import org.wolkenproject.utils.FileService;
-
 import java.io.IOException;
 import java.io.InputStream;
 
 public class ResourceManager {
     public InputStream get(String path) throws IOException {
-        return getClass().getResourceAsStream(path);
+        InputStream inputStream = getClass().getResourceAsStream(path);
+        if (inputStream == null) {
+            throw new IOException("'" + path + "' could no be reached.");
+        }
+
+        return inputStream;
     }
 }
