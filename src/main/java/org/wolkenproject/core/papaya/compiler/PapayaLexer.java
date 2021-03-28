b7a6bbd41482a07ace940fd79685add4981ef99f
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 01:42:15 +0200

    Update PapayaLexer.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
@@ -89,12 +89,14 @@ public class PapayaLexer {
         tokenType.put("~", TokenType.NotSymbol);
         tokenType.put("and", TokenType.LogicalAndSymbol);
         tokenType.put("\\&\\&", TokenType.LogicalAndSymbol);
+        tokenType.put("\\&\\&\\=", TokenType.LogicalAndEqualsSymbol);
         tokenType.put("or", TokenType.LogicalOrSymbol);
         tokenType.put("\\|\\|", TokenType.LogicalOrSymbol);
+        tokenType.put("\\|\\|\\=", TokenType.LogicalOrEqualsSymbol);
         tokenType.put("\\>\\>\\>", TokenType.UnsignedRightShiftSymbol);
         tokenType.put("\\>\\>", TokenType.RightShiftSymbol);
         tokenType.put("\\<\\<", TokenType.LeftShiftSymbol);
-        
+
 //        tokenType.put("N([A-z]|[1|2|3|4|5|6|7|8|9])+", TokenType.Base58String);
 
         return tokenType;
