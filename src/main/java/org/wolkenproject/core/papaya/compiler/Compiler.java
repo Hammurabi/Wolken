253f1c9b4ff2d430dcf4c3740643a28cc061aa9e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 00:32:51 +0200

    Update Compiler.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/Compiler.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/Compiler.java
@@ -1,9 +1,12 @@
 package org.wolkenproject.core.papaya.compiler;
 
+import org.json.JSONObject;
 import org.wolkenproject.exceptions.WolkenException;
 
-public class Compiler {
+public abstract class Compiler {
     public static Compiler getFor(String language) throws WolkenException {
         throw new WolkenException("compiler for language '" + language + "' could not be found.");
     }
+
+    public abstract CompiledScript compile(String text, JSONObject compilerArguments);
 }
