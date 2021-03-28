bebb074866f886ce1a2c4effd6448e14c3804c71
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 00:27:14 +0200

    Update ResourceManager.java

--- src/main/java/org/wolkenproject/core/ResourceManager.java
+++ src/main/java/org/wolkenproject/core/ResourceManager.java
@@ -1,9 +1,12 @@
 package org.wolkenproject.core;
 
+import org.wolkenproject.utils.FileService;
+
+import java.io.IOException;
 import java.io.InputStream;
 
 public class ResourceManager {
-    public InputStream get(String path) {
+    public InputStream get(String path) throws IOException {
         return getClass().getResourceAsStream(path);
     }
 }
