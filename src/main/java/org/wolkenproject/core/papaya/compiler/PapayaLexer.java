b0379ab42ec362290609de8eb313c133c34d0ce3
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 00:42:53 +0200

    Update PapayaLexer.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
@@ -1,4 +1,8 @@
 package org.wolkenproject.core.papaya.compiler;
 
+import org.wolkenproject.core.assets.Token;
+
 public class PapayaLexer {
+    public Iterator<Token> ingest(String data) {
+    }
 }
