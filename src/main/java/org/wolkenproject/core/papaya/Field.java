1fb74253d098c87dfefeb11ae4d5e60c9c3ac59d
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 00:28:47 +0200

    Create Field.java

---
+++ src/main/java/org/wolkenproject/core/papaya/Field.java
@@ -0,0 +1,26 @@
+package org.wolkenproject.core.papaya;
+
+public class Field {
+    public String getName() {
+        return name;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public static final class Visibility {
+        public static final int
+                Private     = 0,
+                Public      = 1,
+                Protected   = 2;
+    }
+
+    private String  name;
+    private int     visibility;
+
+    public Field(String name, int visibility) {
+        this.name       = name;
+        this.visibility = visibility;
+    }
+}
