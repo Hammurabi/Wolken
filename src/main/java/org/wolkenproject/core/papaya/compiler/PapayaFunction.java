57e4976bab13ddaf4882b31e20c8fd4537272932
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 08:17:32 +0200

    Update PapayaFunction.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaFunction.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaFunction.java
@@ -1,4 +1,33 @@
 package org.wolkenproject.core.papaya.compiler;
 
+import java.util.Set;
+
 public class PapayaFunction {
+    private final String                name;
+    private final Set<PapayaField>      arguments;
+    private final Set<PapayaStatement>  statementSet;
+    private final LineInfo              lineInfo;
+
+    public PapayaFunction(String name, Set<PapayaField> arguments, Set<PapayaStatement> statementSet, LineInfo lineInfo) {
+        this.name = name;
+        this.arguments = arguments;
+        this.statementSet = statementSet;
+        this.lineInfo = lineInfo;
+    }
+
+    public LineInfo getLineInfo() {
+        return lineInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<PapayaField> getArguments() {
+        return arguments;
+    }
+
+    public Set<PapayaStatement> getStatementSet() {
+        return statementSet;
+    }
 }
