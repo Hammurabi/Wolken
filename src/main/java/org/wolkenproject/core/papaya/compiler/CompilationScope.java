6208ae49215775951c1bc6f67beb53cb7aa9751b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 12:32:20 +0200

    Update CompilationScope.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/CompilationScope.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/CompilationScope.java
@@ -1,4 +1,11 @@
 package org.wolkenproject.core.papaya.compiler;
 
+import java.util.Set;
+
 public class CompilationScope {
+    public void newField(PapayaField field, PapayaStatement assignment) {
+    }
+
+    public void memberCall(String function, Set<PapayaStatement> arguments) {
+    }
 }
