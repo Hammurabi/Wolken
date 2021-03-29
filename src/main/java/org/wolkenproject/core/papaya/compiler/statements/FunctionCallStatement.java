6dd3c313a25bc2e377b6408770e2020200b83368
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 12:32:52 +0200

    Create FunctionCallStatement.java

---
+++ src/main/java/org/wolkenproject/core/papaya/compiler/statements/FunctionCallStatement.java
@@ -0,0 +1,12 @@
+package org.wolkenproject.core.papaya.compiler.statements;
+
+import org.wolkenproject.core.papaya.compiler.LineInfo;
+import org.wolkenproject.core.papaya.compiler.PapayaStatement;
+
+import java.util.Set;
+
+public class FunctionCallStatement extends PapayaStatement {
+    public FunctionCallStatement(String tokenValue, Set<PapayaStatement> arguments, LineInfo lineInfo) {
+        super(scope -> scope.memberCall(tokenValue, arguments), lineInfo);
+    }
+}
