ffc7412d6dd0e3fb7e9552f546db4f488e566bae
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 02:25:55 +0200

    Update PapayaLexer.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
@@ -39,6 +39,9 @@ public class PapayaLexer {
                 }
             }
 
+            if (character == '\'' && builder.length() != 0) {
+            }
+
             lastChar = character;
         }
     }
