b1ac05dd7ff0e9930b3955f07baf8fec90c7d5f2
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 02:47:33 +0200

    Update Token.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/Token.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/Token.java
@@ -3,10 +3,14 @@ package org.wolkenproject.core.papaya.compiler;
 public class Token {
     private final String    tokenValue;
     private final TokenType tokenType;
+    private final int       line;
+    private final int       offset;
 
     public Token(String value, TokenType type, int line, int offset) {
         this.tokenValue = value;
         this.tokenType  = type;
+        this.line       = line;
+        this.offset     = offset;
     }
 
     public String getTokenValue() {
@@ -15,5 +19,15 @@ public class Token {
 
     public TokenType getTokenType() {
         return tokenType;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "tokenValue='" + tokenValue + '\'' +
+                ", tokenType=" + tokenType +
+                ", line=" + line +
+                ", offset=" + offset +
+                '}';
     }
 }
