d8cd54225d482f20e1340b47bf1af59b28f182ec
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 12:02:19 +0200

    Create NewFieldStatement.java

---
+++ src/main/java/org/wolkenproject/core/papaya/compiler/statements/NewFieldStatement.java
@@ -0,0 +1,9 @@
+package org.wolkenproject.core.papaya.compiler.statements;
+
+import org.wolkenproject.core.papaya.compiler.PapayaField;
+import org.wolkenproject.core.papaya.compiler.PapayaStatement;
+
+public class NewFieldStatement extends PapayaStatement {
+    public NewFieldStatement(PapayaField field, PapayaStatement assignment) {
+    }
+}
