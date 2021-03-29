98ac36833335ea41567f3db20997b72d704418ed
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 08:07:42 +0200

    Update ApplicationBinaryInterface.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/ApplicationBinaryInterface.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/ApplicationBinaryInterface.java
@@ -1,6 +1,13 @@
 package org.wolkenproject.core.papaya.compiler;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ApplicationBinaryInterface {
     // this contains structures in order of declaration
-    private final Map<String, PapayaStructure>  structureMap;
+    private final Map<String, PapayaStructure> structureMap;
+
+    public ApplicationBinaryInterface() {
+        this.structureMap = new LinkedHashMap<>();
+    }
 }
