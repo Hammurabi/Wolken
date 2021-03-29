96621eef10f9ee1f3c0df4c7045ae35387494e0e
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 07:10:41 +0200

    Create papaya.papaya

--- src/main/java/org/wolkenproject/core/papaya/compiler/Compiler.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/Compiler.java
@@ -5,8 +5,12 @@ import org.wolkenproject.exceptions.WolkenException;
 
 public abstract class Compiler {
     public static Compiler getFor(String language) throws WolkenException {
+        if (language.toLowerCase().equals("papaya+v0.01a")) {
+            return new PapayaCompiler(new PapayaLexer(PapayaLexer.getTokenTypes()), new PapayaParser());
+        }
+
         throw new WolkenException("compiler for language '" + language + "' could not be found.");
     }
 
-    public abstract CompiledScript compile(String text, JSONObject compilerArguments);
+    public abstract CompiledScript compile(String text, JSONObject compilerArguments) throws WolkenException;
 }
