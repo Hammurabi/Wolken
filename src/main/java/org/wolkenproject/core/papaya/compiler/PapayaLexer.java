03a423be352b2beaa280385b39a88ef68657a2a2
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 09:01:01 +0200

    Update PapayaLexer.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaLexer.java
@@ -294,7 +294,7 @@ public class PapayaLexer {
         tokenType.put("\\#", TokenType.HashTagSymbol);
         tokenType.put("\\;", TokenType.SemiColonSymbol);
         tokenType.put("\\:\\:", TokenType.StaticMemberAccessSymbol);
-        tokenType.put("\\:\\=", TokenType.SemiColonEqualsSymbol);
+        tokenType.put("\\:\\=", TokenType.ColonEqualsSymbol);
         tokenType.put("\\=\\>", TokenType.LambdaSymbol);
 
         tokenType.put("\\<", TokenType.LessThanSymbol);
