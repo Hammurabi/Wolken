7179da717b67ab61966addd910a8b16b97648c71
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 07:10:47 +0200

    Update PapayaParser.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaParser.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaParser.java
@@ -1,4 +1,9 @@
 package org.wolkenproject.core.papaya.compiler;
 
+import org.wolkenproject.exceptions.WolkenException;
+
 public class PapayaParser {
+    public Token ingest(TokenStream ingest) throws WolkenException {
+        return null;
+    }
 }
