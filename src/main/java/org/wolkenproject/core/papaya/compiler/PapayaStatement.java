ebf82d7e31df8fb5e6698e19f6019acd311eb31c
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 12:02:16 +0200

    Update PapayaStatement.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaStatement.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaStatement.java
@@ -1,4 +1,24 @@
 package org.wolkenproject.core.papaya.compiler;
 
 public class PapayaStatement {
+    private final PapayaStatement left;
+    private final PapayaStatement right;
+
+    public PapayaStatement(PapayaStatement left, PapayaStatement right) {
+        this.left   = left;
+        this.right  = right;
+    }
+
+    public void compile(CompilationScope scope) {
+        left.compile(scope);
+        right.compile(scope);
+    }
+
+    public PapayaStatement getLeft() {
+        return left;
+    }
+
+    public PapayaStatement getRight() {
+        return right;
+    }
 }
