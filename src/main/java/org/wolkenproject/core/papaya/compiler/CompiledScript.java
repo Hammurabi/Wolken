c185608f2d868a12f2d76840ac83dcad19836b2b
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 12:32:25 +0200

    Update CompiledScript.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/CompiledScript.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/CompiledScript.java
@@ -1,4 +1,13 @@
 package org.wolkenproject.core.papaya.compiler;
 
+import java.io.ByteArrayOutputStream;
+
 public class CompiledScript {
+    private final ByteArrayOutputStream compilationResult;
+    private final PapayaApplication     application;
+
+    public CompiledScript(PapayaApplication application) {
+        this.application = application;
+        this.compilationResult = new ByteArrayOutputStream();
+    }
 }
