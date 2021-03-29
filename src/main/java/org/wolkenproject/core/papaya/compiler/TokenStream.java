6d1899d6b9d09fe290cc676a781062939de19769
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 07:40:44 +0200

    Update TokenStream.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/TokenStream.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/TokenStream.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class TokenStream {
     private List<Token> tokenList;
+    private int index;
 
     public TokenStream() {
         this.tokenList = new ArrayList<>();
@@ -17,5 +18,35 @@ public class TokenStream {
     @Override
     public String toString() {
         return tokenList.toString();
+    }
+
+    public boolean hasNext() {
+        return index < tokenList.size();
+    }
+
+    public boolean hasPrevious() {
+        return index >= 0;
+    }
+
+    public Token next() {
+        return tokenList.get(index ++);
+    }
+
+    public Token previous() {
+        return tokenList.get(index - 1);
+    }
+
+    public boolean matches(TokenType ...pattern) {
+        if (index + pattern.length < tokenList.size()) {
+            for (int i = 0; i < pattern.length; i ++) {
+                if (tokenList.get(i + index).getTokenType() != pattern[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
