19f6a43d9560bbf29f65692cc7d152583f649f77
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 08:07:19 +0200

    Update PapayaStructure.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaStructure.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaStructure.java
@@ -1,14 +1,34 @@
 package org.wolkenproject.core.papaya.compiler;
 
+import org.wolkenproject.exceptions.WolkenException;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class PapayaStructure {
-    private final StructureType             structureType;
-    private final Map<String, PapayaField>  fieldMap;
+    private final StructureType                 structureType;
+    private final Map<String, PapayaField>      fieldMap;
+    private final Map<String, PapayaFunction>   functionMap;
 
     public PapayaStructure(StructureType structureType) {
         this.structureType  = structureType;
         this.fieldMap       = new LinkedHashMap<>();
+        this.functionMap    = new LinkedHashMap<>();
+    }
+
+    public void addField(String name, PapayaField field) throws WolkenException {
+        if (fieldMap.containsKey(name)) {
+            throw new WolkenException("redeclaration of field '" + name + "' {"+field+"}.");
+        }
+
+        fieldMap.put(name, field);
+    }
+
+    public void addFunction(String name, PapayaFunction function) throws WolkenException {
+        if (fieldMap.containsKey(name)) {
+            throw new WolkenException("redeclaration of function '" + name + "' {"+function+"}.");
+        }
+
+        functionMap.put(name, function);
     }
 }
