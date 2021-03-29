cd2efb5932cdf84654b2361c271398818bb408b2
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 08:03:57 +0200

    Create ApplicationBinaryInterface.java

---
+++ src/main/java/org/wolkenproject/core/papaya/compiler/ApplicationBinaryInterface.java
@@ -0,0 +1,6 @@
+package org.wolkenproject.core.papaya.compiler;
+
+public class ApplicationBinaryInterface {
+    // this contains structures in order of declaration
+    private final Map<String, PapayaStructure>  structureMap;
+}
