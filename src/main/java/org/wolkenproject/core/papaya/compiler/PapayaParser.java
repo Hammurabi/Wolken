f8bf0885d836827b96aa9acec08228616e742bc4
Hammurabi <abdullahafadhil@gmail.com>
2021-03-29 07:39:12 +0200

    Update PapayaParser.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaParser.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaParser.java
@@ -2,8 +2,24 @@ package org.wolkenproject.core.papaya.compiler;
 
 import org.wolkenproject.exceptions.WolkenException;
 
+import static org.wolkenproject.core.papaya.compiler.TokenType.*;
+
 public class PapayaParser {
-    public Token ingest(TokenStream ingest) throws WolkenException {
+    public Token ingest(TokenStream stream) throws WolkenException {
+        return recursive(stream);
+    }
+
+    private Token recursive(TokenStream stream) throws WolkenException {
+        while (stream.hasNext()) {
+            if (stream.matches(ContractKeyword, Identifier)) { // contact declaration
+            } else if (stream.matches(ClassKeyword, Identifier)) { // class declaration
+            } else if (stream.matches(StructKeyword, Identifier)) { // struct declaration
+            } else if (stream.matches(FunctionKeyword, Identifier)) { // function declaration
+            } else if (stream.matches(Identifier, Identifier)) { // field declaration
+            } else if (stream.matches(Identifier, SemiColonEqualsSymbol)) { // local field declaration a:=b
+            }
+        }
+
         return null;
     }
 }
