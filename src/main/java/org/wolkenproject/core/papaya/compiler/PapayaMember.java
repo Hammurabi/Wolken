0bf5bc351c8f7a91407510076eed15eed0742a08
Hammurabi <abdullahafadhil@gmail.com>
2021-03-30 06:18:33 +0200

    Update PapayaMember.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaMember.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaMember.java
@@ -3,7 +3,13 @@ package org.wolkenproject.core.papaya.compiler;
 import org.wolkenproject.core.papaya.AccessModifier;
 
 public class PapayaMember {
+    private final AccessModifier accessModifier;
+
+    public PapayaMember(AccessModifier accessModifier) {
+        this.accessModifier = accessModifier;
+    }
+
     public AccessModifier getAccessModifier() {
-        return null;
+        return accessModifier;
     }
 }
