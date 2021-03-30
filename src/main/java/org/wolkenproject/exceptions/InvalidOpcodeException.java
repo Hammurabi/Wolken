aaa2ee9b5fe4d478c4c487d9e8a976c59dfd95d1
Hammurabi <abdullahafadhil@gmail.com>
2021-03-30 06:06:57 +0200

    Update InvalidOpcodeException.java

--- src/main/java/org/wolkenproject/exceptions/InvalidOpcodeException.java
+++ src/main/java/org/wolkenproject/exceptions/InvalidOpcodeException.java
@@ -1,6 +1,6 @@
 package org.wolkenproject.exceptions;
 
-public class InvalidOpcodeException extends MochaException {
+public class InvalidOpcodeException extends PapayaException {
     public InvalidOpcodeException(String msg) {
         super(msg);
     }
