9d93fd6aa0ba5123fc972bba9716899e9cd74efa
Hammurabi <abdullahafadhil@gmail.com>
2021-03-30 06:03:15 +0200

    Update MochaCallable.java

--- src/main/java/org/wolkenproject/core/papaya/internal/MochaCallable.java
+++ src/main/java/org/wolkenproject/core/papaya/internal/MochaCallable.java
@@ -2,8 +2,8 @@ package org.wolkenproject.core.papaya.internal;
 
 import org.wolkenproject.core.papaya.Scope;
 import org.wolkenproject.exceptions.InvalidTransactionException;
-import org.wolkenproject.exceptions.MochaException;
+import org.wolkenproject.exceptions.PapayaException;
 
 public interface MochaCallable {
-    MochaObject call(Scope scope) throws MochaException, InvalidTransactionException;
+    MochaObject call(Scope scope) throws PapayaException, InvalidTransactionException;
 }
