f1723e3e1a9ff60eea0c7d461acd4ac35ebc5354
Hammurabi <abdullahafadhil@gmail.com>
2021-03-30 23:25:21 +0200

    Update PapayaField.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaField.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaField.java
@@ -1,13 +1,15 @@
 package org.wolkenproject.core.papaya.compiler;
 
-public class PapayaField {
+import org.wolkenproject.core.papaya.AccessModifier;
+
+public class PapayaField extends PapayaMember {
     private final String name;
     private final String typeName;
     private final LineInfo lineInfo;
     private final PapayaStatement valueAssignment;
-    private int position;
 
-    public PapayaField(String name, String typeName, LineInfo lineInfo, PapayaStatement valueAssignment) {
+    public PapayaField(AccessModifier accessModifier, String name, String typeName, LineInfo lineInfo, PapayaStatement valueAssignment) {
+        super(accessModifier, name);
         this.name = name;
         this.typeName = typeName;
         this.lineInfo = lineInfo;
@@ -26,11 +28,11 @@ public class PapayaField {
         return typeName;
     }
 
-    public PapayaStatement getValueAssignment() {
-        return valueAssignment;
+    public String getType() {
+        return typeName;
     }
 
-    public void setPosition(int position) {
-        this.position = position;
+    public PapayaStatement getValueAssignment() {
+        return valueAssignment;
     }
 }
