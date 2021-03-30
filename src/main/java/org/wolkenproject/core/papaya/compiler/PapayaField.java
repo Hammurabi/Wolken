ddf48f0eb0ac2975af90f4c82ba832b003bb79de
Hammurabi <abdullahafadhil@gmail.com>
2021-03-30 06:02:36 +0200

    Update PapayaField.java

--- src/main/java/org/wolkenproject/core/papaya/compiler/PapayaField.java
+++ src/main/java/org/wolkenproject/core/papaya/compiler/PapayaField.java
@@ -5,6 +5,7 @@ public class PapayaField {
     private final String typeName;
     private final LineInfo lineInfo;
     private final PapayaStatement valueAssignment;
+    private int position;
 
     public PapayaField(String name, String typeName, LineInfo lineInfo, PapayaStatement valueAssignment) {
         this.name = name;
@@ -15,5 +16,21 @@ public class PapayaField {
 
     public LineInfo getLineInfo() {
         return lineInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public PapayaStatement getValueAssignment() {
+        return valueAssignment;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
     }
 }
