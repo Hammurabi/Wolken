e29775e67c3f1807195a35c8c6f58ef499116114
Hammurabi <abdullahafadhil@gmail.com>
2021-03-31 06:22:30 +0200

    Create MochaPublicKey.java

---
+++ src/main/java/org/wolkenproject/papaya/internal/MochaPublicKey.java
@@ -0,0 +1,16 @@
+package org.wolkenproject.papaya.internal;
+
+import org.wolkenproject.papaya.MochaBool;
+import org.wolkenproject.crypto.Key;
+
+public class MochaPublicKey extends MochaObject {
+    private Key             publicKey;
+
+    public MochaPublicKey(Key key) {
+        this.publicKey = key;
+    }
+
+    public MochaObject checkSignature(MochaCryptoSignature signature, byte signatureData[]) {
+        return new MochaBool(signature.getSignature().checkSignature(signatureData, publicKey));
+    }
+}
