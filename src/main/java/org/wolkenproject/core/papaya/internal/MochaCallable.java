126f555047c339b8b02154bbf8ce4244e8ea8740
Hammurabi <abdullahafadhil@gmail.com>
2021-03-31 06:19:32 +0200

    Delete MochaCallable.java

--- src/main/java/org/wolkenproject/core/papaya/internal/MochaCallable.java
+++
@@ -1,9 +0,0 @@
-package org.wolkenproject.core.papaya.internal;
-
-import org.wolkenproject.core.papaya.Scope;
-import org.wolkenproject.exceptions.InvalidTransactionException;
-import org.wolkenproject.exceptions.PapayaException;
-
-public interface MochaCallable {
-    MochaObject call(Scope scope) throws PapayaException, InvalidTransactionException;
-}
