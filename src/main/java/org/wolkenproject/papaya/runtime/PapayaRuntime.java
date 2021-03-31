5a3c85d05cdb424954bc474ca58eb7083a807082
Hammurabi <abdullahafadhil@gmail.com>
2021-03-31 10:35:46 +0200

    Create PapayaRuntime.java

---
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaRuntime.java
@@ -0,0 +1,11 @@
+package org.wolkenproject.papaya.runtime;
+
+import org.wolkenproject.papaya.compiler.PapayaApplication;
+
+public class PapayaRuntime {
+    private PapayaApplication application;
+
+    public PapayaRuntime(PapayaApplication application) {
+        this.application = application;
+    }
+}
