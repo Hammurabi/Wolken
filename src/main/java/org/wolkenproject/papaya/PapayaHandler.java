12c4455390171fc672952102456c07f992b06248
Hammurabi <abdullahafadhil@gmail.com>
2021-03-31 06:22:56 +0200

    Create PapayaHandler.java

---
+++ src/main/java/org/wolkenproject/papaya/PapayaHandler.java
@@ -0,0 +1,23 @@
+package org.wolkenproject.papaya;
+
+import org.wolkenproject.papaya.compiler.PapayaStructure;
+import org.wolkenproject.exceptions.PapayaIllegalAccessException;
+import org.wolkenproject.exceptions.WolkenException;
+
+import java.util.Stack;
+
+public abstract class PapayaHandler {
+    private final PapayaObject papayaObject;
+
+    public PapayaHandler(PapayaObject papayaObject) {
+        this.papayaObject = papayaObject;
+    }
+
+    public abstract void setMember(int memberId, PapayaHandler member, Stack<PapayaStructure> stackTrace) throws PapayaIllegalAccessException;
+    public abstract PapayaHandler getMember(int memberId, Stack<PapayaStructure> stackTrace) throws PapayaIllegalAccessException;
+    public abstract void call(Scope scope) throws WolkenException;
+
+    public PapayaObject getPapayaObject() {
+        return papayaObject;
+    }
+}
