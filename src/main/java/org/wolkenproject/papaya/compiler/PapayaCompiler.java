f8abc84655a7a8ec75308ee5f720e241a93246ff
Hammurabi <abdullahafadhil@gmail.com>
2021-03-31 06:20:59 +0200

    Create PapayaCompiler.java

---
+++ src/main/java/org/wolkenproject/papaya/compiler/PapayaCompiler.java
@@ -0,0 +1,21 @@
+package org.wolkenproject.papaya.compiler;
+
+import org.json.JSONObject;
+import org.wolkenproject.exceptions.WolkenException;
+
+public class PapayaCompiler extends Compiler {
+    private PapayaLexer lexer;
+    private PapayaParser parser;
+
+    public PapayaCompiler(PapayaLexer papayaLexer, PapayaParser papayaParser) {
+        super();
+        this.lexer = papayaLexer;
+        this.parser= papayaParser;
+    }
+
+    @Override
+    public CompiledScript compile(String text, JSONObject compilerArguments) throws WolkenException {
+        PapayaApplication application = parser.ingest(lexer.ingest(text));
+        return application.compile();
+    }
+}
