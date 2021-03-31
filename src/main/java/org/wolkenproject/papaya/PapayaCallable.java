09ea62ff18c79ffb7015e79a5016e6058fbb9f38
Hammurabi <abdullahafadhil@gmail.com>
2021-03-31 10:34:55 +0200

    Delete PapayaCallable.java

--- src/main/java/org/wolkenproject/papaya/PapayaCallable.java
+++
@@ -1,7 +0,0 @@
-package org.wolkenproject.papaya;
-
-import org.wolkenproject.exceptions.WolkenException;
-
-public interface PapayaCallable {
-    public void call(Scope scope) throws WolkenException;
-}
