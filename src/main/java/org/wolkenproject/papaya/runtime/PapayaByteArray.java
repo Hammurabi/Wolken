300c3c7c8e476b953f7174d8ee7de6d966e4266f
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 08:43:52 +0200

    Update PapayaByteArray.java

--- src/main/java/org/wolkenproject/papaya/runtime/PapayaByteArray.java
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaByteArray.java
@@ -1,11 +1,5 @@
 package org.wolkenproject.papaya.runtime;
 
-import org.wolkenproject.exceptions.PapayaException;
-import org.wolkenproject.exceptions.PapayaIllegalAccessException;
-import org.wolkenproject.papaya.compiler.PapayaStructure;
-
-import java.util.Stack;
-
 public class PapayaByteArray extends PapayaObject {
     public PapayaByteArray(byte[] bytes) {
         super();
