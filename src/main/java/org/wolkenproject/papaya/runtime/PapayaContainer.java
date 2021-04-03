af75df1cea21003800330a09be14e82dc81289b5
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 17:41:10 +0200

    Update PapayaContainer.java

--- src/main/java/org/wolkenproject/papaya/runtime/PapayaContainer.java
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaContainer.java
@@ -2,8 +2,12 @@ package org.wolkenproject.papaya.runtime;
 
 import org.wolkenproject.exceptions.PapayaException;
 
+import java.math.BigInteger;
+
 public interface PapayaContainer {
     public PapayaObject getAtIndex(int index) throws PapayaException;
     public void setAtIndex(int index, PapayaHandler handler) throws PapayaException;
     public void append(PapayaHandler object);
+    public BigInteger asInt();
+    public BigInteger asSignedInt();
 }
