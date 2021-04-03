d6314cbb7ef8ac55049372de077481b74bd8a71f
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 09:59:37 +0200

    Update PapayaByteArray.java

--- src/main/java/org/wolkenproject/papaya/runtime/PapayaByteArray.java
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaByteArray.java
@@ -1,7 +1,17 @@
 package org.wolkenproject.papaya.runtime;
 
+import org.wolkenproject.exceptions.PapayaException;
+
 public class PapayaByteArray extends PapayaObject {
+    private PapayaContainer container;
+
     public PapayaByteArray(byte[] bytes) {
         super();
+        this.container = new ByteContainer(bytes);
+    }
+
+    @Override
+    public PapayaContainer asContainer() throws PapayaException {
+        return container;
     }
 }
