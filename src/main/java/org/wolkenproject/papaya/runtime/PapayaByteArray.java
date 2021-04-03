9b75e882302c137c4a3842412bc9b9ee991cfe04
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 17:41:01 +0200

    Update PapayaByteArray.java

--- src/main/java/org/wolkenproject/papaya/runtime/PapayaByteArray.java
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaByteArray.java
@@ -2,6 +2,8 @@ package org.wolkenproject.papaya.runtime;
 
 import org.wolkenproject.exceptions.PapayaException;
 
+import java.math.BigInteger;
+
 public class PapayaByteArray extends PapayaObject {
     private PapayaContainer container;
 
@@ -13,5 +15,10 @@ public class PapayaByteArray extends PapayaObject {
     @Override
     public PapayaContainer asContainer() throws PapayaException {
         return container;
+    }
+
+    @Override
+    public BigInteger asInt() {
+        return container.asInt();
     }
 }
