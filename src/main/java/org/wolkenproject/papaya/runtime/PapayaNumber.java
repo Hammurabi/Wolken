da75af2d182bcbefe4b6fb593c8ed981433212e0
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 18:22:54 +0200

    Update PapayaNumber.java

--- src/main/java/org/wolkenproject/papaya/runtime/PapayaNumber.java
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaNumber.java
@@ -3,18 +3,28 @@ package org.wolkenproject.papaya.runtime;
 import java.math.BigInteger;
 
 public class PapayaNumber extends PapayaObject {
-    private final BigInteger number;
+    private final BigInteger    number;
+    private boolean             allowSignedOperations;
 
-    public PapayaNumber(long number) {
-        this(new BigInteger(Long.toString(number)));
+    public PapayaNumber(long number, boolean allowSignedOperations) {
+        this(fromLong(number, allowSignedOperations), allowSignedOperations);
     }
 
-    public PapayaNumber(BigInteger number) {
-        this.number = number;
+    public PapayaNumber(BigInteger number, boolean allowSignedOperations) {
+        this.number                 = number;
+        this.allowSignedOperations  = allowSignedOperations;
     }
 
     @Override
     public BigInteger asInt() {
         return number;
+    }
+
+    protected static BigInteger fromLong(long number, boolean allowSignedOperations) {
+        if (allowSignedOperations) {
+            return new BigInteger(Long.toString(number));
+        }
+
+        return new BigInteger(Long.toUnsignedString(number));
     }
 }
