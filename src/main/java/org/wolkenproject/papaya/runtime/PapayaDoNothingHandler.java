b9a38994d105739c36cca305d26ff8ee9ce54832
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 09:01:15 +0200

    Update PapayaDoNothingHandler.java

--- src/main/java/org/wolkenproject/papaya/runtime/PapayaDoNothingHandler.java
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaDoNothingHandler.java
@@ -1,5 +1,6 @@
 package org.wolkenproject.papaya.runtime;
 
+import org.wolkenproject.exceptions.PapayaException;
 import org.wolkenproject.exceptions.PapayaIllegalAccessException;
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.papaya.compiler.PapayaStructure;
@@ -12,17 +13,17 @@ public class PapayaDoNothingHandler extends PapayaHandler {
     }
 
     @Override
-    public void setMember(int memberId, PapayaHandler member, Stack<PapayaStructure> stackTrace) throws PapayaIllegalAccessException {
+    public void setMember(byte memberId[], PapayaHandler member, Stack<PapayaStructure> stackTrace) throws PapayaIllegalAccessException {
         getPapayaObject().setMember(memberId, member, stackTrace);
     }
 
     @Override
-    public PapayaHandler getMember(int memberId, Stack<PapayaStructure> stackTrace) throws PapayaIllegalAccessException {
+    public PapayaHandler getMember(byte memberId[], Stack<PapayaStructure> stackTrace) throws PapayaIllegalAccessException {
         return getPapayaObject().getMember(memberId, stackTrace);
     }
 
     @Override
-    public void call(Scope scope) throws WolkenException {
+    public void call(Scope scope) throws PapayaException {
         getPapayaObject().call(scope);
     }
 }
