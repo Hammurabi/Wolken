ef0a96d002a5952c142e07a61cd044c40ef79bdf
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 16:51:00 +0200

    Update PapayaReadOnlyWrapper.java

--- src/main/java/org/wolkenproject/papaya/runtime/PapayaReadOnlyWrapper.java
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaReadOnlyWrapper.java
@@ -30,7 +30,7 @@ public class PapayaReadOnlyWrapper extends PapayaHandler {
 
     @Override
     public PapayaHandler getAtIndex(int index) throws PapayaException {
-        return new PapayaReadOnlyWrapper(getPapayaObject().asContainer().getAtIndex(index, AccessModifier.ReadOnly));
+        return new PapayaReadOnlyWrapper(getPapayaObject().asContainer().getAtIndex(index));
     }
 
     @Override
