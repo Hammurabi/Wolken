2d634a70e0ded6c45215a7b3e924c99b94b9fac1
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 06:33:16 +0200

    Update ProgramCounter.java

--- src/main/java/org/wolkenproject/papaya/runtime/ProgramCounter.java
+++ src/main/java/org/wolkenproject/papaya/runtime/ProgramCounter.java
@@ -43,6 +43,10 @@ public class ProgramCounter {
         return VarInt.readCompactUint64(preserveAllBits, program);
     }
 
+    public BigInteger nextVarint32(boolean preserveAllBits) throws EmptyProgramCounterException, WolkenException, IOException {
+        return VarInt.readCompactUInt32(preserveAllBits, program);
+    }
+
     public int nextShort() throws EmptyProgramCounterException {
         if (remaining() >= 2) {
             return program.getShort();
