f4b6262a639abefd1aa008be609151dafa10f316
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 04:58:12 +0200

    Update RandomAccessInputStream.java

--- src/main/java/org/wolkenproject/utils/RandomAccessInputStream.java
+++ src/main/java/org/wolkenproject/utils/RandomAccessInputStream.java
@@ -2,5 +2,5 @@ package org.wolkenproject.utils;
 
 import java.io.InputStream;
 
-public class RandomAccessInputStream implements InputStream {
+public class RandomAccessInputStream extends InputStream {
 }
