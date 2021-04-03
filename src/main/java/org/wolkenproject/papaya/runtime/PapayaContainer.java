a04789c03a76eb95d33d573a5237848109bd850a
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 10:11:57 +0200

    Update PapayaContainer.java

--- src/main/java/org/wolkenproject/papaya/runtime/PapayaContainer.java
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaContainer.java
@@ -4,6 +4,6 @@ import org.wolkenproject.exceptions.PapayaException;
 
 public interface PapayaContainer {
     public PapayaObject getAtIndex(int index) throws PapayaException;
-    public void setAtIndex(int index, PapayaHandler handler);
+    public void setAtIndex(int index, PapayaHandler handler) throws PapayaException;
     public void append(PapayaHandler object);
 }
