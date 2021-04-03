ad9cdd32e361792941ef083c06b6fb029e3975a7
Hammurabi <abdullahafadhil@gmail.com>
2021-04-03 10:18:57 +0200

    Create PapayaObjectContainer.java

---
+++ src/main/java/org/wolkenproject/papaya/runtime/PapayaObjectContainer.java
@@ -0,0 +1,37 @@
+package org.wolkenproject.papaya.runtime;
+
+import org.wolkenproject.exceptions.PapayaException;
+
+import java.util.Arrays;
+
+public class PapayaObjectContainer implements PapayaContainer {
+    private PapayaObject objects[];
+
+    public PapayaObjectContainer(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    @Override
+    public PapayaObject getAtIndex(int index) throws PapayaException {
+        if (index >= objects.length) {
+            throw new PapayaException("array index out of '" + objects.length + "' bounds.");
+        }
+
+        return objects[index];
+    }
+
+    @Override
+    public void setAtIndex(int index, PapayaHandler handler) throws PapayaException {
+        if (index >= objects.length) {
+            throw new PapayaException("array index out of '" + objects.length + "' bounds.");
+        }
+
+        objects[index] = handler.getPapayaObject();
+    }
+
+    @Override
+    public void append(PapayaHandler object) {
+        objects = Arrays.copyOf(objects, objects.length + 1);
+        objects[objects.length - 1] = object.getPapayaObject();
+    }
+}
