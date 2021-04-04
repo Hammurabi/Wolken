e997614760adefb946ca68add878b58d61b402fd
Hammurabi <abdullahafadhil@gmail.com>
2021-04-05 00:39:51 +0200

    Update VarIntTests.java

--- src/test/java/org/wolkenproject/VarIntTests.java
+++ src/test/java/org/wolkenproject/VarIntTests.java
@@ -2,12 +2,15 @@ package org.wolkenproject;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.utils.BigMath;
 import org.wolkenproject.utils.Utils;
 import org.wolkenproject.utils.VarInt;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 
 public class VarIntTests {
     @Test
@@ -88,7 +91,7 @@ public class VarIntTests {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
         long ints[] = new long[] {
-                0, 1, 2, 5, 10, 20, 22, 30, 32, 40, 60, 120, 12000, 24000, 124000, 1000000, 1000000000, 1000000000L, 90000000000L, 190000000000L, 3190000000000L, 203190000000000L, 1355203190000000000L,
+                0, 1, 2, 5, 10, 20, 22, 30, 31, 32, 40, 60, 120, 12000, 24000, 124000, 1000000, 1000000000, 1000000000L, 90000000000L, 190000000000L, 3190000000000L, 203190000000000L, 1355203190000000000L,
                 21_000_000__00_000_000_000L
         };
 
@@ -103,6 +106,33 @@ public class VarIntTests {
 
         for (long i : ints) {
             Assertions.assertEquals(i, VarInt.readCompactUInt64(true, inputStream));
+            Assertions.assertEquals(inputStream.read(empty), empty.length);
+            Assertions.assertTrue(Utils.isEmpty(empty), "array should consist of zeros.");
+        }
+    }
+
+    @Test
+    public void testReadWriteLossy128() throws IOException, WolkenException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        BigInteger ints[] = new BigInteger[] {
+                BigInteger.ZERO, BigInteger.ONE, BigInteger.ONE.shiftLeft(2), BigInteger.ONE.shiftLeft(3), BigInteger.ONE.shiftLeft(4), BigInteger.ONE.shiftLeft(8),
+                BigInteger.ONE.shiftLeft(12), BigInteger.ONE.shiftLeft(16), BigInteger.ONE.shiftLeft(20), BigInteger.ONE.shiftLeft(24), BigInteger.ONE.shiftLeft(28),
+                BigInteger.ONE.shiftLeft(32), BigInteger.ONE.shiftLeft(36), BigInteger.ONE.shiftLeft(40).subtract(BigInteger.ONE), BigInteger.ONE.shiftLeft(46), BigInteger.ONE.shiftLeft(57),
+                BigInteger.ONE.shiftLeft(63), BigInteger.ONE.shiftLeft(64), BigInteger.ONE.shiftLeft(120).subtract(BigInteger.ONE), BigInteger.ONE.shiftLeft(120), BigInteger.ONE.shiftLeft(125), BigInteger.ONE.shiftLeft(124).subtract(BigInteger.ONE)
+        };
+
+        byte empty[] = new byte[12];
+
+        for (BigInteger i : ints) {
+            VarInt.writeCompactUint128(i, false, outputStream);
+            outputStream.write(empty);
+        }
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+        for (BigInteger i : ints) {
+            Assertions.assertEquals(i, VarInt.readCompactUint128(false, inputStream));
             Assertions.assertEquals(inputStream.read(empty), empty.length);
             Assertions.assertTrue(Utils.isEmpty(empty), "array should consist of zeros.");
         }
