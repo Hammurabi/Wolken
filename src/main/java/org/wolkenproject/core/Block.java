11d59ce998769b20ee7b54b3587b6194e322d762
Hammurabi <abdullahafadhil@gmail.com>
2021-04-04 08:54:14 +0200

    Update Block.java

--- src/main/java/org/wolkenproject/core/Block.java
+++ src/main/java/org/wolkenproject/core/Block.java
@@ -21,7 +21,6 @@ import java.util.zip.DeflaterOutputStream;
 public class Block implements Iterable<Transaction> {
     private static BigInteger       LargestHash             = BigInteger.ONE.shiftLeft(256);
     public static int               UniqueIdentifierLength  = 32;
-    private BlockHeader             blockHeader;
     private Set<Transaction>        transactions;
     private BlockStateChangeResult  stateChange;
 
