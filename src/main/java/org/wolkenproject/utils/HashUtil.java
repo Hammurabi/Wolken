b056ab9a588597ab3ff00b2d18ee40dab176736c
Hammurabi <abdullahafadhil@gmail.com>
2021-04-04 07:16:52 +0200

    Update HashUtil.java

--- src/main/java/org/wolkenproject/utils/HashUtil.java
+++ src/main/java/org/wolkenproject/utils/HashUtil.java
@@ -1,12 +1,17 @@
 package org.wolkenproject.utils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.bouncycastle.crypto.digests.SHA512Digest;
 import org.bouncycastle.crypto.macs.HMac;
 import org.bouncycastle.crypto.params.KeyParameter;
+import org.wolkenproject.exceptions.WolkenException;
 
 public class HashUtil {
     public static byte[] sha256(byte data[])
@@ -118,16 +123,41 @@ public class HashUtil {
         }
     }
 
+    public static byte[] reduceBits(byte msg[]) {
+        final byte alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_".getBytes();
+        Map<Byte, Integer> mapping = new HashMap<>();
+        mapping.put((byte) 0, mapping.size());
+        for (byte b : alphabet) {
+            mapping.put(b, mapping.size());
+        }
+
+        BitOutputStream stream = new BitOutputStream();
+        try {
+            for (int i = 0; i < msg.length; i ++) {
+                    stream.write(mapping.get(msg[i]), 6);
+            }
+        } catch (IOException e) {
+            return null;
+        }
+
+        return stream.toByteArray();
+    }
+
     // returns a hash of any string 'A-z|0-1'
     // will not have any collisions
     // finds the index of a given string in
     // n-dimensional space.
-    public static byte[] enHash(byte msg[]) {
+    public static byte[] hammurabi(byte msg[]) {
         final byte alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_".getBytes();
+        Map<Byte, Integer> mapping = new HashMap<>();
+        mapping.put((byte) 0, mapping.size());
+        for (byte b : alphabet) {
+            mapping.put(b, mapping.size());
+        }
+
         final int blockSize = 5;
         final int singleDim = alphabet.length;
-        final int numBlocks = (msg.length / blockSize + 1) * blockSize;
-        final BigInteger secondDim = new BigInteger(Integer.toString(alphabet.length)).pow(numBlocks);
+        final int numBlocks = (msg.length / blockSize + 1);
         final int paddedLen = numBlocks * blockSize;
 
         final byte padded[] = Arrays.copyOf(msg, paddedLen);
@@ -135,38 +165,33 @@ public class HashUtil {
 
         for (int i = 0; i < padded.length; i += blockSize) {
             // prepare the block
-            int indexOf0 = Arrays.binarySearch(alphabet, padded[i]);
-            int indexOf1 = Arrays.binarySearch(alphabet, padded[i + 1]);
-            int indexOf2 = Arrays.binarySearch(alphabet, padded[i + 2]);
-            int indexOf3 = Arrays.binarySearch(alphabet, padded[i + 3]);
-            int indexOf4 = Arrays.binarySearch(alphabet, padded[i + 4]);
+            int indexOf0 = mapping.get(padded[i]);
+            int indexOf1 = mapping.get(padded[i + 1]);
+            int indexOf2 = mapping.get(padded[i + 2]);
+            int indexOf3 = mapping.get(padded[i + 3]);
+            int indexOf4 = mapping.get(padded[i + 4]);
 
-            // array = a0 b0 c0 d0 e0 a1 b1 c1 d1 e1
-            // index = 0 + len * 0 1 1 0
-            // x + WIDTH * (y + DEPTH * z)
-
-            blocks[i / numBlocks] =
+            // map 5d to 1d
+            blocks[i / blockSize] =
                     singleDim*singleDim*singleDim*singleDim * indexOf0
-                  + singleDim*singleDim*singleDim * indexOf1
-                  + singleDim*singleDim * indexOf2
-                  + singleDim * indexOf3
-                  + indexOf4
+                  + singleDim*singleDim*singleDim           * indexOf1
+                  + singleDim*singleDim                     * indexOf2
+                  + singleDim                               * indexOf3
+                  +                                           indexOf4
                     ;
         }
 
-        BigInteger result = BigInteger.ZERO;
-
+        // calculate compact hash
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
         for (int i = 0; i < numBlocks; i ++) {
-            result = result.add(secondDim.pow(numBlocks - (i + 1)).multiply(new BigInteger(1, Utils.takeApart(blocks[i]))));
+            try {
+                // 2 bits of precision is lost but since block dimension size is 63^5 30 bits should suffice.
+                VarInt.writeCompactUInt32(blocks[i], false, stream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        byte hash[] = result.toByteArray();
-        if (hash[0] == 0) {
-            byte temp[] = new byte[hash.length - 1];
-            System.arraycopy(hash, 1, temp, 0, temp.length);
-            hash = temp;
-        }
-
-        return hash;
+        return stream.toByteArray();
     }
 }
