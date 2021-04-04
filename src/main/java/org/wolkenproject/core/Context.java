361e59312b7fa6d2aec6ec70e012c88c8c6068b9
Hammurabi <abdullahafadhil@gmail.com>
2021-04-04 07:42:36 +0200

    Update Context.java

--- src/main/java/org/wolkenproject/core/Context.java
+++ src/main/java/org/wolkenproject/core/Context.java
@@ -31,7 +31,7 @@ public class Context {
     private Server                  server;
     private Address                 payList[];
     private BlockChain              blockChain;
-    private OpcodeRegister opcodeRegister;
+    private OpcodeRegister          opcodeRegister;
     private ResourceManager         resourceManager;
     private RpcServer               rpcServer;
     private FileService             fileService;
@@ -52,34 +52,7 @@ public class Context {
         this.resourceManager = new ResourceManager();
         this.compressionEngine = new CompressionEngine();
 
-        Transaction.register(serializationFactory);
-        serializationFactory.registerClass(RecoverableSignature.class, new RecoverableSignature());
-
-        serializationFactory.registerClass(BlockHeader.class, new BlockHeader());
-        serializationFactory.registerClass(Block.class, new Block());
-        serializationFactory.registerClass(BlockIndex.class, new BlockIndex());
-        serializationFactory.registerClass(Ancestors.class, new Ancestors(new byte[Block.UniqueIdentifierLength]));
-
-        serializationFactory.registerClass(NetAddress.class, new NetAddress(InetAddress.getLocalHost(), 0, 0));
-        serializationFactory.registerClass(VersionMessage.class, new VersionMessage());
-        serializationFactory.registerClass(VerackMessage.class, new VerackMessage());
-        serializationFactory.registerClass(VersionInformation.class, new VersionInformation());
-        serializationFactory.registerClass(CheckoutMessage.class, new CheckoutMessage(0));
-
-        serializationFactory.registerClass(BlockList.class, new BlockList(0, new LinkedHashSet<>(), new byte[Message.UniqueIdentifierLength]));
-        serializationFactory.registerClass(FailedToRespondMessage.class, new FailedToRespondMessage(0, 0, new byte[Message.UniqueIdentifierLength]));
-        serializationFactory.registerClass(FoundCommonAncestor.class, new FoundCommonAncestor(new byte[Block.UniqueIdentifierLength], new byte[Message.UniqueIdentifierLength]));
-        serializationFactory.registerClass(HeaderList.class, new HeaderList(0, new LinkedHashSet<>(), new byte[Message.UniqueIdentifierLength]));
-        serializationFactory.registerClass(Inv.class, new Inv(0, 0, new LinkedHashSet<>()));
-        serializationFactory.registerClass(RequestBlocks.class, new RequestBlocks(0, new LinkedHashSet<>()));
-        serializationFactory.registerClass(RequestCommonAncestorChain.class, new RequestCommonAncestorChain(0, new Ancestors(new byte[Block.UniqueIdentifierLength])));
-        serializationFactory.registerClass(RequestHeaders.class, new RequestHeaders(0, new LinkedHashSet<>()));
-        serializationFactory.registerClass(RequestHeadersBefore.class, new RequestHeadersBefore(0, new byte[Block.UniqueIdentifierLength], 0, new BlockHeader()));
-        serializationFactory.registerClass(RequestInv.class, new RequestInv(0));
-        serializationFactory.registerClass(RequestTransactions.class, new RequestTransactions(0, new LinkedHashSet<>()));
-        serializationFactory.registerClass(TransactionList.class, new TransactionList(0, new LinkedHashSet<>(), new byte[Message.UniqueIdentifierLength]));
-        serializationFactory.registerClass(AddressList.class, new AddressList(0, new LinkedHashSet<>()));
-
+        SerializationFactory.register(serializationFactory);
         OpcodeRegister.register(opcodeRegister);
 
         this.blockChain = new BlockChain(this);
