6824baf45a34d5ed69ff7524cc15205fd754c996
Hammurabi <abdullahafadhil@gmail.com>
2021-04-04 08:36:49 +0200

    Create BlockMetadata.java

---
+++ src/main/java/org/wolkenproject/core/BlockMetadata.java
@@ -0,0 +1,45 @@
+package org.wolkenproject.core;
+
+import java.math.BigInteger;
+
+public class BlockMetadata {
+    private int height;
+    private int transactionCount;
+    private int eventCount;
+    private int totalValue;
+    private int fees;
+    private BigInteger chainWork;
+
+    public BlockMetadata(int height, int transactionCount, int eventCount, int totalValue, int fees, BigInteger chainWork) {
+        this.height = height;
+        this.transactionCount = transactionCount;
+        this.eventCount = eventCount;
+        this.totalValue = totalValue;
+        this.fees = fees;
+        this.chainWork = chainWork;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getTransactionCount() {
+        return transactionCount;
+    }
+
+    public int getEventCount() {
+        return eventCount;
+    }
+
+    public int getTotalValue() {
+        return totalValue;
+    }
+
+    public int getFees() {
+        return fees;
+    }
+
+    public BigInteger getChainWork() {
+        return chainWork;
+    }
+}
