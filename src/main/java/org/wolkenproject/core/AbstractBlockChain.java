cc8769f674d9104ca1916e4bf149c0a1c307e029
Hammurabi <abdullahafadhil@gmail.com>
2021-04-05 12:45:21 +0200

    Update AbstractBlockChain.java

--- src/main/java/org/wolkenproject/core/AbstractBlockChain.java
+++ src/main/java/org/wolkenproject/core/AbstractBlockChain.java
@@ -1,29 +1,21 @@
 package org.wolkenproject.core;
 
-import org.wolkenproject.core.transactions.Transaction;
-import org.wolkenproject.encoders.Base16;
-import org.wolkenproject.exceptions.WolkenException;
-import org.wolkenproject.network.Message;
-import org.wolkenproject.network.messages.BlockList;
-import org.wolkenproject.network.messages.Inv;
-import org.wolkenproject.network.messages.RequestBlocks;
-import org.wolkenproject.network.messages.RequestHeadersBefore;
-import org.wolkenproject.utils.Logger;
-import org.wolkenproject.utils.Null;
-import org.wolkenproject.utils.Utils;
-
-import java.math.BigInteger;
-import java.util.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class AbstractBlockChain implements Runnable {
-    // contains rejected blocks.
-    private Context         context;
-    // a mutex
-    private ReentrantLock   mutex;
+    private final Context       context;
+    private final ReentrantLock mutex;
 
     public AbstractBlockChain(Context context) {
         this.context    = context;
         this.mutex      = new ReentrantLock();
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public ReentrantLock getMutex() {
+        return mutex;
     }
 }
