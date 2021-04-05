2cfaa5bcfbc0ba9e4300dfa46ab4afe301ff4a76
Hammurabi <abdullahafadhil@gmail.com>
2021-04-05 12:55:52 +0200

    Update PapayaTests.java

--- src/test/java/org/wolkenproject/PapayaTests.java
+++ src/test/java/org/wolkenproject/PapayaTests.java
@@ -2,7 +2,7 @@ package org.wolkenproject;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.wolkenproject.core.papaya.compiler.PapayaLexer;
+import org.wolkenproject.papaya.compiler.PapayaLexer;
 
 public class PapayaTests {
     @Test
