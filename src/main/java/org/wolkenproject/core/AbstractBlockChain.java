403884c3aa70559d9a3733216b5eb17935f9bd1a
Hammurabi <abdullahafadhil@gmail.com>
2021-04-05 12:56:08 +0200

    Update AbstractBlockChain.java

--- src/main/java/org/wolkenproject/core/AbstractBlockChain.java
+++ src/main/java/org/wolkenproject/core/AbstractBlockChain.java
@@ -1,21 +1,4 @@
 package org.wolkenproject.core;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 public abstract class AbstractBlockChain implements Runnable {
-    private final Context       context;
-    private final ReentrantLock mutex;
-
-    public AbstractBlockChain(Context context) {
-        this.context    = context;
-        this.mutex      = new ReentrantLock();
-    }
-
-    public Context getContext() {
-        return context;
-    }
-
-    public ReentrantLock getMutex() {
-        return mutex;
-    }
 }
