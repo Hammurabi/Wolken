45f3f73ae37092bcfbfe977a560e95abf9ac8604
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 22:42:32 +0200

    Update RegisterAliasEvent.java

--- src/main/java/org/wolkenproject/core/events/RegisterAliasEvent.java
+++ src/main/java/org/wolkenproject/core/events/RegisterAliasEvent.java
@@ -4,7 +4,14 @@ import org.json.JSONObject;
 import org.wolkenproject.core.Context;
 import org.wolkenproject.core.Event;
 import org.wolkenproject.encoders.Base58;
+import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.serialization.SerializableI;
 import org.wolkenproject.utils.Utils;
+import org.wolkenproject.utils.VarInt;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class RegisterAliasEvent extends Event {
     private byte    address[];
@@ -36,5 +43,27 @@ public class RegisterAliasEvent extends Event {
 
     public long getAlias() {
         return alias;
+    }
+
+    @Override
+    public void write(OutputStream stream) throws IOException, WolkenException {
+        stream.write(address);
+        VarInt.writeCompactUInt64(alias, false, stream);
+    }
+
+    @Override
+    public void read(InputStream stream) throws IOException, WolkenException {
+        checkFullyRead(stream.read(address), address.length);
+        alias = VarInt.readCompactUInt64(false, stream);
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return (Type) new RegisterAliasEvent(new byte[address.length], 0);
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return Context.getInstance().getSerialFactory().getSerialNumber(RegisterAliasEvent.class);
     }
 }
