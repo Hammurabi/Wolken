14be6a9c48a60f78dc6ff615f4b1c23a8fbec78d
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 06:53:44 +0200

    Update FullChain.java

--- src/main/java/org/wolkenproject/core/FullChain.java
+++ src/main/java/org/wolkenproject/core/FullChain.java
@@ -1,4 +1,59 @@
 package org.wolkenproject.core;
 
+import java.util.Set;
+
 public class FullChain extends AbstractBlockChain {
+    public FullChain(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean verifyBlock(BlockIndex block) {
+        return false;
+    }
+
+    @Override
+    protected void setBlock(int height, BlockIndex block) {
+
+    }
+
+    @Override
+    protected void markRejected(byte[] block) {
+
+    }
+
+    @Override
+    protected boolean isRejected(byte[] hash) {
+        return false;
+    }
+
+    @Override
+    protected void addOrphan(BlockIndex block) {
+
+    }
+
+    @Override
+    protected void addStale(BlockIndex block) {
+
+    }
+
+    @Override
+    public void suggest(Set<BlockIndex> blocks) {
+
+    }
+
+    @Override
+    public void suggest(BlockIndex block) {
+
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
