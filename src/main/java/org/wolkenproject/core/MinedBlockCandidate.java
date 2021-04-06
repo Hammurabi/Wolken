b5161f7669a802783a29698b2cb913158f7dbea5
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 09:59:55 +0200

    Create MinedBlockCandidate.java

---
+++ src/main/java/org/wolkenproject/core/MinedBlockCandidate.java
@@ -0,0 +1,19 @@
+package org.wolkenproject.core;
+
+public class MinedBlockCandidate extends CandidateBlock {
+    private BlockIndex block;
+
+    public MinedBlockCandidate(BlockIndex block) {
+        this.block = block;
+    }
+
+    @Override
+    public BlockHeader getBlockHeader() {
+        return block.getBlock().getBlockHeader();
+    }
+
+    @Override
+    public BlockIndex getBlock() {
+        return block;
+    }
+}
