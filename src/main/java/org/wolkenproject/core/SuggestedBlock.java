cab539301ef6ffb4d9f70205248db0abc3945d07
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 09:54:07 +0200

    Update SuggestedBlock.java

--- src/main/java/org/wolkenproject/core/SuggestedBlock.java
+++ src/main/java/org/wolkenproject/core/SuggestedBlock.java
@@ -1,8 +1,6 @@
 package org.wolkenproject.core;
 
-import org.wolkenproject.serialization.SerializableI;
-
-public abstract class SuggestedBlock extends SerializableI {
+public abstract class SuggestedBlock {
     public abstract BlockHeader getBlockHeader();
     public abstract BlockIndex getBlock();
 }
