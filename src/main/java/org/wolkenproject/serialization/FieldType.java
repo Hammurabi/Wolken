c6cc55ad03b9be76ad64d8eae281f26f3ef1e013
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 03:32:13 +0200

    Update FieldType.java

--- src/main/java/org/wolkenproject/serialization/FieldType.java
+++ src/main/java/org/wolkenproject/serialization/FieldType.java
@@ -1,4 +1,22 @@
 package org.wolkenproject.serialization;
 
 public enum FieldType {
+    int8,
+    int16,
+    int32,
+    int64,
+    int128,
+    int256,
+
+    uint8,
+    uint16,
+    uint32,
+    uint64,
+    uint128,
+    uint256,
+
+    hash256,
+    hash160,
+
+    bytes
 }
