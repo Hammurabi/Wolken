be8aea10c9b39eb1979c93131a129fce0c2b47a4
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 18:05:15 +0200

    Update CandidateBlockChain.java

--- src/main/java/org/wolkenproject/core/CandidateBlockChain.java
+++ src/main/java/org/wolkenproject/core/CandidateBlockChain.java
@@ -14,25 +14,45 @@ import java.util.List;
 public abstract class CandidateBlockChain implements Comparable<CandidateBlockChain> {
     private final long          sequenceId;
     private final int           transactionCount;
+    private final Context       context;
 
-    protected CandidateBlockChain(int transactionCount) {
+    protected CandidateBlockChain(Context context, int transactionCount) {
         this.sequenceId = System.currentTimeMillis();
         this.transactionCount = transactionCount;
+        this.context = context;
     }
 
+    public Context getContext() {
+        return context;
+    }
     // returns the 'best block' header of the chain.
     public abstract BlockHeader getBlockHeader();
     // returns the 'best block' of the chain.
     public abstract BlockIndex getBlock();
     // returns true if the 'getBlock' will return a value.
     public abstract boolean isFullBlockAvailable();
-    // returns a list of all blocks from the most recent common ancestor to the best block.
+    // returns a list of all blocks from and excluding the most recent common ancestor to and including the best block.
     public abstract List<BlockHeader> getChain();
     // returns true if the 'getChain' will return a value.
     public abstract boolean isChainAvailable();
 
     public BigInteger getTotalChainWork() {
-        return chainWork;
+        if (isChainAvailable()) {
+            List<BlockHeader> chain = getChain();
+            byte mostRecentCommonAncestor[] = chain.get(0).getParentHash();
+            BlockMetadata metadata = getContext().getDatabase().findBlockMetaData(mostRecentCommonAncestor);
+            BigInteger startWork = BigInteger.ZERO;
+
+            if (metadata != null) {
+                startWork = metadata.getChainWork();
+
+                for (BlockHeader header : chain) {
+                    startWork = startWork.add(header.getWork());
+                }
+            }
+        }
+
+        return BigInteger.ZERO;
     }
 
     public long getSequenceId() {
