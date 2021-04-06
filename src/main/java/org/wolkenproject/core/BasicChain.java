0ca61fbad930c15f9c39ec6febfb559fe6767adf
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 18:05:37 +0200

    Update BasicChain.java

--- src/main/java/org/wolkenproject/core/BasicChain.java
+++ src/main/java/org/wolkenproject/core/BasicChain.java
@@ -14,7 +14,7 @@ public abstract class BasicChain extends AbstractBlockChain {
     // contains blocks that were valid pre-fork.
     private HashQueue<BlockIndex>       staleBlocks;
     // contains blocks sent from peers.
-    private HashQueue<CandidateBlockChain>   blockPool;
+    private HashQueue<CandidateBlock>   blockPool;
 
     public BasicChain(Context context) {
         super(context);
