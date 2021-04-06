1bc13217d58e72a1138cdad606099935ea187247
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 09:58:21 +0200

    Update CandidateBlock.java

--- src/main/java/org/wolkenproject/core/CandidateBlock.java
+++ src/main/java/org/wolkenproject/core/CandidateBlock.java
@@ -1,5 +1,13 @@
 package org.wolkenproject.core;
 
+/*
+    Candidate blocks are sent by peers
+    or are locally created when a miner
+    finds a new block, only the PoW is
+    verified before sending it to the
+    BlockChain as a suggestion. So a
+    secondary verification is required.
+ */
 public abstract class CandidateBlock {
     public abstract BlockHeader getBlockHeader();
     public abstract BlockIndex getBlock();
