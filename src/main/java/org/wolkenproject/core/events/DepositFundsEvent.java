a9ad8b65d2ab4a0871dbc7fe532aa64212ffda49
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 22:41:09 +0200

    Update DepositFundsEvent.java

--- src/main/java/org/wolkenproject/core/events/DepositFundsEvent.java
+++ src/main/java/org/wolkenproject/core/events/DepositFundsEvent.java
@@ -4,7 +4,14 @@ import org.json.JSONObject;
 import org.wolkenproject.core.Context;
 import org.wolkenproject.core.Event;
 import org.wolkenproject.encoders.Base58;
+import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.serialization.SerializableI;
 import org.wolkenproject.utils.Utils;
+import org.wolkenproject.utils.VarInt;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class DepositFundsEvent extends Event {
     private byte address[];
@@ -35,5 +42,27 @@ public class DepositFundsEvent extends Event {
     @Override
     public JSONObject toJson() {
         return new JSONObject().put("event", this.getClass().getName()).put("address", Base58.encode(address)).put("amount", amount);
+    }
+
+    @Override
+    public void write(OutputStream stream) throws IOException, WolkenException {
+        stream.write(address);
+        VarInt.writeCompactUInt64(amount, false, stream);
+    }
+
+    @Override
+    public void read(InputStream stream) throws IOException, WolkenException {
+        checkFullyRead(stream.read(address), address.length);
+        amount = VarInt.readCompactUInt64(false, stream);
+    }
+
+    @Override
+    public <Type extends SerializableI> Type newInstance(Object... object) throws WolkenException {
+        return (Type) new DepositFundsEvent(new byte[address.length], 0);
+    }
+
+    @Override
+    public int getSerialNumber() {
+        return Context.getInstance().getSerialFactory().getSerialNumber(DepositFundsEvent.class);
     }
 }
