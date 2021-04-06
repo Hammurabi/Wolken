74102225243954b16fa255834c846f53b15f141c
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 22:32:18 +0200

    Update Event.java

--- src/main/java/org/wolkenproject/core/Event.java
+++ src/main/java/org/wolkenproject/core/Event.java
@@ -2,9 +2,13 @@ package org.wolkenproject.core;
 
 import org.json.JSONObject;
 import org.wolkenproject.serialization.SerializableI;
+import org.wolkenproject.serialization.SerializationFactory;
 import org.wolkenproject.utils.HashUtil;
 
 public abstract class Event extends SerializableI {
+    public static void register(SerializationFactory serializationFactory) {
+    }
+
     public abstract void apply();
     public abstract void undo();
     public abstract byte[] getEventBytes();
