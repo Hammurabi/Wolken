9a2990e632c67dea9179d69129c51d339286e213
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 09:35:07 +0200

    Update FullChain.java

--- src/main/java/org/wolkenproject/core/FullChain.java
+++ src/main/java/org/wolkenproject/core/FullChain.java
@@ -1,4 +1,85 @@
 package org.wolkenproject.core;
 
+import java.util.Set;
+
 public class FullChain extends BasicChain {
+    @Override
+    protected boolean makeBest(BlockIndex candidate) {
+        return false;
+    }
+
+    @Override
+    protected void broadcastChain() {
+
+    }
+
+    @Override
+    protected BlockIndex getBestBlock() {
+        return null;
+    }
+
+    @Override
+    protected BlockIndex getCandidate() {
+        return null;
+    }
+
+    @Override
+    protected void loadBestBlock() {
+
+    }
+
+    @Override
+    protected void setBestBlock(BlockIndex block) {
+
+    }
+
+    @Override
+    public boolean verifyBlock(BlockIndex block) {
+        return false;
+    }
+
+    @Override
+    protected boolean containsBlock(int height) {
+        return false;
+    }
+
+    @Override
+    protected boolean containsBlock(byte[] hash) {
+        return false;
+    }
+
+    @Override
+    protected byte[] getBlockHash(int height) {
+        return new byte[0];
+    }
+
+    @Override
+    protected void removeBlock(byte[] hash) {
+
+    }
+
+    @Override
+    protected void addOrphan(BlockIndex block) {
+
+    }
+
+    @Override
+    protected void addStale(BlockIndex block) {
+
+    }
+
+    @Override
+    public void suggest(Set<BlockIndex> blocks) {
+
+    }
+
+    @Override
+    public void suggest(BlockIndex block) {
+
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
 }
