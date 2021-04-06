759f13a12f75a915645523a198837c9d453ac118
Hammurabi <abdullahafadhil@gmail.com>
2021-04-06 22:35:26 +0200

    Update Event.java

--- src/main/java/org/wolkenproject/core/Event.java
+++ src/main/java/org/wolkenproject/core/Event.java
@@ -1,12 +1,19 @@
 package org.wolkenproject.core;
 
 import org.json.JSONObject;
+import org.wolkenproject.core.events.*;
 import org.wolkenproject.serialization.SerializableI;
 import org.wolkenproject.serialization.SerializationFactory;
 import org.wolkenproject.utils.HashUtil;
 
 public abstract class Event extends SerializableI {
     public static void register(SerializationFactory serializationFactory) {
+        serializationFactory.registerClass(NewAccountEvent.class, new NewAccountEvent(new byte[20]));
+        serializationFactory.registerClass(MintRewardEvent.class, new MintRewardEvent(new byte[20], 0));
+        serializationFactory.registerClass(DepositFundsEvent.class, new DepositFundsEvent(new byte[20], 0));
+        serializationFactory.registerClass(WithdrawFundsEvent.class, new WithdrawFundsEvent(new byte[20], 0));
+        serializationFactory.registerClass(DepositFeesEvent.class, new DepositFeesEvent(new byte[20], 0));
+        serializationFactory.registerClass(RegisterAliasEvent.class, new RegisterAliasEvent(new byte[20], 0));
     }
 
     public abstract void apply();
