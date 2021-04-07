b213cb2e3db68d3978979cfcc4c789fc0b79a9cf
Hammurabi <abdullahafadhil@gmail.com>
2021-04-07 03:05:41 +0200

    Update TaskScheduler.java

--- src/main/java/org/wolkenproject/core/TaskScheduler.java
+++ src/main/java/org/wolkenproject/core/TaskScheduler.java
@@ -2,12 +2,15 @@ package org.wolkenproject.core;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TaskScheduler implements Runnable {
-    private final Queue<Runnable> tasks;
+    private final Queue<Runnable>   tasks;
+    private final ReentrantLock     mutex;
 
     public TaskScheduler() {
         tasks = new LinkedList<>();
+        mutex = new ReentrantLock();
     }
 
     @Override
