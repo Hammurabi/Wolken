ae568de877ee09f46077c9b1d2d3958db8f0c2c1
Hammurabi <abdullahafadhil@gmail.com>
2021-04-11 18:34:13 +0200

    Update HashQueue.java

--- src/main/java/org/wolkenproject/utils/HashQueue.java
+++ src/main/java/org/wolkenproject/utils/HashQueue.java
@@ -4,9 +4,15 @@ public interface HashQueue<T> {
     boolean containsKey(byte[] hash);
     void removeTails(int newLength);
     T getByHash(byte[] hash);
-    void add(T element);
+    void add(T element, byte hash[]);
+    T poll();
+    T peek();
     void remove(byte hash[]);
     int size();
     // should return the amount of bytes in memory this queue is taking (if available)
     int byteCount();
+    default boolean hasElements() {
+        return !isEmpty();
+    }
+    boolean isEmpty();
 }
