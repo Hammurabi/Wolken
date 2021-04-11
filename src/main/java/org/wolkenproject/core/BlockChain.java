b6830da5c7f3766d7bb42b40238c028c8fcc2612
Hammurabi <abdullahafadhil@gmail.com>
2021-04-11 17:59:30 +0200

    Update BlockChain.java

--- src/main/java/org/wolkenproject/core/BlockChain.java
+++ src/main/java/org/wolkenproject/core/BlockChain.java
@@ -1,5 +1,6 @@
 package org.wolkenproject.core;
 
+import org.wolkenproject.core.consensus.CandidateBlock;
 import org.wolkenproject.core.transactions.Transaction;
 import org.wolkenproject.encoders.Base16;
 import org.wolkenproject.exceptions.WolkenException;
