c9d7d3dbf60ee65d43daba7a79a29f49a15ef0da
Hammurabi <abdullahafadhil@gmail.com>
2021-04-13 16:05:56 +0200

    Update HashQueue.java

--- src/main/java/org/wolkenproject/utils/HashQueue.java
+++ src/main/java/org/wolkenproject/utils/HashQueue.java
@@ -2,7 +2,10 @@ package org.wolkenproject.utils;
 
 public interface HashQueue<T> {
     boolean containsKey(byte[] hash);
-    void removeTails(int newLength);
+    default void removeTails(int newLength) {
+        removeTails(newLength, (a, b)->{});
+    }
+    void removeTails(int newLength, VoidCallableTY<T, byte[]> callable);
     T getByHash(byte[] hash);
     void add(T element, byte hash[]);
     T poll();
