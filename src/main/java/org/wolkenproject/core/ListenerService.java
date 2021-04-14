daaecdbc06ada1d333dd2b6eef361eaea2ae587c
Hammurabi <abdullahafadhil@gmail.com>
2021-04-14 07:06:12 +0200

    Update ListenerService.java

--- src/main/java/org/wolkenproject/core/ListenerService.java
+++ src/main/java/org/wolkenproject/core/ListenerService.java
@@ -1,9 +1,16 @@
 package org.wolkenproject.core;
 
+import org.wolkenproject.PendingTransaction;
+import org.wolkenproject.utils.VoidCallable;
+
 public class ListenerService {
     private final Context context;
 
     public ListenerService(Context context) {
         this.context = context;
+    }
+
+    public void registerPendingTransactionListener(VoidCallable<PendingTransaction> listener) {
+        context.getTransactionPool().registerPendingTransactionListener(listener);
     }
 }
