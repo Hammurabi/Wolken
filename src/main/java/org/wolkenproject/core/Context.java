abf14bf353b493acc20a7f0031b97fcf496f3e49
Hammurabi <abdullahafadhil@gmail.com>
2021-04-14 08:42:26 +0200

    Update Context.java

--- src/main/java/org/wolkenproject/core/Context.java
+++ src/main/java/org/wolkenproject/core/Context.java
@@ -52,6 +52,7 @@ public class Context {
 
         SerializationFactory.register(serializationFactory);
         OpcodeRegister.register(opcodeRegister);
+        ListenerService.register(verbosity);
 
         this.blockChain = AbstractBlockChain.create(pruned);
         this.server = new Server(forceConnections);
