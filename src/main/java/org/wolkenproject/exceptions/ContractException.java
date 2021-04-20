fb7fd86a8b59abda0b40314822b7e66548a78eeb
Hammurabi <abdullahafadhil@gmail.com>
2021-04-20 17:13:54 +0200

    Update ContractException.java

--- src/main/java/org/wolkenproject/exceptions/ContractException.java
+++ src/main/java/org/wolkenproject/exceptions/ContractException.java
@@ -1,4 +1,10 @@
 package org.wolkenproject.exceptions;
 
 public class ContractException extends WolkenException {
+    public ContractException() {
+    }
+
+    public ContractException(String msg) {
+        super(msg);
+    }
 }
