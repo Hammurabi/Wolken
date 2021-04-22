ddd35bd6febac2dde2dd9df0bb70234d9c548d4f
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-04-22 17:19:06 +0200

    Create PendingTransaction.java

---
+++ src/main/java/org/wolkenproject/core/PendingTransaction.java
@@ -0,0 +1,43 @@
+package org.wolkenproject.core;
+
+import org.wolkenproject.core.transactions.Transaction;
+
+public class PendingTransaction implements Comparable<PendingTransaction> {
+    private final Transaction   transaction;
+    private final long          when;
+
+    public PendingTransaction(Transaction transaction, long when) {
+        this.transaction = transaction;
+        this.when = when;
+    }
+
+    public boolean isInvalid() {
+        return transaction.checkTransaction() == Transaction.TransactionCode.InvalidTransaction;
+    }
+
+    public boolean shouldDelete() {
+        return System.currentTimeMillis() - when > Context.getInstance().getContextParams().getMaxTransactionUnconfirmedTime();
+    }
+
+    @Override
+    public int compareTo(PendingTransaction o) {
+        return transaction.getTransactionFee() > o.transaction.getTransactionFee() ? -1 : 1;
+    }
+
+    public int calculateSize() {
+        return transaction.calculateSize();
+    }
+
+    public byte[] getHash() {
+        return transaction.getHash();
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    @Override
+    public String toString() {
+        return transaction.toString();
+    }
+}
