529cb808ec5baa2fc5610efeae93874cdeb5973d
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-04-23 00:22:41 +0200

    Update Compiler.java

--- src/main/java/org/wolkenproject/papaya/compiler/Compiler.java
+++ src/main/java/org/wolkenproject/papaya/compiler/Compiler.java
@@ -4,14 +4,16 @@ import org.json.JSONObject;
 import org.wolkenproject.exceptions.PapayaException;
 import org.wolkenproject.exceptions.WolkenException;
 
+import java.io.IOException;
+
 public abstract class Compiler {
-    public static Compiler getFor(String language) throws WolkenException {
+    public static Compiler getFor(String language) throws WolkenException, IOException {
         if (language.toLowerCase().equals("papaya")) {
             language = "papaya+v0.01a";
         }
 
         if (language.toLowerCase().equals("papaya+v0.01a")) {
-            return new PapayaCompiler(new PapayaLexer(PapayaLexer.getTokenTypes()), new PapayaParser());
+            return new PapayaCompiler(new PapayaLexer(), new PapayaParser());
         }
 
         throw new WolkenException("compiler for language '" + language + "' could not be found.");
