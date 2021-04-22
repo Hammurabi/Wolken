146c995b6de9b8ad5c3c62f528cca4ebc94d6557
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-04-23 00:22:57 +0200

    Update PapayaCompiler.java

--- src/main/java/org/wolkenproject/papaya/compiler/PapayaCompiler.java
+++ src/main/java/org/wolkenproject/papaya/compiler/PapayaCompiler.java
@@ -1,24 +1,30 @@
 package org.wolkenproject.papaya.compiler;
 
 import org.json.JSONObject;
+import org.wolkenproject.core.Context;
 import org.wolkenproject.exceptions.PapayaException;
 import org.wolkenproject.exceptions.WolkenException;
+
+import java.io.IOException;
 
 public class PapayaCompiler extends Compiler {
     private PapayaLexer lexer;
     private PapayaParser parser;
+    private JSONObject grammar;
+    private JSONObject tokens;
 
-    public PapayaCompiler(PapayaLexer papayaLexer, PapayaParser papayaParser) {
+    public PapayaCompiler(PapayaLexer papayaLexer, PapayaParser papayaParser) throws IOException {
         super();
         this.lexer = papayaLexer;
         this.parser= papayaParser;
+        this.tokens = Context.getInstance().getResourceManager().getJson("/papaya/tokens.json");
+        this.grammar = Context.getInstance().getResourceManager().getJson("/papaya/grammar.json");
     }
 
     @Override
     public PapayaApplication compile(String text, JSONObject compilerArguments) throws PapayaException, WolkenException {
-        PapayaApplication application = parser.ingest(lexer.ingest(text));
-        application.compile();
+        AbstractSyntaxTree ast = parser.ingest(lexer.ingest(text, tokens), grammar);
 
-        return application;
+        return null;
     }
 }
