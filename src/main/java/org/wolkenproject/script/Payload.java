9ee72fa0091dc8a09a98409458e07b0082bb0912
Hammurabi <abdullahafadhil@gmail.com>
2021-04-22 17:03:27 +0200

    Update Payload.java

--- src/main/java/org/wolkenproject/script/Payload.java
+++ src/main/java/org/wolkenproject/script/Payload.java
@@ -3,34 +3,30 @@ package org.wolkenproject.script;
 import org.wolkenproject.exceptions.ContractException;
 import org.wolkenproject.exceptions.WolkenException;
 import org.wolkenproject.serialization.SerializableI;
-import org.wolkenproject.utils.VarInt;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public abstract class Payload extends SerializableI {
-    /* this is the "bytecode" version of the payload */
-    private int version;
-
-    public Payload(int version) {
-        this.version = version;
+    public Payload() {
     }
 
     /* this will call any function marked as "entrypoint" in payload */
     public abstract void entryPoint(Invoker invoker) throws ContractException;
 
-    public int getVersion() {
-        return version;
-    }
+    public abstract int getVersion();
+
+    public abstract void writePayload(OutputStream stream) throws IOException, WolkenException;
+    public abstract void readPayload(InputStream stream) throws IOException, WolkenException;
 
     @Override
     public final void write(OutputStream stream) throws IOException, WolkenException {
-        VarInt.writeCompactUInt32(version, false, stream);
+        writePayload(stream);
     }
 
     @Override
     public final void read(InputStream stream) throws IOException, WolkenException {
-        version = VarInt.readCompactUInt32(false, stream);
+        readPayload(stream);
     }
 }
