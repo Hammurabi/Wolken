6c52642cd233a1191bb2825c1dc5faa6032fd246
Hammurabi <abdullahafadhil@gmail.com>
2021-04-22 17:01:20 +0200

    Create OperatorName.java

---
+++ src/main/java/org/wolkenproject/papaya/compiler/OperatorName.java
@@ -0,0 +1,21 @@
+package org.wolkenproject.papaya.compiler;
+
+public enum OperatorName {
+    AddOperator,
+    SubOperator,
+    MulOperator,
+    DivOperator,
+    ModOperator,
+    PowOperator,
+
+    AndOperator,
+    OrOperator,
+    XorOperator,
+    ShiftOperator,
+    RightShiftOperator,
+    LeftShiftOperator,
+
+    NotOperator,
+    NegateOperator,
+    ;
+}
