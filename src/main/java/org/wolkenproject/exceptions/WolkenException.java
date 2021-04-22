e0c9692b8380a7b4d41ca0030559172f290def26
Hammurabi <abdullahafadhil@gmail.com>
2021-04-22 17:00:39 +0200

    Update WolkenException.java

--- src/main/java/org/wolkenproject/exceptions/WolkenException.java
+++ src/main/java/org/wolkenproject/exceptions/WolkenException.java
@@ -3,10 +3,14 @@ package org.wolkenproject.exceptions;
 public class WolkenException extends Exception {
     private static final long serialVersionUID = 5112431926980310096L;
 
+    public WolkenException(){
+    }
+
     public WolkenException(String msg)
     {
         super(msg);
     }
+
     public WolkenException(Throwable cause)
     {
         super(cause);
