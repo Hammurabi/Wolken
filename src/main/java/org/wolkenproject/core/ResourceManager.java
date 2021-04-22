e2f8ade27b94c6102f766b5eebca7610a9fb2ffc
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-04-23 00:22:35 +0200

    Update ResourceManager.java

--- src/main/java/org/wolkenproject/core/ResourceManager.java
+++ src/main/java/org/wolkenproject/core/ResourceManager.java
@@ -1,7 +1,11 @@
 package org.wolkenproject.core;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class ResourceManager {
     public InputStream get(String path) throws IOException {
@@ -11,5 +15,17 @@ public class ResourceManager {
         }
 
         return inputStream;
+    }
+
+    public JSONObject getJson(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(get(path)));
+        StringBuilder builder = new StringBuilder();
+        String line = "";
+
+        while ((line = reader.readLine()) != null) {
+            builder.append(line).append("\n");
+        }
+
+        return new JSONObject(builder.toString());
     }
 }
