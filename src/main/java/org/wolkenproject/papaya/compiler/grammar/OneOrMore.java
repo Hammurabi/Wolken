01062a7a4a0a7df2274fd5c3ae050ebc0f6c0167
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-05-02 10:51:46 +0200

    Create OneOrMore.java

---
+++ src/main/java/org/wolkenproject/papaya/compiler/grammar/OneOrMore.java
@@ -0,0 +1,54 @@
+package org.wolkenproject.papaya.compiler.grammar;
+
+import org.wolkenproject.exceptions.PapayaException;
+import org.wolkenproject.papaya.compiler.TokenStream;
+import org.wolkenproject.papaya.parser.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OneOrMore implements Rule {
+    private final Rule rule;
+
+    public OneOrMore(Rule rule) {
+        this.rule = rule;
+    }
+
+    @Override
+    public Node parse(TokenStream stream, DynamicParser rules, ParseResult result) throws PapayaException {
+        List<Node> list = new ArrayList<>();
+        Node node = null;
+
+        do {
+            node = rule.parse(stream, rules, result);
+            if (node != null) {
+                list.add(node);
+            }
+        } while (node != null);
+        if (!list.isEmpty()) {
+            String name = list.get(0).getTokenRule();
+            Node oom = new Node(name + "+", "");
+            oom.add(list);
+            return oom;
+        }
+
+        return null;
+    }
+
+    @Override
+    public int length(DynamicParser parser) throws PapayaException {
+        return 0;
+    }
+
+    @Override
+    public String toSimpleString(DynamicParser parser) {
+        return rule.toSimpleString(parser) + "+";
+    }
+
+    @Override
+    public String toString() {
+        return "OneOrMore{" +
+                "rule=" + rule +
+                '}';
+    }
+}
