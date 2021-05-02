ab85176ef85b0dd0717a4d93c47312f42e37dc6e
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-05-02 10:44:53 +0200

    Update Rule.java

--- src/main/java/org/wolkenproject/papaya/parser/Rule.java
+++ src/main/java/org/wolkenproject/papaya/parser/Rule.java
@@ -4,6 +4,8 @@ import org.wolkenproject.exceptions.PapayaException;
 import org.wolkenproject.papaya.compiler.TokenStream;
 
 public interface Rule {
-    ParseToken parse(final TokenStream stream, DynamicParser rules) throws PapayaException;
+    Node parse(final TokenStream stream, DynamicParser rules, ParseResult result) throws PapayaException;
     int length(DynamicParser parser) throws PapayaException;
+
+    String toSimpleString(DynamicParser parser);
 }
