324ac58ffb60406d3ea0d88a8322476e0e060138
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-05-02 10:51:51 +0200

    Create ZeroOrMore.java

---
+++ src/main/java/org/wolkenproject/papaya/compiler/grammar/ZeroOrMore.java
@@ -0,0 +1,58 @@
+package org.wolkenproject.papaya.compiler.grammar;
+
+import org.wolkenproject.exceptions.PapayaException;
+import org.wolkenproject.papaya.compiler.TokenStream;
+import org.wolkenproject.papaya.parser.DynamicParser;
+import org.wolkenproject.papaya.parser.Node;
+import org.wolkenproject.papaya.parser.ParseResult;
+import org.wolkenproject.papaya.parser.Rule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZeroOrMore implements Rule {
+    private final Rule rule;
+
+    public ZeroOrMore(Rule rule) {
+        this.rule = rule;
+    }
+
+    @Override
+    public Node parse(TokenStream stream, DynamicParser rules, ParseResult result) throws PapayaException {
+        List<Node> list = new ArrayList<>();
+        Node node = null;
+
+        do {
+            node = rule.parse(stream, rules, result);
+            if (node != null) {
+                list.add(node);
+            }
+        } while (node != null);
+
+        String name = "empty";
+        if (!list.isEmpty()) {
+            name = list.get(0).getTokenRule();
+        }
+
+        Node zom = new Node(name + "*", "");
+        zom.add(list);
+        return zom;
+    }
+
+    @Override
+    public int length(DynamicParser parser) throws PapayaException {
+        return 0;
+    }
+
+    @Override
+    public String toSimpleString(DynamicParser parser) {
+        return rule.toSimpleString(parser) + "*";
+    }
+
+    @Override
+    public String toString() {
+        return "ZeroOrMore{" +
+                "rule=" + rule +
+                '}';
+    }
+}
