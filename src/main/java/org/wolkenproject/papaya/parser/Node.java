ee0009b287a7d21e8ac6782770350ecaa981da73
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-05-17 21:21:43 +0200

    Update Node.java

--- src/main/java/org/wolkenproject/papaya/parser/Node.java
+++ src/main/java/org/wolkenproject/papaya/parser/Node.java
@@ -4,11 +4,13 @@ import org.wolkenproject.papaya.compiler.LineInfo;
 import org.wolkenproject.papaya.compiler.Token;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
-public class Node {
-    private final String tokenRule;
-    private final String ruleVariation;
+public class Node implements Iterable<Node> {
+    private String tokenRule;
+    private String ruleVariation;
     private final Token token;
     private final List<Node> children;
 
@@ -31,6 +33,10 @@ public class Node {
         return tokenRule;
     }
 
+    public String getRuleVariation() {
+        return ruleVariation;
+    }
+
     public Token getToken() {
         return token;
     }
@@ -51,6 +57,7 @@ public class Node {
 
         builder.append(indentation);
         builder.append(tokenRule);
+
         if (!ruleVariation.equals("default")) {
             builder.append(" ").append(ruleVariation);
         }
@@ -77,5 +84,87 @@ public class Node {
 
     public void add(List<Node> list) {
         children.addAll(list);
+    }
+
+    public void cleanUp() {
+        for (Node node : children) {
+            node.cleanUp();
+        }
+
+        if (tokenRule.isEmpty() && children.size() == 1) {
+            switchWithChild();
+        }
+    }
+
+    private void switchWithChild() {
+        Node child = children.get(0);
+        tokenRule = child.tokenRule;
+        ruleVariation = child.ruleVariation;
+        List<Node> nodes = child.children;
+        children.clear();
+        children.addAll(nodes);
+    }
+
+    @Override
+    public Iterator<Node> iterator() {
+        return children.iterator();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof String) {
+            return tokenRule.equals(o);
+        }
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return Objects.equals(tokenRule, node.tokenRule) && Objects.equals(ruleVariation, node.ruleVariation) && Objects.equals(token, node.token) && Objects.equals(children, node.children);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tokenRule, ruleVariation, token, children);
+    }
+
+    public boolean isNull() {
+        return children.isEmpty() && tokenRule.isEmpty() && ruleVariation.isEmpty();
+    }
+
+    public Node get(String type) {
+        for (Node node : this) {
+            if (node.equals(type)) {
+                return node;
+            }
+        }
+
+        return new Node("", "");
+    }
+
+    public List<Node> getAll(String type) {
+        List<Node> nodes = new ArrayList<>();
+        for (Node node : this) {
+            if (node.equals(type)) {
+                nodes.add(node);
+            }
+        }
+
+        return nodes;
+    }
+
+    public Node at(int index) {
+        return children.get(index);
+    }
+
+    public Node getLast(String type) {
+        Node last = null;
+
+        for (Node node : this) {
+            if (node.equals(type)) {
+                last = node;
+            }
+        }
+
+        return last;
     }
 }
