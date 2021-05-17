a5c80122ffc6c2334264c9af1448c6a5660e5763
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-05-17 21:18:08 +0200

    Update Compiler.java

--- src/main/java/org/wolkenproject/papaya/compiler/Compiler.java
+++ src/main/java/org/wolkenproject/papaya/compiler/Compiler.java
@@ -1,23 +1,38 @@
 package org.wolkenproject.papaya.compiler;
 
-import org.json.JSONObject;
+import org.wolkenproject.core.Context;
 import org.wolkenproject.exceptions.PapayaException;
 import org.wolkenproject.exceptions.WolkenException;
+import org.wolkenproject.papaya.archive.ArchivedMember;
+import org.wolkenproject.papaya.archive.ArchivedStruct;
+import org.wolkenproject.papaya.archive.PapayaArchive;
+import org.wolkenproject.papaya.parser.Node;
+import org.wolkenproject.utils.ByteArray;
 
 import java.io.IOException;
+import java.util.Map;
 
 public abstract class Compiler {
-    public static Compiler getFor(String language) throws WolkenException, IOException {
+    private Map<String, ByteArray> typeNames;
+
+    public static Compiler getFor(String language) throws WolkenException, IOException, PapayaException {
         if (language.toLowerCase().equals("papaya")) {
             language = "papaya+v0.01a";
         }
 
         if (language.toLowerCase().equals("papaya+v0.01a")) {
-            return new PapayaCompiler(new PapayaLexer(), new PapayaParser());
+            return new PapayaCompiler(Context.getInstance().getOpcodeRegister());
         }
 
         throw new WolkenException("compiler for language '" + language + "' could not be found.");
     }
 
-    public abstract PapayaApplication compile(String text, JSONObject compilerArguments) throws PapayaException, WolkenException;
+    public abstract PapayaArchive createArchive(String text, String compilerArguments) throws PapayaException, WolkenException, IOException;
+    public abstract PapayaApplication compile(PapayaArchive archive, String compilerArguments) throws PapayaException, WolkenException, IOException;
+
+    public ByteArray uniqueTypename(String type) {
+        return typeNames.get(type);
+    }
+
+    public abstract Expression compile(ArchivedStruct parent, ArchivedMember archivedMember, Node expression) throws PapayaException;
 }
