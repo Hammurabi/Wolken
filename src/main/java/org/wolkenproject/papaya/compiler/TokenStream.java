87542021bd6153475dda00b1fad38b5c08af49a8
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-05-17 21:21:30 +0200

    Update TokenStream.java

--- src/main/java/org/wolkenproject/papaya/compiler/TokenStream.java
+++ src/main/java/org/wolkenproject/papaya/compiler/TokenStream.java
@@ -1,24 +1,21 @@
 package org.wolkenproject.papaya.compiler;
 
-import org.wolkenproject.exceptions.PapayaException;
-import org.wolkenproject.utils.Pair;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TokenStream implements Iterable<Token> {
     private List<Token> tokenList;
     private int index;
+    private Queue<TokenPath> paths;
 
     public TokenStream() {
         this.tokenList = new ArrayList<>();
+        this.paths = new PriorityQueue<>();
     }
 
     public TokenStream(List<Token> tokens, int index) {
         this.tokenList = tokens;
         this.index = index;
+        this.paths = new PriorityQueue<>();
     }
 
     public void add(Token token) {
@@ -68,7 +65,8 @@ public class TokenStream implements Iterable<Token> {
         return tokenList.isEmpty();
     }
 
-    public void jump(int mark) {
+    public void jump(int mark, String parseRule) {
+        paths.add(new TokenPath(mark, get(mark), parseRule));
         this.index = mark;
     }
 
@@ -79,5 +77,40 @@ public class TokenStream implements Iterable<Token> {
     @Override
     public Iterator<Token> iterator() {
         return tokenList.iterator();
+    }
+
+    public Token get(int index) {
+        return tokenList.get(index);
+    }
+
+    public TokenPath getLongestPath() {
+        TokenPath token = paths.poll();
+        paths.clear();
+        return token;
+    }
+
+    public class TokenPath implements Comparable<TokenPath> {
+        private final int mark;
+        private final Token token;
+        private final String rule;
+
+        private TokenPath(int mark, Token token, String rule) {
+            this.mark = mark;
+            this.token = token;
+            this.rule = rule;
+        }
+
+        public Token getToken() {
+            return token;
+        }
+
+        public String getRule() {
+            return rule;
+        }
+
+        @Override
+        public int compareTo(TokenStream.TokenPath o) {
+            return mark > o.mark ? -1 : 1;
+        }
     }
 }
