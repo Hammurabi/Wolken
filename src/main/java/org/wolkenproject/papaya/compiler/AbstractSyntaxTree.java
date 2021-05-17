9152ca452f666f35cd1823af48e797eda8b89fa5
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2021-05-17 21:17:43 +0200

    Update AbstractSyntaxTree.java

--- src/main/java/org/wolkenproject/papaya/compiler/AbstractSyntaxTree.java
+++ src/main/java/org/wolkenproject/papaya/compiler/AbstractSyntaxTree.java
@@ -1,4 +1,19 @@
 package org.wolkenproject.papaya.compiler;
 
+import org.wolkenproject.papaya.parser.Node;
+
 public class AbstractSyntaxTree {
+    public Node root;
+
+    public AbstractSyntaxTree() {
+        root = new Node("root", "");
+    }
+
+    public void add(Node token) {
+        root.add(token);
+    }
+
+    public Node getRoot() {
+        return root;
+    }
 }
