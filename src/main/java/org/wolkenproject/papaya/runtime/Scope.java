0b249a8c76506d416a0e7d242034418d0e4f9fba
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2022-01-02 18:59:37 +0100

    Update Scope.java

--- src/main/java/org/wolkenproject/papaya/runtime/Scope.java
+++ src/main/java/org/wolkenproject/papaya/runtime/Scope.java
@@ -6,38 +6,42 @@ import org.wolkenproject.core.transactions.Transaction;
 import org.wolkenproject.exceptions.ContractOutOfFundsExceptions;
 import org.wolkenproject.exceptions.InvalidTransactionException;
 import org.wolkenproject.exceptions.PapayaException;
-import org.wolkenproject.papaya.compiler.PapayaStructure;
+import org.wolkenproject.papaya.compiler.Struct;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Scope {
     // the 'container' block
-    private BlockIndex              block;
+    private BlockIndex                          block;
     // the 'caller' transaction
-    private Transaction             caller;
+    private Transaction                         caller;
     // the contract in which the point of entry exists
-    private Contract                contract;
+    private Contract                            contract;
     // the stack
-    private PapayaStack<PapayaHandler> stack;
+    private Stack<PapayaStack<PapayaHandler>>   stack;
     // tell all subprocesses to continue running
-    private AtomicBoolean           keepRunning;
+    private AtomicBoolean                       keepRunning;
     // an interrupt signal (if any)
-    private AtomicInteger           interruptSignal;
+    private AtomicInteger                       interruptSignal;
     // the program counter
-    private ProgramCounter          programCounter;
+    private ProgramCounter                      programCounter;
+    private PapayaHandler                       nullreference;
 
-    public Scope(Block block, Transaction caller, Contract contract, PapayaStack<PapayaHandler> stack, ProgramCounter programCounter) {
+    public Scope(Block block, Transaction caller, Contract contract, ProgramCounter programCounter) {
         this.caller     = caller;
         this.contract   = contract;
-        this.stack      = stack;
+        this.stack      = new Stack<>();
         this.programCounter = programCounter;
+        this.nullreference  = new DefaultHandler(new PapayaObject());
     }
 
     public long startProcess(long availableFee) throws InvalidTransactionException, PapayaException, ContractOutOfFundsExceptions {
         while (getProgramCounter().hasNext() && keepRunning.get()) {
-            Opcode opcode = getProgramCounter().next();
+            OpcodeDefinition opcode = getProgramCounter().next();
 
             if (availableFee >= opcode.getWeight()) {
                 opcode.execute(this);
@@ -61,7 +65,7 @@ public class Scope {
     }
 
     public PapayaStack<PapayaHandler> getStack() {
-        return stack;
+        return stack.peek();
     }
 
     public ProgramCounter getProgramCounter() {
@@ -103,12 +107,36 @@ public class Scope {
         throw new PapayaException("invalid address provided.");
     }
 
-    public Stack<PapayaStructure> getStackTrace() {
+    public Stack<Struct> getStackTrace() {
         return null;
     }
 
     public void callOperator(int operator) throws PapayaException {
-        PapayaHandler a = getStack().peek(2);
-        a.getMember(a.getStructure().getOperatorId(operator), getStackTrace()).call(this);
+        getStack().peek().getStructure().getOperator(operator).call(this);
+    }
+
+    public PapayaHandler getNullReference() {
+        return nullreference;
+    }
+
+    public void makeTuple(int size) throws PapayaException {
+        List<PapayaHandler> tuple = new ArrayList<>(size);
+        for (int i = size - 1; i > -1; i --) {
+            tuple.set(i, stack.peek().pop());
+        }
+
+        stack.peek().push(new DefaultHandler(new PapayaTuple(tuple)));
+    }
+
+    public void rebase() {
+        stack.push(new PapayaStack<>());
+    }
+
+    public void drop() {
+        stack.pop();
+    }
+
+    public void dropKeep(int amount) {
+        stack.pop();
     }
 }
