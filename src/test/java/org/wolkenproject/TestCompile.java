232ea581ac7a2086e3d0ed8819585d9cd82b5df2
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2022-01-02 19:00:38 +0100

    Update TestCompile.java

--- src/test/java/org/wolkenproject/TestCompile.java
+++ src/test/java/org/wolkenproject/TestCompile.java
@@ -1,28 +1,32 @@
 package org.wolkenproject;
 
-import org.json.JSONObject;
-import org.junit.jupiter.api.Test;
 import org.wolkenproject.core.ResourceManager;
+import org.wolkenproject.crypto.CryptoLib;
 import org.wolkenproject.exceptions.PapayaException;
 import org.wolkenproject.exceptions.WolkenException;
-import org.wolkenproject.papaya.ParseRule;
-import org.wolkenproject.papaya.compiler.AbstractSyntaxTree;
-import org.wolkenproject.papaya.compiler.PapayaLexer;
-import org.wolkenproject.papaya.compiler.TokenStream;
-import org.wolkenproject.papaya.parser.DynamicParser;
+import org.wolkenproject.papaya.archive.PapayaArchive;
+import org.wolkenproject.papaya.compiler.*;
 import org.wolkenproject.papaya.compiler.Compiler;
+import org.wolkenproject.papaya.runtime.OpcodeRegister;
 
 import java.io.IOException;
 
 public class TestCompile {
-    @Test
     public void testCompile() throws WolkenException, IOException, PapayaException {
-        JSONObject papaya = ResourceManager.getJson("/papaya/papaya.json");
-        PapayaLexer lexer = new PapayaLexer();
-        DynamicParser dynamicParser = new DynamicParser(ResourceManager.getJson("/papaya/grammar.json"), papaya.getJSONArray("tokens"));
-
+        CryptoLib.getInstance();
         String program = ResourceManager.getString("/papaya/contract.pya");
-        TokenStream stream = lexer.ingest(program, papaya.getJSONArray("tokens"));
-        AbstractSyntaxTree ast = dynamicParser.parse(stream);
+        String librariesProgram = ResourceManager.getString("/papaya/libraries.pya");
+        OpcodeRegister opcodeRegister = new OpcodeRegister();
+        OpcodeRegister.register(opcodeRegister);
+        Compiler compiler = new PapayaCompiler(opcodeRegister);
+        PapayaArchive archive = compiler.createArchive(program, "-identifiers sequential");
+        PapayaArchive libraries = compiler.createArchive(librariesProgram, "-identifiers sequential");
+        PapayaApplication application = compiler.compile(archive, libraries, "-identifiers sequential");
+
+        System.out.println(application.toString());
+    }
+
+    public static void main(String args[]) throws WolkenException, IOException, PapayaException {
+        new TestCompile().testCompile();
     }
 }
