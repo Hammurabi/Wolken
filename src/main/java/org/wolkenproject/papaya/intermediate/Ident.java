a16e9a016cf785ce3489cdb1f5b061e3ffbf3066
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2022-01-02 18:58:01 +0100

    Create Ident.java

---
+++ src/main/java/org/wolkenproject/papaya/intermediate/Ident.java
@@ -0,0 +1,10 @@
+package org.wolkenproject.papaya.intermediate;
+
+import org.wolkenproject.exceptions.PapayaException;
+import org.wolkenproject.papaya.runtime.Scope;
+
+public class Ident implements Opcode {
+    @Override
+    public void execute(Scope scope) throws PapayaException {
+    }
+}
